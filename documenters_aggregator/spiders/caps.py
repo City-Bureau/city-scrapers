12805d0d9600d1559b07761ff397d6ac62fe8f25
Cyrus Sethna <csethna@gmail.com>
2017-08-10 10:37:32 -0500

    beginning to make the scraper for the Chicago Police community events.

---
+++ documenters_aggregator/spiders/caps.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+"""
+All spiders should yield data shaped according to the Open Civic Data
+specification (http://docs.opencivicdata.org/en/latest/data/event.html).
+"""
+import scrapy
+#, requests, json
+
+from datetime import datetime
+
+# LOOKUP_URL = "https://home.chicagopolice.org/wp-content/themes/cpd-bootstrap/proxy/miniProxy.php?https://home.chicagopolice.org/get-involved-with-caps/all-community-event-calendars/"
+#
+# headers = {
+# 'User-Agent': 'Mozilla/5.0 (Linux; <Android Version>; <Build Tag etc.>) AppleWebKit/<WebKit Rev> (KHTML, like Gecko) Chrome/<Chrome Rev> Mobile Safari/<WebKit Rev>'
+# }
+#
+# r = requests.get(LOOKUP_URL, headers=headers)
+# response = r.json()
+
+
+class CapsSpider(scrapy.Spider):
+    name = 'caps'
+    allowed_domains = ['https://home.chicagopolice.org/wp-content/themes/cpd-bootstrap/proxy/miniProxy.php?https://home.chicagopolice.org/get-involved-with-caps/all-community-event-calendars/']
+    start_urls = ['http://https://home.chicagopolice.org/wp-content/themes/cpd-bootstrap/proxy/miniProxy.php?https://home.chicagopolice.org/get-involved-with-caps/all-community-event-calendars/']
+
+    def parse(self, response):
+        """
+        `parse` should always `yield` a dict that follows the `Open Civic Data
+        event standard <http://docs.opencivicdata.org/en/latest/data/event.html>`_.
+
+        Change the `_parse_id`, `_parse_name`, etc methods to fit your scraping
+        needs.
+        """
+        for item in response.css('.eventspage'):
+            yield {
+                '_type': 'event',
+                'id': self._parse_id(calendarId),
+                'name': self._parse_name(title),
+                'description': self._parse_description(eventDetails),
+                'classification': 'CAPS community event',
+                'start_time': self._parse_start(start),
+                'end_time': self._parse_end(end),
+                'all_day': 'false',
+                'status': 'confirmed',
+                'location': self._parse_location(location),
+                'url': self._parse_url(eventUrl),
+            }
+
+        # self._parse_next(response) yields more responses to parse if necessary.
+        # uncomment to find a "next" url
+        # yield self._parse_next(response)
+
+    def _parse_next(self, response):
+        """
+        Get next page. You must add logic to `next_url` and
+        return a scrapy request.
+        """
+        next_url = None  # What is next URL?
+        return scrapy.Request(next_url, callback=self.parse)
+
+    def _parse_id(self, item):
+        """
+        Calulate ID. ID must be unique within the data source being scraped.
+        """
+        return None
+
+    def _parse_classification(self, item):
+        """
+        Parse or generate classification (e.g. town hall).
+        """
+        return 'Not classified'
+
+    def _parse_status(self, item):
+        """
+        Parse or generate status of meeting. Can be one of:
+
+        * cancelled
+        * tentative
+        * confirmed
+        * passed
+
+        By default, return "tentative"
+        """
+        return 'tentative'
+
+    def _parse_location(self, item):
+        """
+        Parse or generate location. Url, latitutde and longitude are all
+        optional and may be more trouble than they're worth to collect.
+        """
+        return {
+            'url': None,
+            'name': None,
+            'coordinates': {
+              'latitude': None,
+              'longitude': None,
+            },
+        }
+
+    def _parse_all_day(self, item):
+        """
+        Parse or generate all-day status. Defaults to false.
+        """
+        return False
+
+    def _parse_name(self, item):
+        """
+        Parse or generate event name.
+        """
+        return None
+
+    def _parse_description(self, item):
+        """
+        Parse or generate event name.
+        """
+        return None
+
+    def _parse_start(self, item):
+        """
+        Parse start date and time.
+        """
+        return None
+
+    def _parse_end(self, item):
+        """
+        Parse end date and time.
+        """
+        return None
