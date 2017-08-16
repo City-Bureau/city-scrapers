f7f0dc13d879cc6f32e54563ead52741b6dc1bab
Maxine Whitely <m.whitely16@gmail.com>
2017-08-15 23:56:37 -0500

    cleaned up event string lists by removing line breaks/html tags

--- documenters_aggregator/spiders/cpl.py
+++ documenters_aggregator/spiders/cpl.py
@@ -8,79 +8,10 @@ import re
 
 from datetime import datetime
 
-"""
-PROJECT STATUS AS OF 7/15/17 AT 7:15PM:
-    Currently have all the data on https://www.chipublib.org/board-of-directors/board-meeting-schedule/
-    loading with each event as a string. Now need to parse out the random characters from
-    each line in the event string, turn the date into the open civic data format date, and
-    then do a few other project reqs (for example make ID) to get the data in the right 
-    format before this spider is declared complete!!!
-
-    SETUP:
-        workon documentors_aggregator
-        scrapy shell https://www.chipublib.org/board-of-directors/board-meeting-schedule/
-            (may need to comment out some random code below to get the interactive
-            shell running)
-        and then you can work on it!!! woohoo!!!
-    
-        LINKS:
-            https://docs.scrapy.org/en/latest/intro/tutorial.html
-            https://docs.scrapy.org/en/latest/topics/selectors.html#topics-selectors
-            http://effbot.org/zone/element-index.htm#documentation
-            http://docs.opencivicdata.org/en/latest/data/event.html
-            https://docs.scrapy.org/en/latest/topics/selectors.html#topics-selectors-ref
-            https://www.chipublib.org/board-of-directors/board-meeting-schedule/
-            https://stackoverflow.com/questions/11252407/xpath-to-extract-text-after-br-tags-in-r
-            https://stackoverflow.com/questions/509211/explain-slice-notation
-            https://github.com/City-Bureau/documenters-aggregator/blob/master/documenters_aggregator/spiders/rta.py
-            https://docs.python.org/2/library/datetime.html
-            https://stackoverflow.com/questions/9750330/how-to-convert-integer-into-date-object-python
-            https://www.google.com/search?q=re.sub+how+to+substitute+multiple+patterns&oq=re.sub+how+to+substitute+multiple+patterns&aqs=chrome..69i57.14519j0j4&sourceid=chrome&ie=UTF-8
-
-
-
-        """
-
 class CplSpider(scrapy.Spider):
     name = 'cpl'
     allowed_domains = ['https://www.chipublib.org/']
     start_urls = ['https://www.chipublib.org/board-of-directors/board-meeting-schedule/']
-
-    #extracts each <P> event as a string
-    events = response.css('div.entry-content p').extract()
-    #extracts each line as its own string
-            #events_text = response.css('div.entry-content p::text').extract()
-    
-    #splices out the intro paragraphs on the page
-    events = events[2:10]
-    
-    #FOR TESTING. just grabs one event so I can try and splice out the uneeded characters
-    event1 = events[0]
-    #splits up an event at its line breaks
-    event1_split = event1.split("<br>")
-
-    #NOW: trying to substitute the character patterns below for empty spaces
-        #so far, it's not working and it only lets me replace the first thing.
-        #I dont want to go thru a for loop for each pattern, so I need to find a more
-        #efficient way to make this work. 
-    for line in event1_split:
-        re.sub('<p>', '', line)
-        re.sub('</p>', '', line)
-        re.sub('<strong>', '', line)
-        re.sub('</strong>', '', line)
-        re.sub('\n', '', line)
-        #maybe to be used for finding multiple patterns in re.sub (below):
-                #'\s+(<p>|</p>|<strong>|</strong>|\n)\s*$'
-
-    for event in events:
-        """
-        if len(lines) == 4:
-            location = lines[1:2]
-            address = lines[3]
-        if len(lines) == 3:
-            location = lines[1]
-            address = lines[2]
-        """
 
     def parse(self, response):
         """
@@ -90,18 +21,31 @@ class CplSpider(scrapy.Spider):
         Change the `_parse_id`, `_parse_name`, etc methods to fit your scraping
         needs.
         """
+        events = response.css('div.entry-content p').extract()
+
+        def cleanhtml(raw_html):
+            cleanr = re.compile('<.*?>')
+            cleantext = re.sub(cleanr, '', raw_html)
+            return cleantext
+
+        all_clean_events = []
+        for val in events:
+            clean_event = cleanhtml(val)
+            final_event = clean_event.splitlines()
+            all_clean_events.append(final_event)
+
         for item in response.css('.entry-content'):
             yield {
                 '_type': 'event',
-                'id': self._parse_id(item),
-                'name': 'Chicago Public Library Board Meeting Schedule'
-                'description': '',
-                'classification': 'Board meeting'
-                'start_time': ''
-                'end_time': '',
-                'all_day': self._parse_all_day(item),
-                'status': self._parse_status(item),
-                'location': self._parse_location(item),
+                'id': self._parse_id(item), #TODO
+                'name': self._parse_name(item), #Chicago Public Library Board Meeting Schedule
+                'description': self._parse_description(item),  #none ever on site
+                'classification': self._parse_classification(item), #Board meeting?
+                'start_time': self._parse_start(item), #turn date into correct format
+                'end_time': self._parse_end(item), #no end time listed
+                'all_day': self._parse_all_day(item), #default is false
+                'status': self._parse_status(item), #?
+                'location': self._parse_location(item), #2nd (and 3rd if it's 4 lines long) strings in event object
             }
 
         # self._parse_next(response) yields more responses to parse if necessary.
@@ -113,7 +57,7 @@ class CplSpider(scrapy.Spider):
         Get next page. You must add logic to `next_url` and
         return a scrapy request.
         """
-        next_url = None  # What is next URL?
+        next_url = None
         return scrapy.Request(next_url, callback=self.parse)
 
     def _parse_id(self, item):
@@ -121,6 +65,7 @@ class CplSpider(scrapy.Spider):
         Calulate ID. ID must be unique within the data source being scraped.
         """
         return None
+        #TODO: figure out unique ID-assigning mechanism
 
     def _parse_classification(self, item):
         """
@@ -146,20 +91,6 @@ class CplSpider(scrapy.Spider):
         Parse or generate location. Url, latitutde and longitude are all
         optional and may be more trouble than they're worth to collect.
         """
-        #how to get the URL of the location? auto-google to grab?
-
-        """
-        event = item.xpath('//para/text()[child::br]')
-        if len(event) == 3:
-            location = event[1]+event[2]
-            address = event[3]
-        else if len(event) == 2:
-            location = event[1]
-            address = event
-        for line in event:
-        address = 
-        """
-
         return {
             'url': None,
             'name': None,
@@ -191,8 +122,9 @@ class CplSpider(scrapy.Spider):
         """
         Parse start date and time.
         """
-        s = response.css('strong::text').extract()
-        tz = timezone('America/Chicago')
+        #s = response.css('strong::text').extract()
+        #tz = timezone('America/Chicago')
+        #TODO: turn every event array's first string into correct date format
 
 
     def _parse_end(self, item):
