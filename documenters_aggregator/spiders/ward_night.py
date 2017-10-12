b71666581fb5c5add5beab34baaf44d0904f6ab0
Jim Benton <jim@autonomousmachine.com>
2017-10-12 18:45:46 -0500

    Loading JSON works. Now working on generating values from the data.

--- documenters_aggregator/spiders/ward_night.py
+++ documenters_aggregator/spiders/ward_night.py
@@ -3,52 +3,109 @@
 All spiders should yield data shaped according to the Open Civic Data
 specification (http://docs.opencivicdata.org/en/latest/data/event.html).
 """
-import scrapy
-import re
 
-from datetime import datetime, timedelta
+import scrapy
+import os
+import json
+from datetime import datetime
 from pytz import timezone
-import time as Time
+from enum import IntEnum
+
+GOOGLE_API_KEY = os.environ.get('DOCUMENTERS_AGGREGATOR_GOOGLE_API_KEY') or 'test-token'
+SPREADSHEET_URL = 'https://sheets.googleapis.com/v4/spreadsheets/1xnt4kZI9Ruinw91wM-nnWftsFD-ZaKaozepdNXeIrpo'
+
+
+class Row(IntEnum):
+    """
+    This enum makes working with the data rows more pleasant.
+    """
+
+    ALDERMAN = 0
+    PHONE = 1
+    WEBSITE = 2
+    WARD = 3
+    DOCUMENTER = 4
+    ADDRESS = 5
+    DAY_OF_WEEK = 6
+    TIME = 7
+    FREQUENCY = 8
+    NOTES = 9
+
 
 class WardNightSpider(scrapy.Spider):
     name = 'ward_night'
-    #allowed_domains = ['']
-    #start_urls = ['']
+    allowed_domains = ['sheets.googleapis.com/v4/']
+    start_urls = []  # assigned in __init__
+
+    def __init__(self, google_api_key=GOOGLE_API_KEY, spreadsheet_url=SPREADSHEET_URL, *args, **kwargs):
+        super(WardNightSpider, self).__init__(*args, **kwargs)
+        self.start_urls = [spreadsheet_url + '/values/A1:AJ100?key=' + google_api_key]
 
     def parse(self, response):
         """
         `parse` should always `yield` a dict that follows the `Open Civic Data
         event standard <http://docs.opencivicdata.org/en/latest/data/event.html>`_.
-
-        Change the `_parse_id`, `_parse_name`, etc methods to fit your scraping
-        needs.
         """
 
-    def parse_event_page(self, response):
+        rows = json.loads(response.body.decode('utf-8'))['values']
+
+        # TODO: update the URL used above to skip the first 2 rows and then delete the next two lines
+        rows.pop(0)  # headers
+        rows.pop(0)  # example row
+
+        for row in rows:
+            # The JSON omits values for trailing columns with no values. By padding
+            # the rows out, the rest of the code can assume there will always be 10 columns.
+            missing_values = 10 - len(row)
+            row.extend([''] * missing_values)
+            yield self._parse_row(row)
+
+    def _parse_row(self, row):
         return {
             '_type': 'event',
-            'id': self._parse_id(response),
-            'name': self._parse_name(response),
-            'description': self._parse_description(response),
-            'classification': self._parse_classification(response),
-            'start_time': self._parse_date_time(response)['start'],
-            'end_time': self._parse_date_time(response)['end'],
-            'all_day': self._parse_all_day(response),
-            'status': self._parse_status(response),
-            'location': self._parse_location(response),
+            'id': self._parse_id(row),
+            'name': self._parse_name(row),
+            'description': self._parse_description(row),
+            'classification': self._parse_classification(row),
+            # 'start_time': self._parse_date_time(row)['start'],
+            # 'end_time': self._parse_date_time(row)['end'],
+            'all_day': self._parse_all_day(row),
+            'status': self._parse_status(row),
+            # 'location': self._parse_location(row),
         }
 
-    def _parse_id(self, response):
+    def _parse_id(self, row):
         """
-        Calulate ID. ID must be unique within the data source being scraped.
+        Generate ID. We are assuming that there will only be a single event in a each ward in a single day.
         """
 
-    def _parse_classification(self, response):
+        values = {
+            'ward': row[Row.WARD],
+            'date': '2017-01-01'
+        }
+        id = 'ward{ward}-{date}'.format(**values)
+        return id
+
+    def _parse_name(self, row):
+        """
+        Create event name
+        """
+
+        values = {
+            'ward': row[Row.WARD],
+            'alderman': row[Row.ALDERMAN],
+        }
+        name = 'Ward Night with Alderman {alderman} (Ward {ward})'.format(**values)
+        return name
+
+    def _parse_classification(self, row):
         """
         Parse or generate classification (e.g. town hall).
         """
 
-    def _parse_status(self, response):
+        return None
+
+    def _parse_status(self, row):
         """
         Parse or generate status of meeting. Can be one of:
 
@@ -61,7 +118,7 @@ class WardNightSpider(scrapy.Spider):
         """
         return 'tentative'
 
-    def _parse_location(self, response):
+    def _parse_location(self, row):
         """
         Parse or generate location. Url, latitutde and longitude are all
         optional and may be more trouble than they're worth to collect.
@@ -75,32 +132,26 @@ class WardNightSpider(scrapy.Spider):
             },
         }
 
-    def _parse_all_day(self, response):
+    def _parse_all_day(self, row):
         """
         Parse or generate all-day status. Defaults to false.
         """
-        date_time_extract = self._extract_date_time(response)
-        if 'all day' in date_time_extract:
-            return True
+
         return False
 
-    def _parse_name(self, response):
-        """
-        Parse or generate event name.
-        """
-        
-
-    def _parse_description(self, response):
+    def _parse_description(self, row):
         """
         Parse or generate event name.
         """
 
-    def _extract_date_time(self, response):
+        return row[Row.NOTES]
+
+    def _extract_date_time(self, row):
         '''
         Extract string with date, start time, end time
         '''
 
-    def _parse_date_time(self, response):
+    def _parse_date_time(self, row):
         """
         Parse start-date-time and end-date-time
         """
--- tests/test_ward_night.py
+++ tests/test_ward_night.py
@@ -3,29 +3,41 @@ import pytest
 from tests.utils import file_response
 from documenters_aggregator.spiders.ward_night import WardNightSpider
 
-test_response = file_response('files/ward_night.html')
+test_response = file_response('files/ward_night.json')
 spider = WardNightSpider()
 parsed_items = [item for item in spider.parse(test_response) if isinstance(item, dict)]
 
 
+def test_id():
+    assert parsed_items[0]['id'] == 'ward1-2017-01-01'
+
+
 def test_name():
-    assert parsed_items[0]['name'] == ''
+    assert parsed_items[0]['name'] == 'Ward Night with Alderman Joe Moreno (Ward 1)'
 
 
 def test_description():
-    assert parsed_items[0]['description'] == ''
+    assert parsed_items[0]['description'] == 'first come first served, one-on-one meetings usually about 10-20 minutes'
 
 
-def test_start_time():
-    assert parsed_items[0]['start_time'] == ''
+# def test_start_time():
+#     assert parsed_items[0]['start_time'] == ''
 
 
-def test_end_time():
-    assert parsed_items[0]['end_time'] == ''
+# def test_end_time():
+#     assert parsed_items[0]['end_time'] == ''
 
 
-def test_id():
-    assert parsed_items[0]['id'] == ''
+# def test_location():
+#     assert parsed_items[0]['location'] == {
+#         'url': '',
+#         'name': '',
+#         'coordinates': {
+#             'latitude': None,
+#             'longitude': None,
+#         },
+#         'address': ''
+#     }
 
 
 @pytest.mark.parametrize('item', parsed_items)
@@ -41,19 +53,6 @@ def test_classification(item):
 @pytest.mark.parametrize('item', parsed_items)
 def test_status(item):
     assert parsed_items[0]['status'] == 'tentative'
-
-
-@pytest.mark.parametrize('item', parsed_items)
-def test_location(item):
-    assert item['location'] == {
-        'url': '',
-        'name': ',
-        'coordinates': {
-            'latitude': None,
-            'longitude': None,
-        },
-        'address': ''
-    }
 
 
 @pytest.mark.parametrize('item', parsed_items)
