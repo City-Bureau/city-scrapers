734444a6a1ecff3fe5a45bfefc7e1a387a46d329
David Eads <davideads@gmail.com>
2017-10-13 00:17:31 -0500

    fix new error with cpb

--- tests/test_cpb.py
+++ tests/test_cpb.py
@@ -14,21 +14,7 @@ def test_name():
 
 @pytest.mark.parametrize('item', parsed_items)
 def test_description(item):
-    expected_description = (
-        'The Police Board holds a regular public '
-        'meeting once a month. Members of the public are invited to '
-        'attend and are welcome to address questions or comments to the '
-        'Board. The Superintendent of Police (or his designee) and the '
-        'Chief Administrator of the Independent Police Review Authority '
-        '(or her designee) will be at the meetings. Prior sign-up is '
-        'required of those wishing to address the Board; contact the '
-        "Board\'s office by 4:30 p.m. of the day before the meeting to "
-        'add your name to the list of speakers.See below for the dates of '
-        'the regular monthly meetings..Also appearing below are links to '
-        'the transcripts of the meetings and the material made available at '
-        'the meeting--the "Blue Book" that includes the meeting agenda, '
-        'minutes, statistics on disciplinary matters, and a list of CPD '
-        'directives issued by the Superintendent.')
+    expected_description = ("The Police Board holds a regular public meeting once a month. Members of the public are invited to attend and are welcome to address questions or comments to the Board. The Superintendent of Police (or his designee) and the Chief Administrator of the Civilian Office of Police Accountability (or her designee) will be at the meetings. Prior sign-up is required of those wishing to address the Board; contact the Board's office by 4:30 p.m. of the day before the meeting to add your name to the list of speakers. See below for the dates of the regular monthly meetings. Unless otherwise noted, the meetings are on the third Thursday of the month, are scheduled to begin at 7:30 p.m., and take place at Chicago Public Safety Headquarters, 3510 South Michigan Avenue. Also appearing below are links to the transcripts of the meetings and the material made available at the meeting--the \"Blue Book\" that includes the meeting agenda, minutes, statistics on disciplinary matters, and a list of CPD directives issued by the Superintendent.")
     assert item['description'] == expected_description
 
 
