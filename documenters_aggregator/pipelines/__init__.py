976ead2bdf83dc5ecc787d93d716d8aae4bb9916
David Eads <davideads@gmail.com>
2017-10-14 16:47:42 -0500

    add all pipelines to __all__

--- documenters_aggregator/pipelines/__init__.py
+++ documenters_aggregator/pipelines/__init__.py
@@ -1,6 +1,12 @@
+from .airtable import AirtablePipline
+from .geocoder import GeocoderPipeline
 from .logging import DocumentersAggregatorLoggingPipeline
+from .sqlalchemy import DocumentersAggregatorSQLAlchemyPipeline
 
 
 __all__ = (
     'DocumentersAggregatorLoggingPipeline',
+    'AirtablePipeline',
+    'GeocoderPipeline',
+    'DocumentersAggregatorSQLAlchemyPipeline'
 )
