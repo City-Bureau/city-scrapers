c6bff6a99afa280c568299409e40e922e251a693
bonfirefan <bonnie.y.fan@gmail.com>
2017-10-14 21:19:58 -0500

    still unable to fully test, most components working on a separate scrapy shell test

--- documenters_aggregator/spiders/ccc.py
+++ documenters_aggregator/spiders/ccc.py
@@ -5,6 +5,7 @@ specification (http://docs.opencivicdata.org/en/latest/data/event.html).
 """
 import scrapy
 import requests
+import json
 import datetime as dt
 
 
@@ -28,26 +29,27 @@ class CccSpider(scrapy.Spider):
         future, i.e. past today's date.
         """
         data = json.loads(response.text)
-        item = scrapy.Selector(text=data['content'], type="html")
 
-        for item in response.content['results']:
+        for item in data['results']:
             yield {
                 '_type': 'event',
-                'id': self._parse_id(item),
-                'name': self._parse_name(item),
-                'description': self._parse_description(item),
-                'classification': self._parse_classification(item),
-                'start_time': self._parse_start(item),
-                'end_time': self._parse_end(item),
-                'all_day': self._parse_all_day(item),
-                'status': self._parse_status(item),
-                'location': self._parse_location(item),
+                'id': item['id'],
+                'name': item['name'],
+                'description': item['description'],
+                'classification': item['classification'],
+                'start_time': item['start_date'],
+                'end_time': item['end_date'],
+                'all_day': item['all_day'],
+                'status': item['status'],
+                #'location': self._parse_location(item),
             }
 
-        # self._parse_next(response) yields more responses to parse if necessary.
+        # self._parse_next(response) yields more (responses to parse if necessary.
         # uncomment to find a "next" url
-        #pages = response.content('meta')['max_page']
-        yield #self._parse_next(response, 1)
+        max_page = data['meta']['max_page']
+        page = data['meta']['page']
+        while page <= max_page:
+            yield self._parse_next(response, page)
 
     def _parse_next(self, response, pgnum):
         """
@@ -56,73 +58,13 @@ class CccSpider(scrapy.Spider):
         """
         pgnum = pgnum + 1
         next_url = start_urls[0]+'&page='+pgnum  # What is next URL?
-        return scrapy.Request(next_url, callback=self.parse)
-
-    def _parse_id(self, item):
-        """
-        Calulate ID. ID must be unique within the data source being scraped.
-        """
-        return item['id']
-
-    def _parse_classification(self, item):
-        """
-        Parse or generate classification (e.g. town hall).
-        """
-        return item['classification']
-
-    def _parse_status(self, item):
-        """
-        Parse or generate status of meeting. Can be one of:
-
-        * cancelled
-        * tentative
-        * confirmed
-        * passed
-
-        By default, return "tentative"
-        """
-        return item['status']
+        return scrapy.Request(next_url, callback=self.parse, dont_filter=True))
 
     def _parse_location(self, item):
         """
         Parse or generate location. Url, latitutde and longitude are all
         optional and may be more trouble than they're worth to collect.
         """
-        e_pg = requests.get(ocd_url + item.id)
-        loc = e_pg.content['location']
-        src = e_pg.sources[1]
-        return {
-            'url': src.url,
-            'name': loc.name,
-            'coordinates': loc.coordinates,
-        }
 
-    def _parse_all_day(self, item):
-        """
-        Parse or generate all-day status. Defaults to false.
-        """
-        return item['all_day']
-
-    def _parse_name(self, item):
-        """
-        Parse or generate event name.
-        """
-        return item['name']
-
-    def _parse_description(self, item):
-        """
-        Parse or generate event name.
-        """
-        return item['description']
-
-    def _parse_start(self, item):
-        """
-        Parse start date and time.
-        """
-        return item['start_date']
-
-    def _parse_end(self, item):
-        """
-        Parse end date and time.
-        """
-        return item['end_date']
+        e_pg = requests.get(ocd_url + item['id'])
+        return e_pg.content['location']
