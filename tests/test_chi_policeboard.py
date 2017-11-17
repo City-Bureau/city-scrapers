3a13685cd99938184a477bafaece1b231acbf2c2
Jim Benton <jim@autonomousmachine.com>
2017-11-17 15:58:27 -0600

    Update policeboard spider to use new id format.

--- tests/test_chi_policeboard.py
+++ tests/test_chi_policeboard.py
@@ -28,7 +28,7 @@ def test_end_time(item):
 
 
 def test_id():
-    assert parsed_items[8]['id'] == 'CPBSeptember18'
+    assert parsed_items[8]['id'] == 'chi_policeboard/201709181930/x/public_meetings_of_the_police_board'
 
 
 @pytest.mark.parametrize('item', parsed_items)
