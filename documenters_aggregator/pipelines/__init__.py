f5f4fcbe85beacae1ffb6319a24ebd4cb4e97245
Rebecca Wei <arebeccawei@gmail.com>
2017-11-21 20:43:24 -0600

    validation pipeline ready for deploy

--- documenters_aggregator/pipelines/__init__.py
+++ documenters_aggregator/pipelines/__init__.py
@@ -2,11 +2,13 @@ from .airtable import AirtablePipeline
 from .geocoder import GeocoderPipeline
 from .LoggingPipeline import DocumentersAggregatorLoggingPipeline
 from .sqlalchemy import DocumentersAggregatorSQLAlchemyPipeline
+from .ValidationPipeline import ValidationPipeline
 
 
 __all__ = (
     'DocumentersAggregatorLoggingPipeline',
     'AirtablePipeline',
     'GeocoderPipeline',
-    'DocumentersAggregatorSQLAlchemyPipeline'
+    'DocumentersAggregatorSQLAlchemyPipeline',
+    'ValidationPipeline'
 )
