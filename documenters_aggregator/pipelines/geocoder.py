30a65f3e3625aaf7303ad15d0837dac75b0c1537
Rebecca Wei <aweia@allstate.com>
2017-12-01 11:33:55 -0600

    minor fixes

--- documenters_aggregator/pipelines/geocoder.py
+++ documenters_aggregator/pipelines/geocoder.py
@@ -5,10 +5,11 @@ import json
 import os
 import requests
 import datetime
+import time
+from random import randint
 
 from mapzen.api import MapzenAPI
 from airtable import Airtable
-from documenters_aggregator.utils import get_key
 
 AIRTABLE_BASE_KEY = os.environ.get('DOCUMENTERS_AGGREGATOR_AIRTABLE_BASE_KEY')
 AIRTABLE_GEOCODE_TABLE = os.environ.get('DOCUMENTERS_AGGREGATOR_AIRTABLE_GEOCODE_TABLE')
@@ -32,11 +33,13 @@ class GeocoderPipeline(object):
             location = item['location'].get('name', '')
         location = location.strip()  # attempt to clean up location, may need to do more
 
+        time.sleep(randint(0, 3))  # to avoid rate limiting?
         fetched_item = self._geocodeDB_fetch(location)
         if fetched_item:
             item['location']['coordinates'] = {
                 'longitude': str(fetched_item['longitude']),
-                'latitude': str(fetched_item['latitude'])
+                'latitude': str(fetched_item['latitude']),
+                'geocode': str(fetched_item['geocode'])
             }
             return item
 
@@ -47,16 +50,17 @@ class GeocoderPipeline(object):
                 'longitude': str(coordinates[0]),
                 'latitude': str(coordinates[1])
             }
-            item['geocode'] = json.dumps(geocode, indent=4, sort_keys=True)            
+            item['geocode'] = json.dumps(geocode, indent=4, sort_keys=True)
         except ValueError:
             spider.logger.warn('Could not geocode {0}-{1}, skipping.'.format(spider.name, item['id']))
         except Exception:
             spider.logger.exception('Unknown error when geocoding, skipping. Message:')
             spider.logger.error(json.dumps(item, indent=4, sort_keys=True))
         else:
-            write_item = {'location': location, 
-                'longitude': str(coordinates[0]),
-                'latitude': str(coordinates[1])}
+            write_item = {'location': location,
+                          'longitude': item['location']['coordinates']['longitude'],
+                          'latitude': item['location']['coordinates']['latitude'],
+                          'geocode': item['geocode']}
             self._geocodeDB_write(spider, write_item)
 
         return item
