cc226355a1b88dd29bc2377dd1fce6b5f6ec34de
Rebecca Wei <aweia@allstate.com>
2018-02-07 17:13:54 -0600

    fix travis branch environment variable

--- scripts/validate_spiders.sh
+++ scripts/validate_spiders.sh
@@ -4,7 +4,7 @@
 rm -f scripts/*.json
 
 # Run scrapers that have changed
-git diff --name-only --diff-filter=AM HEAD...$TRAVIS_BRANCH | \
+git diff --name-only --diff-filter=AM $TRAVIS_PULL_REQUEST_BRANCH...$TRAVIS_BRANCH | \
     grep .*documenters_aggregator/spiders/.*\.py | \
     xargs -I{} sh -c 'scraper=$(basename "${1%%.*}") ; scrapy crawl $scraper -o ./scripts/$scraper.json --loglevel=ERROR' -- {}
 
