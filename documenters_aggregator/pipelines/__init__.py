7504f8a4f224b002ff8be978d65a1e9fb4fc2176
bonfirefan <bonnie.y.fan@gmail.com>
2018-02-09 21:02:04 -0600

    inital creation for local file output pipeline

--- documenters_aggregator/pipelines/__init__.py
+++ documenters_aggregator/pipelines/__init__.py
@@ -2,13 +2,12 @@ from .airtable import AirtablePipeline
 from .geocoder import GeocoderPipeline
 from .LoggingPipeline import DocumentersAggregatorLoggingPipeline
 from .sqlalchemy import DocumentersAggregatorSQLAlchemyPipeline
-from .ValidationPipeline import ValidationPipeline
-
+from .output import CsvPipeline
 
 __all__ = (
     'DocumentersAggregatorLoggingPipeline',
     'AirtablePipeline',
     'GeocoderPipeline',
     'DocumentersAggregatorSQLAlchemyPipeline',
-    'ValidationPipeline'
+    'CsvPipeline'
 )
