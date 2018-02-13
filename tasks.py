29fea2ada9a0269aee29dcf6d54818e0cb7e3780
pjsier <pjsier@gmail.com>
2018-02-12 18:49:38 -0600

    Remove pandas dependency

--- tasks.py
+++ tasks.py
@@ -2,7 +2,8 @@ import requests
 import os
 import time
 import json
-import pandas as pd
+from collections import defaultdict
+from functools import reduce
 
 from deploy import ecs
 from invoke import Collection, task, run
@@ -166,14 +167,20 @@ def validate_spider(ctx, spider):
     conform to the schema.
     """
     scraped_items = json.load(open(os.path.join(TRAVIS_DIR, '{0}.json'.format(spider))))
-    validated_items = [{k: v for k, v in item.items() if k.startswith('val_')} for item in scraped_items]
-    validation_summary = pd.DataFrame(validated_items).mean()
+    validated_items = defaultdict(list)
+    for item in scraped_items:
+        for k, v in item.items():
+            if k.startswith('val_'):
+                validated_items[k].append(v)
 
     print('\n------------Validation Summary for: {0}---------------\n'.format(spider))
-    print(validation_summary)
+    validation_summary = {}
+    for item_key, item_list in validated_items.items():
+        validation_summary[item_key] = reduce(lambda x, y: x + y, item_list) / len(item_list)
+        print('{}: {:.0%}'.format(item_key[4:], validation_summary[item_key]))
 
     try:
-        assert all([x >= 0.9 for x in validation_summary.tolist()])
+        assert all([x >= 0.9 for x in validation_summary.values()])
     except AssertionError as e:
         message = (
             'Less than 90% of the scraped items from {0} passed validation. '
