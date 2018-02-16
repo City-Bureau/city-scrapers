ec0ffe12e46a5910064689a1fb7ae9780f5989d5
bonfirefan <bonnie.y.fan@gmail.com>
2018-02-16 11:30:33 -0600

    Stashing changes

--- documenters_aggregator/pipelines/exporter.py
+++ documenters_aggregator/pipelines/exporter.py
@@ -3,24 +3,24 @@ from scrapy.exporters import CsvItemExporter
 
 class CsvPipeline(object):
     """
-    This is a built in pipeline for scrapy
+    Pipeline uses a built in feed exporter from Scrapy currently
     Outputs csv files for local development to the /local_output/ folder
     """
     def __init__(self):
         self.file = open("documenters_aggregator/local_outputs/outputtest.csv", 'wb')
         self.exporter = CsvItemExporter(self.file)
         self.exporter.start_exporting()
-  
+
+    def create_valid_csv(self, item):
+        for key, value in item.items():
+            is_string = (isinstance(value, str))
+            if (is_string and ("," in value.encode('utf-8'))):
+                item[key] = "\"" + value + "\""
+
     def process_item(self, item, spider):
         self.exporter.export_item(item)
         return item
-    
-    def create_valid_csv(self, item):
-        for key, value in item.items():
-            is_string = (isinstance(value, basestring))
-            if (is_string and ("," in value.encode('utf-8'))):
-                item[key] = "\"" + value + "\""
-                
+                    
     def close_spider(self, spider):
         self.exporter.finish_exporting()
         self.file.close()
