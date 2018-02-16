483d7461d393211cf43f0cc88016720a932ff81a
bonfirefan <bonnie.y.fan@gmail.com>
2018-02-16 13:03:55 -0600

    Pipeline exports to one file per spider per crawl

--- documenters_aggregator/pipelines/exporter.py
+++ documenters_aggregator/pipelines/exporter.py
@@ -1,4 +1,8 @@
+from scrapy.xlib.pydispatch import dispatcher
+from scrapy import signals
 from scrapy.exporters import CsvItemExporter
+from documenters_aggregator.utils import get_key
+import datetime
 
 
 class CsvPipeline(object):
@@ -7,20 +11,52 @@ class CsvPipeline(object):
     Outputs csv files for local development to the /local_output/ folder
     """
     def __init__(self):
-        self.file = open("documenters_aggregator/local_outputs/outputtest.csv", 'wb')
-        self.exporter = CsvItemExporter(self.file)
+        dispatcher.connect(self.spider_opened, signals.spider_opened)
+        dispatcher.connect(self.spider_closed, signals.spider_closed)
+        self.files = {}
+
+    def spider_opened(self, spider):
+        stamp = datetime.datetime.now().strftime('%Y%m%d_%H%M')
+        path = 'documenters_aggregator/local_outputs/'
+        file = open('{}{}_{}.csv'.format(path, spider.name, stamp), 'w+b')
+        self.files[spider] = file
+        self.exporter = CsvItemExporter(file)
+        self.exporter.fields_to_export = ['agency_name'
+                                          'id', 'name', 'description',
+                                          'classification', 'start_time',
+                                          'end_time', 'timezone', 'all_day',
+                                          'location_name', 'location_url',
+                                          'location_address', 'location_latitude',
+                                          'location_longitude', 'url',
+                                          ]
         self.exporter.start_exporting()
 
-    def create_valid_csv(self, item):
-        for key, value in item.items():
-            is_string = (isinstance(value, str))
-            if (is_string and ("," in value.encode('utf-8'))):
-                item[key] = "\"" + value + "\""
+    def spider_closed(self, spider):
+        self.exporter.finish_exporting()
+        file = self.files.pop(spider)
+        file.close()
 
     def process_item(self, item, spider):
-        self.exporter.export_item(item)
-        return item
-                    
-    def close_spider(self, spider):
-        self.exporter.finish_exporting()
-        self.file.close()
+        new_item = item.copy()
+
+        # flatten location
+        new_item['location_url'] = get_key(new_item, 'location.url')
+        new_item['location_name'] = get_key(new_item, 'location.name')
+        new_item['location_address'] = get_key(new_item, 'location.address')
+        new_item['location_latitude'] = get_key(new_item, 'location.coordinates.latitude')
+        new_item['location_longitude'] = get_key(new_item, 'location.coordinates.longitude')
+        new_item['url'] = new_item.get('sources', [{'url': ''}])[0].get('url', '')
+        new_item['agency_name'] = spider.long_name
+        new_item = {k: self._format_values(k, v) for k, v in new_item.items() if k in self.exporter.fields_to_export}
+
+        self.exporter.export_item(new_item)
+        return new_item
+
+    def _format_values(self, k, v):
+        if ((v is None) or v == '') and (k not in ['start_time', 'end_time']):
+            return 'N/A'
+        if k == 'location_name':
+            return ' '.join([w.capitalize() for w in v.split(' ')])
+        if isinstance(v, bool):
+            return int(v)
+        return v
