6e391bff984b79a1caeb4c81331accf5b5768bfd
Rebecca Wei <aweia@allstate.com>
2018-02-16 17:48:00 -0600

    more tests for travis validation

--- tests/test_travis_pipeline.py
+++ tests/test_travis_pipeline.py
@@ -4,35 +4,55 @@ from documenters_aggregator.pipelines.TravisValidation import TravisValidationPi
 
 
 fixtures = json.loads(read_test_file_content('files/travis_fixture.json'))
+valid_item = fixtures[0]
 pipeline = TravisValidationPipeline()
 
 
-def test_process_item():
-    processed = pipeline.process_item(fixtures[0], None)
-    for k, v in pipeline.SCHEMA.items():
-        if v['required']:
-            assert processed['val_{}'.format(k)] == 1
-        else:
-            assert processed['val_{}'.format(k)] in [0, 1]
+def test_valid_process_item():
+    processed = pipeline.process_item(valid_item, None)
+    for k, v in processed.items():
+        if k.startswith('val_'):
+            assert v == 1
 
 
-def test_validate_location():
-    item = fixtures[0]
-    item['location']['name'] = None
-    processed = pipeline.process_item(item, None)
-    assert processed['val_loc_name'] == 0
+def test_invalid_required_value():
+    invalid_item = valid_item.copy()
+    invalid_item['id'] = ''
+    processed = pipeline.process_item(invalid_item, None)
+    assert processed['val_id'] == 0
 
 
-def test_validate_coordinates():
-    # Should be float, not string
-    item = fixtures[0]
-    item['location']['coordinates']['latitude'] = 10.0
-    processed = pipeline.process_item(item, None)
+def test_invalid_type():
+    invalid_item = valid_item.copy()
+    invalid_item['id'] = True
+    processed = pipeline.process_item(invalid_item, None)
+    assert processed['val_id'] == 0
+
+
+def test_invalid_format():
+    invalid_item = valid_item.copy()
+    invalid_item['id'] = 'invalid-id-format'
+    processed = pipeline.process_item(invalid_item, None)
+    assert processed['val_id'] == 0
+
+
+def test_invalid_location():
+    invalid_item = valid_item.copy()
+    invalid_item['location']['address'] = ''
+    processed = pipeline.process_item(invalid_item, None)
+    assert processed['val_loc_address'] == 0
+
+
+def test_invalid_coordinates():
+    # Should be string, not a float
+    invalid_item = valid_item.copy()
+    invalid_item['location']['coordinates']['latitude'] = 10.0
+    processed = pipeline.process_item(invalid_item, None)
     assert processed['val_coord_latitude'] == 0
 
 
-def test_validate_sources():
-    item = fixtures[0]
-    item['sources'].append({})
-    processed = pipeline.process_item(item, None)
+def test_invalid_sources():
+    invalid_item = valid_item.copy()
+    invalid_item['sources'].append({})
+    processed = pipeline.process_item(invalid_item, None)
     assert processed['val_sources'] == 0
