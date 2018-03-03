37635b7fe3dd3d940a1bcd04d5f2e904a4788de0
Jim Benton <jim@autonomousmachine.com>
2018-03-02 22:37:24 -0600

    Add debug_validations.sh

---
+++ travis/debug_validations.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Remove existing output
+rm -f travis/*.json
+
+## Get parent dir
+DIR="$( cd "$( dirname `dirname "${BASH_SOURCE[0]}"` )" && pwd )"
+
+## Force parent dir into python search path
+export PYTHONPATH=$PYTHONPATH:$DIR
+
+# Set scrapy settings
+export SCRAPY_SETTINGS_MODULE='travis.travis_settings'
+
+# Run new or modified spiders and save output
+find documenters_aggregator/spiders -name "[^_]*.py" | \
+    xargs sh -c 'scraper=$(basename "${1%%.*}") ; scrapy crawl $scraper -o ./travis/$scraper.json --loglevel=ERROR' --
+
+# Validate saved output
+find travis -name *.json | xargs invoke validate-spider
