599f1a32f741437eb23030ade913c2b33a6742b6
easherma <ericandrewsherman@gmail.com>
2018-04-06 22:15:56 -0500

    removing test code, makes no sense

--- tests/test_chi-mwrd.py
+++ tests/test_chi-mwrd.py
@@ -1,18 +1,8 @@
 import pytest
+import json
 
 from tests.utils import file_response
 from documenters_aggregator.spiders.chi_mwrd import Chi_mwrdSpider
 
 
 #TBD pending text file as testing fixture
-
-test_response = []
-with open('tests/files/chi_mwrd_fixture.json') as f:
-    for line in f:
-        test_response.append(json.loads(line))
-spider = Chi_mwrdSpider()
-parsed_items = [item for item in spider._parse_events(test_response)]
-
-def test_name():
-    assert parsed_items[0]['name'] == 'Board of Commissioners'
-    assert parsed_items[2]['name'] == 'Annual Meeting'
