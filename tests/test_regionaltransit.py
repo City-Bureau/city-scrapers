7d1255e8a5fbc1ae9804a154372a74b8759d16ed
o-stovicek <37935396+o-stovicek@users.noreply.github.com>
2018-04-13 17:51:35 -0500

    Update test_regional_transit description

--- tests/test_regionaltransit.py
+++ tests/test_regionaltransit.py
@@ -16,7 +16,12 @@ def test_name():
 
 
 def test_description():
-    assert parsed_items[0]['description'] == 'Test Description'
+    assert 'Test Description' == ("The RTA Board of Directors is a 16-member group of professionals"
+                        "governing the activities and initiatives of the RTA. The RTA is "
+                        "charged with financial oversight, funding, and regional transit "
+                        "planning for the region’s transit operators: the Chicago Transit
+                        "Authority (CTA), Metra and Pace Suburban Bus and Pace Americans "
+                        "with Disabilities Act (ADA) Paratransit.")
 
 
 def test_start_time():
