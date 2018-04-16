8a6c47ca0f646fea686df702c01a645dc823330c
Vince Cima <contact@vincecima.com>
2018-04-15 20:54:30 -0500

    Skeleton of spider

---
+++ tests/test_det_schools.py
@@ -0,0 +1,77 @@
+import pytest
+
+from tests.utils import file_response
+from documenters_aggregator.spiders.det_schools import Det_schoolsSpider
+
+
+def test_tests():
+    print('Please write some tests for this spider or at least disable this one.')
+    assert False
+
+
+"""
+Uncomment below
+"""
+
+# test_response = file_response('files/det_schools.html')
+# spider = Det_schoolsSpider()
+# parsed_items = [item for item in spider.parse(test_response) if isinstance(item, dict)]
+
+
+# def test_name():
+    # assert parsed_items[0]['name'] == 'EXPECTED NAME'
+
+
+# def test_description():
+    # assert parsed_items[0]['description'] == 'EXPECTED DESCRIPTION'
+
+
+# def test_start_time():
+    # assert parsed_items[0]['start_time'].isoformat() == 'EXPECTED START DATE AND TIME'
+
+
+# def test_end_time():
+    # assert parsed_items[0]['end_time'].isoformat() == 'EXPECTED END DATE AND TIME'
+
+
+# def test_id():
+    # assert parsed_items[0]['id'] == 'EXPECTED ID'
+
+
+# def test_location():
+    # assert parsed_items[0]['location'] == {
+        # 'url': 'EXPECTED URL',
+        # 'name': 'EXPECTED NAME',
+        # 'address': 'EXPECTED ADDRESS'
+        # 'coordinates': {
+            # 'latitude': 'EXPECTED LATITUDE',
+            # 'longitude': 'EXPECTED LONGITUDE',
+        # },
+    # }
+
+
+# def test_sources():
+    # assert parsed_items[0]['sources'] == {
+        # 'url': 'EXPECTED URL',
+        # 'note': 'EXPECTED NOTE'
+    # }
+
+
+# @pytest.mark.parametrize('item', parsed_items)
+# def test_timezone(item):
+    # assert item['timezone'] == 'EXPECTED TIMEZONE'
+
+
+# @pytest.mark.parametrize('item', parsed_items)
+# def test_all_day(item):
+    # assert item['all_day'] is False
+
+
+# @pytest.mark.parametrize('item', parsed_items)
+# def test_classification(item):
+    # assert item['classification'] is None
+
+
+# @pytest.mark.parametrize('item', parsed_items)
+# def test__type(item):
+    # assert parsed_items[0]['_type'] == 'event'
