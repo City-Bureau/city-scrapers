ad5d8307497c877d9cbefdf451ff6e2ecf8e35a0
bonfirefan <bonnie.y.fan@gmail.com>
2018-05-14 03:57:36 -0500

    Added address parsing and copied over geocaching from airtable

--- city_scrapers/pipelines/geocoder.py
+++ city_scrapers/pipelines/geocoder.py
@@ -3,6 +3,13 @@ Geocoder.
 """
 import geocoder
 import requests
+import usaddress
+from airtable import Airtable
+
+AIRTABLE_BASE_KEY = os.environ.get('DOCUMENTERS_AGGREGATOR_AIRTABLE_BASE_KEY')
+AIRTABLE_GEOCODE_TABLE = os.environ.get('DOCUMENTERS_AGGREGATOR_AIRTABLE_GEOCODE_TABLE')
+
+'TAMU_API_KEY' = 'e8a8f1283b3440c4b248adb52204c8ae' #temporary for testing
 
 
 class GeocoderPipeline(object):
@@ -10,6 +17,7 @@ class GeocoderPipeline(object):
         if session is None:
             session = requests.Session()
         self.session = session
+        self.geocode_database = Airtable(AIRTABLE_BASE_KEY, AIRTABLE_GEOCODE_TABLE)
 
     def process_item(self, item, spider):
         """
@@ -17,10 +25,122 @@ class GeocoderPipeline(object):
         coordinates.
         """
         if item['location']['coordinates'] is None:
-            item['location']['coordinates'] = self._geocode_address(item['location']['name'])
+            query = self.self.street_clean_dict(item.get('location', {}),'Chicago', 'IL')
+            if not query:
+                spider.logger.debug('GEOCODER PIPELINE: Empty query. Not geocoding {0}'.format(item['id']))
+                return item
+            item['location']['coordinates'] = self._geocode_address(query)
             return item
 
-    def _geocode_address(self, address):
-        g = geocoder.tamu(address, session=self.session)
+    def _geocode_address(self, query):
+        city_found = query['PlaceName']
+        state_found = query['StateName']
+        zipcode_found = querydict.get('ZipCode', '')
+        address = ', '.join(v for (k, v) in query.items() if k not in ['PlaceName', 'StateName', 'ZipCode'])
+        g = geocoder.tamu(address,
+                          city=city_found,
+                          state=state_found,
+                          session=self.session, key=TAMU_API_KEY)
         coords = g.latlng
         return {'latitude': str(coords[0]), 'longitude': str(coords[1])}
+
+    def street_clean_dict(self, location_dict, default_city, default_state):
+        """
+        Disabled Fuzzy match Chicago addresses on Chicago Data Portal Address API
+        """
+        name = location_dict.get('name', None)
+        address = location_dict.get('address', None)
+
+        if name is None and address is None:
+            return {}
+
+        if name is None:
+            query = address.strip()
+        else:
+            query = ', '.join([name.strip(), address.strip()])
+
+        # replace city hall
+        query = re.sub('city hall((?!.*chicago, il).)*$',
+                       'City Hall 121 N LaSalle Dr, Chicago, IL', query, flags=re.I)
+
+        try:
+            querydict = usaddress.tag(query)[0]
+        except usaddress.RepeatedLabelError as ex: 
+        # includ multiple errors
+            querydict = self.bad_address_tag(ex.parsed_string)
+
+        city = querydict.get('PlaceName', default_city) # replace w default city if blank
+        state = querydict.get('StateName', default_state)  # replace w default state if blank
+        querydict['PlaceName'] = city
+        querydict['StateName'] = state
+        return querydict
+
+    def bad_address_tag(parsed_string):
+        """
+        Turn the usaddress parse list of tuples with
+        RepeatedLabelError issues (duplicate labels)
+        Returns the second label as the valid label.
+        Thus far the only duplicate label appears to be due to
+        incorrectly identifying an earlier string as the PlaceName, Thus
+        we are encoding the second PlaceName as the true PlaceName while
+        preserving the order in an ordered dictionary.
+        """
+        counts = collections.Counter(t[1] for t in parsed_string)
+        parsedDict = collections.OrderedDict()
+
+        for t in parsed_string:
+            num = counts.get(t[1], 0)
+            signifier = t[1]
+            if num > 1:
+                signifier = signifier+str(num)
+                counts[t[1]] = num-1
+            parsedDict[signifier] = t[0]
+        return parsedDict
+
+    def _update_fromDB(self, query, item):
+        """
+        Query the geocode database and update item
+        with results.
+        """
+        fetched_item = self._geocodeDB_fetch(query)
+        try:
+            new_data = {
+                'location': {
+                    'coordinates': {
+                        'longitude': str(fetched_item['longitude']),
+                        'latitude': str(fetched_item['latitude'])
+                    },
+                    'name': fetched_item.get('name', ''),
+                    'address': fetched_item['address'],
+                    'url': item.get('location', {'url': ''}).get('url', '')
+                },
+                'geocode': str(fetched_item.get('geocode', '')),
+                'community_area': fetched_item.get('community_area', '')
+            }
+        except:
+            return {}
+        else:
+            updated_item = item.copy()
+            updated_item.update(new_data)
+            return updated_item
+
+    def _geocodeDB_fetch(self, query):
+        """
+        Fetch from geocode_database.
+        """
+        try:
+            return self.geocode_database.match('mapzen_query', query)['fields']
+        except:
+            return None
+
+    def _geocodeDB_write(self, spider, item):
+        """
+        Write to geocode_database.
+        """
+        spider.logger.debug('GEOCODER PIPELINE: Caching {0}'.format(item['mapzen_query']))
+        item['geocode_date_updated'] = datetime.datetime.now().isoformat()
+        airtable_item = self.geocode_database.match('mapzen_query', item['mapzen_query'])
+        if airtable_item:
+            self.geocode_database.update_by_field('mapzen_query', item['mapzen_query'], item)
+        else:
+            self.geocode_database.insert(item)
