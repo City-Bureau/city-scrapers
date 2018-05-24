a5892ae0a80f9c340fdda11851f3dc9e23331412
Matt Krump <mkrump@8thlight.com>
2018-05-23 23:04:18 -0500

    Fixes #365.
    - Update spider to reflect new page layout

--- city_scrapers/spiders/cook_housingauthority.py
+++ city_scrapers/spiders/cook_housingauthority.py
@@ -4,6 +4,7 @@ All spiders should yield data shaped according to the Open Civic Data
 specification (http://docs.opencivicdata.org/en/latest/data/event.html).
 """
 import scrapy
+import re
 
 from dateutil.parser import parse as dateparse
 from datetime import datetime
@@ -14,7 +15,7 @@ from city_scrapers.spider import Spider
 
 class Cook_housingAuthoritySpider(Spider):
     name = 'cook_housingauthority'
-    long_name = 'Housing Authority of Cook County'    
+    long_name = 'Housing Authority of Cook County'
     allowed_domains = ['http://thehacc.org/']
     start_urls = ['http://thehacc.org/events/feed/']
     event_timezone = 'America/Chicago'
@@ -27,21 +28,22 @@ class Cook_housingAuthoritySpider(Spider):
         Change the `_parse_id`, `_parse_name`, etc methods to fit your scraping
         needs.
         """
-        #events = response.css('item')
+        # events = response.css('item')
 
         for url in self._get_event_urls(response):
             yield scrapy.Request(url, callback=self._parse_event, dont_filter=True)
 
-        #for event in events:
+        # for event in events:
         #    yield self._parse_event(event)
-
-
 
     def _parse_event(self, response):
         """
         Parse the event page.
         """
-        start_time, end_time = self._parse_date_time(response)
+        raw_meeting_details = response.xpath(
+            "//header/h3[@class='entry-title']/following-sibling::p/text()").extract_first()
+        details = self._parse_raw_details_string(raw_meeting_details)
+        start_time, end_time = self._parse_date_time(details)
         data = {
             '_type': 'event',
             'name': response.css('h3.entry-title::text').extract_first(),
@@ -52,7 +54,7 @@ class Cook_housingAuthoritySpider(Spider):
             'all_day': self._parse_all_day(response),
             'timezone': self.event_timezone,
             'status': self._parse_status(response),
-            'location': self._parse_location(response),
+            'location': details['location'],
             'sources': [{'url': response.url, 'note': ''}]
         }
         data['id'] = self._generate_id(data, start_time)
@@ -61,18 +63,41 @@ class Cook_housingAuthoritySpider(Spider):
     def _get_event_urls(self, response):
         return response.css('item link::text').extract()
 
-    def _parse_date_time(self, response):
+    def _parse_date_time(self, details):
         '''
         '''
-        date = response.css('div.dtstart::attr(title)').extract_first()
-        time_range = response.css('div.dtstart::text').extract_first().strip()
-        time_list = time_range.split(' - ')
-
-        start_time = dateparse(' '.join([date, time_list[0]]))
-        end_time = dateparse(' '.join([date, time_list[1]]))
-
+        start_time = dateparse('{date} {time}'.format(date=details['date'], time=details['start_time']))
+        end_time = dateparse('{date} {time}'.format(date=details['date'], time=details['end_time']))
         return self._naive_datetime_to_tz(start_time), self._naive_datetime_to_tz(end_time)
 
+    @staticmethod
+    def _parse_raw_details_string(raw_meeting_details):
+        """
+        Parse meeting details from raw string
+            June 12, 2018, 1:00 pm - 2:30 pm 15306 S. Robey Ave.
+        """
+        matches = re.search('(.*(?: am | pm ))(.*)', raw_meeting_details)
+        date = re.search('(.*,.*), (.*)', matches.group(1))
+        address = matches.group(2)
+        time_list = date.group(2).split(' - ')
+        start_time = time_list[0].strip()
+        end_time = time_list[1].strip()
+        date = date.group(1)
+        location = {
+            'url': None,
+            'address': address,
+            'name': None,
+            'coordinates': {
+                'latitude': None,
+                'longitude': None,
+            },
+        }
+        return {
+            'date': date,
+            'start_time': start_time,
+            'end_time': end_time,
+            'location': location
+        }
 
     def _parse_description(self, response):
         desc = response.css('div.description::text').extract_first().strip()
@@ -82,38 +107,12 @@ class Cook_housingAuthoritySpider(Spider):
         else:
             return desc
 
-            
-    def _parse_location(self, response):
-        """
-        Parse or generate location. Url, latitude and longitude are all
-        optional and may be more trouble than they're worth to collect.
-        """
-        name = response.css('dd.tribe-venue::text').extract_first().strip()
-        street_address = response.css('span.tribe-street-address::text').extract_first().strip()
-        city = response.css('span.tribe-locality::text').extract_first().strip()
-        state = response.css('abbr.tribe-region::text').extract_first().strip()
-        zipcode = response.css('span.tribe-postal-code::text').extract_first().strip()
-
-        address = [street_address, city, state, zipcode]
-
-        address = ' '.join([x for x in address])
-        
-        return {
-            'url': None,
-            'address': address,
-            'name': name,
-            'coordinates': {
-                'latitude': None,
-                'longitude': None,
-            },
-        }
-
     def _parse_classification(self):
 
         return 'Not classified'
 
     def _parse_all_day(self, response):
-        
+
         return False
 
     def _parse_status(self, response):
