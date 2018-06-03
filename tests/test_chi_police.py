74ad3d84ba679c4861a9fe6ce284aab3436c762c
Rebecca Wei <arebeccawei@gmail.com>
2018-06-03 13:30:46 -0500

    update chi_police to new schema

--- tests/test_chi_police.py
+++ tests/test_chi_police.py
@@ -1,3 +1,4 @@
+from datetime import date, time
 import pytest
 from tests.utils import file_response
 from city_scrapers.spiders.chi_police import Chi_policeSpider
@@ -8,7 +9,7 @@ parsed_items = [item for item in spider.parse(test_response) if isinstance(item,
 
 
 def test_name():
-    assert parsed_items[0]['name'] == '2514 Beat Meeting'
+    assert parsed_items[0]['name'] == 'Beat Meeting, District 25'
 
 
 def test_description():
@@ -20,43 +21,49 @@ def test_description():
         "prioritize problems, and begin developing solutions "
         "to those problems."
     )
-    assert parsed_items[0]['description'] == EXPECTED_DESCRIPTION
+    assert parsed_items[0]['event_description'] == EXPECTED_DESCRIPTION
 
 
-def test_start_time():
-    assert parsed_items[0]['start_time'].isoformat() == '2017-12-28T18:30:00-06:00'
+def test_start():
+    EXPECTED_START = {
+        'start_date': date(2017, 12, 28),
+        'start_time': time(18, 30),
+        'note': ''
+    }
+    assert parsed_items[0]['start'] == EXPECTED_START
 
 
 def test_end_time():
-    assert parsed_items[0]['end_time'].isoformat() == '2017-12-28T19:30:00-06:00'
+    EXPECTED_END = {
+        'end_date': date(2017, 12, 28),
+        'end_time': time(19, 30),
+        'note': ''
+    }
+    assert parsed_items[0]['end'] == EXPECTED_END
 
 
 def test_id():
-    assert parsed_items[0]['id'] == 'chi_police/201712281830/25/2514_beat_meeting'
+    assert parsed_items[0]['id'] == 'chi_police/201712281830/25/beat_meeting_district_25'
 
 
 def test_all_day():
     assert parsed_items[0]['all_day'] is False
 
 
-def test_classification():
-    assert parsed_items[0]['classification'] == 'Beat Meeting, District 25'
-
-
 def test_status():
-    assert parsed_items[0]['status'] == 'confirmed'
+    assert parsed_items[0]['status'] == 'passed'
+
+
+def test_documents():
+    assert parsed_items[0]['documents'] == []
 
 
 def test_location():
-    EXPECTED_LOCATION = {
-            'url': None,
+    EXPECTED_LOCATION = [{
             'address': "St. Ferdinand's3115 N. Mason",
-            'name': None,
-            'coordinates': {
-                'latitude': None,
-                'longitude': None,
-            },
-    }
+            'name': '',
+            'neighborhood': ''
+    }]
     assert parsed_items[0]['location'] == EXPECTED_LOCATION
 
 
@@ -68,7 +75,3 @@ def test_sources():
     EXPECTED_SOURCES = [{'url': 'https://home.chicagopolice.org/get-involved-with-caps/all-community-event-calendars',
                          'note': ''}]
     assert parsed_items[0]['sources'] == EXPECTED_SOURCES
-
-
-def test_timezone():
-    assert parsed_items[0]['timezone'] == 'America/Chicago'
