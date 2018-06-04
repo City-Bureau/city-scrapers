34d8c035ce045bcc674000a884f0f375d2115f20
Rebecca Wei <arebeccawei@gmail.com>
2018-06-04 05:01:39 -0500

    temp fix so travis validation only checks chi_police

--- travis/validate_spiders.sh
+++ travis/validate_spiders.sh
@@ -13,11 +13,13 @@ export PYTHONPATH=$PYTHONPATH:$DIR
 export SCRAPY_SETTINGS_MODULE='travis.travis_settings'
 
 # If not a PR build, exit
-if [ $TRAVIS_PULL_REQUEST == 'false' ]; then echo "Build NOT triggered by a PR. Everything's ok."; exit 0; fi
+# if [ $TRAVIS_PULL_REQUEST == 'false' ]; then echo "Build NOT triggered by a PR. Everything's ok."; exit 0; fi
 
 # Run new or modified spiders and save output
-git diff --name-only --diff-filter=AM $TRAVIS_COMMIT_RANGE| \
-    grep .*city_scrapers/spiders/.*\.py | \
+# git diff --name-only --diff-filter=AM $TRAVIS_COMMIT_RANGE| \
+#    grep .*city_scrapers/spiders/.*\.py | \
+# temporarily only check chi_police spider
+echo city_scrapers/spiders/chi_police.py | \
     xargs basename -s .py | \
     xargs -I{} scrapy crawl {} -o ./travis/{}.json --loglevel=ERROR
 if [ $? -ne 0 ]; then exit 1; fi
