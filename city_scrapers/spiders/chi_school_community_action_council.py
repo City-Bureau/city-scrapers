126835d3e92d0ee9666c98ec64a9ed439b23361b
Rebecca Wei <aweia@allstate.com>
2018-07-02 21:04:32 -0500

    update CAC spider

--- city_scrapers/spiders/chi_school_community_action_council.py
+++ city_scrapers/spiders/chi_school_community_action_council.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from city_scrapers.spider import Spider
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, time
 from dateutil.parser import parse as dateparse
 import re
 
 class Chi_school_community_action_councilSpider(Spider):
     name = 'chi_school_community_action_council'
-    long_name = 'Chicago Public Schools Community Action Council'
+    agency_id = 'Chicago Public Schools Community Action Councils'
+    timezone = 'America/Chicago'
     allowed_domains = ['cps.edu']
     start_urls = ['http://cps.edu/FACE/Pages/CAC.aspx']
 
@@ -34,20 +35,18 @@ class Chi_school_community_action_councilSpider(Spider):
                     data = {
                         '_type': 'event',
                         'name': self._parse_name(item),
-                        'description': self._parse_description(item),
-                        'classification': self._parse_classification(item),
-                        'start_time': self._parse_start(item, month_counter),
-                        'end_time': self._parse_end(item),
-                        'timezone': self._parse_timezone(item),
-                        'status': self._parse_status(item),
+                        'event_description': self._parse_description(),
+                        'classification': self._parse_classification(),
+                        'start': self._parse_start(item, month_counter),
                         'all_day': self._parse_all_day(item),
                         'location': self._parse_location(item),
-                        'sources': self._parse_sources(response),
-                        'community_area' : self._parse_community_area(item)
+                        'sources': self._parse_sources(response, item),
+                        'documents': self._parse_documents()
                     }
 
                     data['id'] = self._generate_id(data)
-                    data['end_time'] = data['start_time'] + timedelta(hours=3) #adds 3 hours to start time
+                    data['status'] = self._generate_status(data, text='')
+                    data['end'] = self._parse_end(data['start'])
                     yield data
             month_counter += 1  # month counter is increased by 1 month with each iteration of the for loop
 
@@ -75,24 +74,17 @@ class Chi_school_community_action_councilSpider(Spider):
             community_name = item.css('li').css('strong').css('a::text').extract()
         return community_name[0] + ' Community Action Council'
 
-    def _parse_description(self, item):
+    def _parse_description(self):
         """
         Parse or generate event description.
         """
-        return "Community Action Councils, or CACs, consist of 25-30 voting members who are " \
-                                  "directly involved in developing a strategic plan for educational success within " \
-                                  "their communities. CAC members include parents; elected officials; faith-based " \
-                                  "institutions, health care and community-based organizations; Local School" \
-                                  " Council (LSC) members; business leaders; educators and school administrators; " \
-                                  "staff members from Chicago's Sister Agencies; community residents; " \
-                                  "and students. There are nine CACs across Chicago. Each works to empower the " \
-                                  "community they serve to lead the improvement of local quality education."
+        return ''
 
-    def _parse_classification(self, item):
+    def _parse_classification(self):
         """
         Parse or generate classification (e.g. public health, education, etc).
         """
-        return 'Education'
+        return 'committee'
 
     def _parse_start(self, item, month_counter):
         """
@@ -113,9 +105,13 @@ class Chi_school_community_action_councilSpider(Spider):
             '''Parses the source material and retrieves the time that the meeting occurs.
             '''
             time_source = source[1]
-            time_regex = re.compile(r'(1[012]|[1-9]):[0-5][0-9](am|pm)')
-            mo = time_regex.search(time_source)
-            return mo.group()
+            time_regex = re.compile(r'(1[012]|[1-9]):([0-5][0-9])(am|pm)')
+            hour, minute, period = time_regex.search(time_source).groups()
+            hour = int(hour)
+            minute = int(minute)
+            if (period == 'pm') and (hour != 12):
+                hour += 12
+            return time(hour, minute)
 
         def count_days(day, week_count):
             '''Because the source material provides meeting dates on a reoccuring schedule, we must use the parsed day
@@ -134,13 +130,6 @@ class Chi_school_community_action_councilSpider(Spider):
                 except ValueError as e:  # will break loop if range exceeds the number of days in the month
                     break
 
-        def concat_date(meeting_date, time):
-            '''Combines the meeting date with the time the meeting occurs. Function return a datetime
-            object.
-            '''
-            return dateparse(
-                str(meeting_date.year) + '-' + str(meeting_date.month) + '-' + str(meeting_date.day) + ' ' + time)
-
         def get_start(source):
             '''Combines above defined parse_day, parse_time, count_days, and concat_date functions to get the start
              date from the source. If a start time cannot be found the UNIX epoch date is returned.
@@ -149,28 +138,43 @@ class Chi_school_community_action_councilSpider(Spider):
             week_count = source[0].strip()[
                 0]  # selects first character in the source, which is usually the week count
             if week_count.isdigit():
-                time = parse_time(source)
                 meeting_date = count_days(day, week_count)
-                start = concat_date(meeting_date, time)
-            else:
-                pass
-            return start
+                return {
+                    'date': meeting_date.date(),
+                    'time': parse_time(source),
+                    'note': ''
+                }
+            return {
+                'date': None,
+                'time': None,
+                'note': ''
+            }
 
         source = item.css('li::text').extract()
 
         return get_start(source)
 
-    def _parse_end(self, item):
+    def _parse_end(self, start):
         """
-        Parse end date and time.
+        Estimates the end time by adding 3 hours to the start time.
         """
-        return 'Estimated 3 hours'
-
-    def _parse_timezone(self, item):
-        """
-        Parse or generate timzone in tzinfo format.
-        """
-        return 'America/Chicago'
+        try:
+            start_datetime = datetime.combine(start['date'], start['time'])
+        except TypeError: 
+            # start date or time is None
+            return {
+                'date': start['date'],
+                'time': None,
+                'note': 'No start or end time available'
+            }
+        else:
+            end_datetime = start_datetime + timedelta(hours=3)
+            return {
+                'date': end_datetime.date(),
+                'time': end_datetime.time(),
+                'note': 'Estimated 3 hours after the start time'
+            }
+        
 
     def _parse_all_day(self, item):
         """
@@ -185,31 +189,32 @@ class Chi_school_community_action_councilSpider(Spider):
         """
         source = item.css('li::text').extract()[1]
         return {
-            'url': None,
             'name': source[source.find("at")+2:source.find("(")].replace('the', ''),
             'address': source[source.find("(")+1:source.find(")")],
-            'coordinates': {
-                'latitude': None,
-                'longitude': None,
-            },
+            'neighborhood': self._parse_community_area(item)
         }
 
-    def _parse_status(self, item):
+    def _parse_sources(self, response, item):
         """
-        Parse or generate status of meeting. Can be one of:
-        * cancelled
-        * tentative
-        * confirmed
-        * passed
-        By default, return "tentative"
+        Parse the sources:
+        * CAC Meetings Website
+        * Neighborhood Website (if available)
         """
-        return 'Tentative'
-
-    def _parse_sources(self, response):
-        """
-        Parse or generate sources.
-        """
-        return [{
+        sources = [{
             'url': response.url,
-            'note': '',
+            'note': 'CAC Meetings Website',
         }]
+        neighborhood_url = item.css('li').css('strong').css('a::attr(href)') \
+                               .extract_first()
+        if neighborhood_url:
+            sources.append({
+                'url': neighborhood_url,
+                'note': "Neighborhood's Website"
+            })
+        return sources
+
+    def _parse_documents(self):
+        """
+        No documents on the site
+        """
+        return []
