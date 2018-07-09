d3cfe85e3ba84ef42b7476a41a3fc0cfce37dada
Vince Cima <contact@vincecima.com>
2018-07-09 18:08:41 -0500

    Put decorator on function call

--- city_scrapers/pipelines/item.py
+++ city_scrapers/pipelines/item.py
@@ -1,14 +1,14 @@
 from city_scrapers.utils import report_error
 
-@report_error
 class CityScrapersItemPipeline(object):
     """
     Pipeline for doing any custom processing on individual
     items, i.e. assigning the long name to agency_name
     """
+    @report_error
     def process_item(self, item, spider):
         raise Exception("HELLO")
-        
+
         if hasattr(spider, 'long_name'):
             item['agency_name'] = spider.long_name
         else:
