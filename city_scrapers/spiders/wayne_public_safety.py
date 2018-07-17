481093282a533c9daa03056fd129c1e56d470426
Novella C <novella@altodev.net>
2018-07-17 14:14:19 -0400

    446 wayne_public_safety remove long_name and amend comment

--- city_scrapers/spiders/wayne_public_safety.py
+++ city_scrapers/spiders/wayne_public_safety.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# WE ARE BRINGING IN A MIXIN WHICH IMPORTS OTHER LIBRARIES.
+# THIS SPIDER USES A MIXIN FOR SHARED FUNCTIONALITY.
 # MIXINS ARE STORED IN /city-scrapers/city-scrapers/mixins
-# YOU CAN TAKE THE DEFINITIONS OUT OF THE MIXIN AND ADD THEM HERE IF THEY ARE
-# UNIQUE.
+# YOU CAN OVERRIDE THE MIXIN HERE BY CREATING YOUR OWN DEFINITION.
 
 from city_scrapers.spider import Spider
 from city_scrapers.mixins.wayne_commission import Wayne_commission
@@ -11,7 +10,6 @@ from city_scrapers.mixins.wayne_commission import Wayne_commission
 
 class Wayne_public_safetySpider(Wayne_commission, Spider):
     name = 'wayne_public_safety'
-    long_name = 'Wayne County Committee on Public Safety, Judiciary, and Homeland Security'
     agency_id = 'Wayne County Committee on Public Safety, Judiciary, and Homeland Security'
     start_urls = ['https://www.waynecounty.com/elected/commission/public-safety-judiciary.aspx']
 
