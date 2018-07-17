270be94f8abdabeb0d6ba2d8f5bdccfafd55e72d
Novella C <novella@altodev.net>
2018-07-17 14:19:55 -0400

    444 wayne_government_operations remove long_name and amend comment.

--- city_scrapers/spiders/wayne_government_operations.py
+++ city_scrapers/spiders/wayne_government_operations.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# WE ARE BRINGING IN A MIXIN WHICH IMPORTS OTHER LIBRARIES.
+# THIS SPIDER USES A MIXIN FOR SHARED FUNCTIONALITY.
 # MIXINS ARE STORED IN /city-scrapers/city-scrapers/mixins
-# YOU CAN TAKE THE DEFINITIONS OUT OF THE MIXIN AND ADD THEM HERE IF THEY ARE
-# UNIQUE.
+# YOU CAN OVERRIDE THE MIXIN HERE BY CREATING YOUR OWN DEFINITION.
 
 from city_scrapers.spider import Spider
 from city_scrapers.mixins.wayne_commission import Wayne_commission
@@ -11,7 +10,6 @@ from city_scrapers.mixins.wayne_commission import Wayne_commission
 
 class Wayne_government_operationsSpider(Wayne_commission, Spider):
     name = 'wayne_government_operations'
-    long_name = 'Wayne County Committee on Government Operations'
     agency_id = 'Wayne County Committe on Government Operations'
     start_urls = ['https://www.waynecounty.com/elected/commission/government-operations.aspx']
 
