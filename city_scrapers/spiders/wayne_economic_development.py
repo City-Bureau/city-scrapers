0adb10b4b50a94a1e5b289abea0b073031c3c342
Novella C <novella@altodev.net>
2018-07-17 14:26:53 -0400

    442 wayne_economic_development remove long_name and amend comment.

--- city_scrapers/spiders/wayne_economic_development.py
+++ city_scrapers/spiders/wayne_economic_development.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# WE ARE BRINGING IN A MIXIN WHICH IMPORTS OTHER LIBRARIES.
+# THIS SPIDER USES A MIXIN FOR SHARED FUNCTIONALITY.
 # MIXINS ARE STORED IN /city-scrapers/city-scrapers/mixins
-# YOU CAN TAKE THE DEFINITIONS OUT OF THE MIXIN AND ADD THEM HERE IF THEY ARE
-# UNIQUE.
+# YOU CAN OVERRIDE THE MIXIN HERE BY CREATING YOUR OWN DEFINITION.
 
 from city_scrapers.spider import Spider
 from city_scrapers.mixins.wayne_commission import Wayne_commission
@@ -11,7 +10,6 @@ from city_scrapers.mixins.wayne_commission import Wayne_commission
 
 class Wayne_economic_developmentSpider(Wayne_commission, Spider):
     name = 'wayne_economic_development'
-    long_name = 'Wayne County Committee on Economic Development'
     agency_id = 'Wayne County Committee on Economic Development'
     start_urls = ['https://www.waynecounty.com/elected/commission/economic-development.aspx']
 
