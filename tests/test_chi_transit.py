78291a623ed4c716d6b1083e5a0ba6f688452ece
Rebecca Wei <aweia@allstate.com>
2018-07-30 19:15:11 -0500

    update chi_transit and test

--- tests/test_chi_transit.py
+++ tests/test_chi_transit.py
@@ -5,62 +5,83 @@ from tests.utils import file_response
 from city_scrapers.spiders.chi_transit import ChiTransitSpider
 from datetime import date, time
 
-freezer = freeze_time('2017-11-10 12:00:00')
+freezer = freeze_time('2018-01-01 12:00:00')
 freezer.start()
 
-test_response = file_response('files/chi_transit.html')
+test_response = file_response('files/chi_transit.html',
+    url='https://www.transitchicago.com/board/notices-agendas-minutes/')
 spider = ChiTransitSpider()
 parsed_items = [item for item in spider.parse(test_response) if isinstance(item, dict)]
 
 freezer.stop()
 
 
-def test_name():
-    assert parsed_items[0]['name'] == 'ADA Advisory Committee Meeting'
-    assert parsed_items[1]['name'] == 'Employee Retirement Review Committee (Cancelled)'
-    assert parsed_items[2]['name'] == 'Regular Board Meeting of Chicago Transit Board'
+def test_meeting_count():
+    assert len(parsed_items) == 23
 
+def test_unique_id_count():
+    assert len(set([item['id'] for item in parsed_items])) == 23
+
+def test_name():
+    assert parsed_items[0]['name'] == 'Employee Retirement Review Committee Meeting'
 
 def test_description():
-    assert parsed_items[0]['description'] == 'CTA Meetings -  Meeting Notice: http://www.transitchicago.com/assets/1/21/ADA_Advs_Commt_Mtg_Notice_for_April_9-2018.pdf?19085 Agenda: http://www.transitchicago.com/assets/1/21/ADA_Advs_Commt_Mtg_Agenda_for_4-9-2018.pdf?19504'
-    assert parsed_items[1]['description'] == 'CTA Meetings -  Meeting Notice: http://www.transitchicago.com/assets/1/21/CANCELLATION_032218_ERR_Committee.pdf?19154'
-    assert parsed_items[2]['description'] == 'Transit Board Meetings -  Meeting Notice: http://www.transitchicago.com/assets/1/21/Mar2018_-_Notice_-_Regular_Brd.pdf?19083 Agenda: http://www.transitchicago.com/assets/1/21/Mar2018_-_Regular_Board_Agenda.pdf?19084'
+    assert parsed_items[0]['event_description'] == ''
 
 def test_start():
     EXPECTED_START = {
-        'date': date(2017, 11, 10),
-        'time': time(12, 00, 00),
+        'date': date(2018, 6, 15),
+        'time': time(14, 0),
         'note': ''
     }
-    assert parsed_items[0]['date'] == EXPECTED_START
+    assert parsed_items[0]['start'] == EXPECTED_START
 
-#def test_end():
-   #EXPECTED_END = {
-        #'date': item_start.date(),
-        #'time': item_start.time(),
-        #'note': 'Estimated 3 hours after start time'
-    #}
-    #assert parsed_items[0]['end_time'] == EXPECTED_END
+def test_end():
+    EXPECTED_END = {
+        'date': date(2018, 6, 15),
+        'time': time(17, 0),
+        'note': 'estimated 3 hours after start time'
+    }
+    assert parsed_items[0]['end'] == EXPECTED_END
 
 
 def test_classification():
-    assert parsed_items[0]['classification'] == 'CTA Meetings'
-    assert parsed_items[2]['classification'] == 'Transit Board Meetings'
+    assert parsed_items[0]['classification'] == 'committee meeting'
 
 
-#def test_id():
-#    assert parsed_items[0]['id'] == 'chi_transit/201804091330/x/ada_advisory_committee_meeting'
-#    assert parsed_items[2]['id'] == 'chi_transit/201803141130/x/regular_board_meeting_of_chicago_transit_board'
+def test_id():
+    assert parsed_items[0]['id'] == 'chi_transit/201806151400/x/employee_retirement_review_committee_meeting'
 
 
-#def test_status():
-    #assert parsed_items[0]['status'] == 'tentative'
-    #assert parsed_items[1]['status'] == 'cancelled'
-    #assert parsed_items[0]['status'] == 'passed'
+def test_status():
+    assert parsed_items[0]['status'] == 'confirmed'
 
-@pytest.mark.parametrize('item', parsed_items)
-def test_end_time(item):
-    assert item['end_time'] is None
+
+def test_location():
+    assert parsed_items[0]['location'] == {
+        'neighborhood': 'west loop',
+        'name': 'Chicago Transit Authority 2nd Floor Boardroom',
+        'address': '567 West Lake Street Chicago, IL'
+    }
+
+
+def test_sources():
+    assert parsed_items[0]['sources'] == [{
+        'url': 'https://www.transitchicago.com/board/notices-agendas-minutes/',
+        'note': ''
+   }]
+
+def test_documents():
+    assert parsed_items[0]['documents'] == [
+        {
+            'note': 'Meeting Notice',
+            'url': 'http://www.transitchicago.com/assets/1/21/061818_ERR_Notice.pdf?20564'
+        },
+        {
+            'note': 'Agenda',
+            'url': 'http://www.transitchicago.com/assets/1/21/061818_ERR_Agenda.pdf?20565'
+        },
+    ]
 
 
 @pytest.mark.parametrize('item', parsed_items)
@@ -69,23 +90,5 @@ def test_all_day(item):
 
 
 @pytest.mark.parametrize('item', parsed_items)
-def test_location(item):
-    assert item['location'] == {
-        'url': 'http://www.transitchicago.com',
-        'name': 'Chicago Transit Authority 2nd Floor Boardroom',
-        'address': '567 West Lake Street Chicago, IL',
-        'coordinates': {
-            'latitude': '41.88528',
-            'longitude': '-87.64235',
-        },
-    }
-
-
-@pytest.mark.parametrize('item', parsed_items)
 def test__type(item):
     assert parsed_items[0]['_type'] == 'event'
-
-
-@pytest.mark.parametrize('item', parsed_items)
-def test_timezone(item):
-    assert item['timezone'] == 'America/Chicago'
