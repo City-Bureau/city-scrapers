17a56c599fc44eedefd0869f8516647a41dde3bf
Matt Krump <mkrump@8thlight.com>
2018-08-01 14:46:06 -0500

    Create General Retirement System spider

---
+++ city_scrapers/spiders/det_general_retirement_system.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from dateutil.parser import parse
+
+from city_scrapers.spider import Spider
+
+
+class DetGeneralRetirementSystemSpider(Spider):
+    name = 'det_general_retirement_system'
+    agency_id = 'General Retirement System'
+    timezone = 'America/Detroit'
+    allowed_domains = ['www.rscd.org']
+    start_urls = ['http://www.rscd.org/grsd/Resources/Meetings']
+
+    def parse(self, response):
+        """
+        `parse` should always `yield` a dict that follows the Event Schema
+        <https://city-bureau.github.io/city-scrapers/06_event_schema.html>.
+
+        Change the `_parse_id`, `_parse_name`, etc methods to fit your scraping
+        needs.
+        """
+        yield from self._generate_requests(response)
+        yield from self._next_month(response)
+
+    @staticmethod
+    def _next_month(response):
+        prev_call_count = response.meta.get('prev_call_count', 0)
+        if prev_call_count < 12:
+            form_params_xpath = "//td[@class='EventNextPrev'][2]/a/@href"
+            event_target, event_argument = response.xpath(form_params_xpath).re(r'\'(.*?)\'')
+            yield scrapy.FormRequest.from_response(
+                response,
+                formname='Form',
+                formdata={'__EVENTTARGET': event_target, '__EVENTARGUMENT': event_argument},
+                meta={'prev_call_count': prev_call_count + 1},
+            )
+
+    def _generate_requests(self, response):
+        anchor_xpath = '//table[@id="dnn_ctr531_Events_EventMonth_EventCalendar"]//a[contains(@id, "ctlEvents")]'
+        anchors = response.xpath(anchor_xpath)
+        for a in anchors:
+            yield response.follow(a, self._parse_item)
+
+    def _parse_item(self, response):
+        name = self._parse_name(response)
+        description = self._parse_description(response)
+        start = self._get_date(response, "Start Date")
+        end = self._get_date(response, "End Date")
+        location = self._get_location(response)
+        classification = self._parse_classification(name)
+
+        data = {
+            '_type': 'event',
+            'name': name,
+            'event_description': description,
+            'classification': classification,
+            'start': start,
+            'end': end,
+            'all_day': False,
+            'location': location,
+            'documents': [],
+            'sources': [{'url': response.url, 'note': ''}],
+        }
+        data['id'] = self._generate_id(data)
+        data['status'] = self._generate_status(data, text='')
+        yield data
+
+    @staticmethod
+    def _parse_description(response):
+        description_xpath = '//div[span[contains(., "Description")]]/following-sibling::div//p/text()'
+        description = response.xpath(description_xpath).extract_first().strip()
+        return description
+
+    @staticmethod
+    def _parse_name(response):
+        name_xpath = '//div[@id="dnn_ctr531_EventDetails_divEventDetails1"]//span[contains(@class, "Head")]/text()'
+        name = response.xpath(name_xpath).extract_first()
+        return name
+
+    @staticmethod
+    def _parse_classification(meeting_name):
+        if 'Board' in meeting_name:
+            return 'Board'
+        return ''
+
+    @staticmethod
+    def _get_location(response):
+        location_xpath = '//p[strong[contains(., "Address")]]/br/following-sibling::text()'
+        location_text = response.xpath(location_xpath).extract()
+        name1, name2, address1, address2 = [l.strip() for l in location_text]
+        location = {
+            'name': name1 + ', ' + name2,
+            'address': address1 + ' ' + address2,
+            'neighborhood': '',
+        }
+        return location
+
+    @staticmethod
+    def _get_date(response, contains):
+        date_xpath = '//div[span[contains(., "{}")]]/following-sibling::div[1]/span[1]/text()'.format(contains)
+        date_text = response.xpath(date_xpath).extract_first()
+        if date_text:
+            dt = parse(date_text)
+            return {'date': dt.date(), 'time': dt.time(), 'note': ''}
+        return {'date': None, 'time': None, 'note': ''}
---
+++ tests/test_det_general_retirement_system.py
@@ -0,0 +1,122 @@
+import urllib
+from datetime import date, time
+from urllib.parse import parse_qsl
+
+import pytest
+import scrapy
+from freezegun import freeze_time
+
+from city_scrapers.spiders.det_general_retirement_system import DetGeneralRetirementSystemSpider
+from tests.utils import file_response
+
+test_response = file_response('files/det_general_retirement_system_meetings.html',
+                              'http://www.rscd.org/grsd/Resources/Meetings')
+
+spider = DetGeneralRetirementSystemSpider()
+
+
+def test_request_count():
+    requests = list(spider.parse(test_response))
+    assert len(requests) == 5
+
+    calendar_events_urls = {urllib.parse.unquote(request.url) for request in requests if 'Details' in request.url}
+    assert calendar_events_urls == {
+        "http://www.rscd.org/grsd/Resources/Meetings/ctl/Details/Mid/531/ItemID/218?ContainerSrc=[G]Containers/RSCD/Plain-NoTitle",
+        "http://www.rscd.org/grsd/Resources/Meetings/ctl/Details/Mid/531/ItemID/1552?ContainerSrc=[G]Containers/RSCD/Plain-NoTitle",
+        "http://www.rscd.org/grsd/Resources/Meetings/ctl/Details/Mid/531/ItemID/1285?ContainerSrc=[G]Containers/RSCD/Plain-NoTitle",
+        "http://www.rscd.org/grsd/Resources/Meetings/ctl/Details/Mid/531/ItemID/223?ContainerSrc=[G]Containers/RSCD/Plain-NoTitle",
+    }
+
+    form_requests = [request for request in requests if isinstance(request, scrapy.FormRequest)]
+    assert len(form_requests) == 1
+
+    form_request = form_requests[0]
+    prev_call_count = form_request.meta.get('prev_call_count')
+    params = parse_qsl(form_request.body.decode(form_request.encoding))
+
+    assert prev_call_count == 1
+    # ASP.NET page paging has to be done via form request
+    # so make sure updated form params are in request
+    assert ('__EVENTTARGET', 'dnn$ctr531$Events$EventMonth$EventCalendar') in params
+    assert ('__EVENTARGUMENT', 'V6818') in params
+
+
+test_detail = file_response(
+    'files/det_general_retirement_system_meetings_detail.html',
+    'http://www.rscd.org/grsd/Resources/Meetings/ctl/Details/Mid/531/ItemID/1552?ContainerSrc=[G]Containers/RSCD/Plain-NoTitle'
+)
+freezer = freeze_time('2018-07-31 12:00:01')
+freezer.start()
+parsed_items = [item for item in spider._parse_item(test_detail) if isinstance(item, dict)]
+freezer.stop()
+
+
+def test_name():
+    assert parsed_items[0]['name'] == 'Board Meeting'
+
+
+def test_description():
+    assert parsed_items[0]['event_description'] == \
+           'Board of Trustees of the General Retirement System of the City of Detroit.'
+
+
+def test_start():
+    assert parsed_items[0]['start'] == {
+        'date': date(2018, 8, 1), 'time': time(10, 00), 'note': ''
+    }
+
+
+def test_end():
+    assert parsed_items[0]['end'] == {
+        'date': None, 'time': None, 'note': ''
+    }
+
+
+def test_id():
+    assert parsed_items[0]['id'] == 'det_general_retirement_system/201808011000/x/board_meeting'
+
+
+def test_status():
+    assert parsed_items[0]['status'] == 'confirmed'
+
+
+def test_location():
+    """
+    Retirement Systems' Conference Room, Ally Detroit Center
+    500 Woodward Ave., Suite 3000 Detroit, Michigan 48226
+    """
+    assert parsed_items[0]['location'] == {
+        'neighborhood': '',
+        'name': "Retirement Systems' Conference Room, Ally Detroit Center",
+        'address': '500 Woodward Ave., Suite 3000 Detroit, Michigan 48226'
+    }
+
+
+def test_sources():
+    assert parsed_items[0]['sources'] == [{
+        'url': 'http://www.rscd.org/grsd/Resources/Meetings/ctl/Details/Mid/531/ItemID/1552?ContainerSrc=[G]Containers/RSCD/Plain-NoTitle',
+        'note': ''
+    }]
+
+
+def test_documents():
+    assert parsed_items[0]['documents'] == []
+
+
+def test_classification():
+    assert parsed_items[0]['classification'] == 'Board'
+
+
+def test_parse_classification():
+    assert spider._parse_classification('EBP Meeting') == ''
+    assert spider._parse_classification('Board Meeting') == 'Board'
+
+
+@pytest.mark.parametrize('item', parsed_items)
+def test_all_day(item):
+    assert item['all_day'] is False
+
+
+@pytest.mark.parametrize('item', parsed_items)
+def test__type(item):
+    assert item['_type'] == 'event'
