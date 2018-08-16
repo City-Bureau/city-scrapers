31cddcda2eb5abc9da462ccf6f17f0da019cbcf8
Matt Krump <mkrump@8thlight.com>
2018-08-16 13:25:16 -0500

    Update timezone

--- city_scrapers/spiders/det_board_of_education.py
+++ city_scrapers/spiders/det_board_of_education.py
@@ -8,7 +8,7 @@ from city_scrapers.spider import Spider
 class DetBoardOfEducationSpider(Spider):
     name = 'det_board_of_education'
     agency_id = 'Board of Education'
-    timezone = 'America/Chicago'
+    timezone = 'America/Detroit'
     allowed_domains = ['detroitk12.org']
     start_urls = ['http://detroitk12.org/board/meetings/']
 
