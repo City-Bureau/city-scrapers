87337d48ae966ce3c90f4eecc6cb570e799f2885
thoo <theinhtay@gmail.com>
2018-11-18 01:23:31 -0500

    Add html/spider/test

---
+++ city_scrapers/spiders/pitt_allegheny_league.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+import os
+import unicodedata
+from datetime import datetime
+from city_scrapers.spider import Spider
+from city_scrapers.constants import BOARD, COMMITTEE, NOT_CLASSIFIED
+
+
+class PittAlleghenyLeagueSpider(Spider):
+    name = 'pitt_allegheny_league'
+    agency_name = 'Allegheny League of Municipalities'
+    timezone = 'America/Chicago'
+    allowed_domains = ['alleghenyleague.org']
+    start_urls = ['http://alleghenyleague.org/']
+
+    def parse(self, response):
+        """
+        `parse` should always `yield` a dict that follows the Event Schema
+        <https://city-bureau.github.io/city-scrapers/06_event_schema.html>.
+
+        Change the `_parse_id`, `_parse_name`, etc methods to fit your scraping
+        needs.
+        """
+        # meeting_titles = response.css('span.simcal-event-title::text').extract()
+        # meeting_titles = response.xpath('//span[@class="simcal-event-title"]/text()').extract()
+        calendar_data = response.xpath('string(//div[@class='
+                                       '"simcal-calendar simcal-default-calendar '
+                                       'simcal-default-calendar-list simcal-default'
+                                       '-calendar-light"]/*)').extract()[0]
+
+        # Remove \xa0 \xa
+        calendar_data = unicodedata.normalize("NFKD", calendar_data)
+        # Split events
+        calendar_data = calendar_data.split('\n See more details')
+        calendar_href = response.xpath('//div[@class="simcal-event-details"]//a/@href').extract()
+
+        for index, item in enumerate(calendar_data):
+            item = os.linesep.join([s.replace('\t', '').strip()
+                                   for s in item.splitlines()
+                                   if s.rstrip()]).split('\n')
+
+            if len(item) < 3:
+                continue
+            data = {
+                '_type': 'event',
+                'name': item[1],
+                'event_description': item[1],
+                'classification': self._parse_classification(item),
+                'start': self._parse_start(item),
+                'end': self._parse_end(item),
+                'all_day': self._parse_all_day(item),
+                'location': self._parse_location(item),
+                'documents': self._parse_documents(calendar_href[index]),
+                'sources': self._parse_sources(calendar_href[index]),
+            }
+
+            data['status'] = self._generate_status(data,
+                                                   item[-1])
+            data['id'] = self._generate_id(data)
+
+            yield data
+
+        # self._parse_next(response) yields more responses to parse if necessary.
+        # uncomment to find a "next" url
+        # yield self._parse_next(response)
+
+    def _parse_start(self, item):
+        """
+        Parse start date and time.
+        """
+        start, _ = item[2].split('-')
+        datetime_obj = datetime.strptime(start.strip(),
+                                 '%B %d, %Y @ %I:%M %p')
+        return {
+            'date': datetime_obj.date(),
+            'time': datetime_obj.time(),
+            'note': ''
+        }
+
+    def _parse_end(self, item):
+        """
+        Parse end date and time.
+        """
+        _, end = item[2].split('-')
+        datetime_obj = datetime.strptime(('{date} {time}'
+                                 .format(date=item[0], time=end)),
+                                 '%B %d, %Y %I:%M %p')
+        return {
+            'date': datetime_obj.date(),
+            'time': datetime_obj.time(),
+            'note': ''
+        }
+
+    def _parse_all_day(self, item):
+        """
+        Parse or generate all-day status. Defaults to False.
+        """
+        return False
+
+    def _parse_location(self, item):
+        """
+        Parse or generate location. Latitude and longitude can be
+        left blank and will be geocoded later.
+        """
+        return {
+            'address': item[-1],
+            'name': '',
+            'neighborhood': '',
+        }
+
+    def _parse_documents(self, item):
+        """
+        Parse or generate documents.
+        """
+        return [{'url': item, 'note': ''}]
+
+    def _parse_sources(self, item):
+        """
+        Parse or generate sources.
+        """
+        return [{'url': item, 'note': ''}]
+
+    def _parse_classification(self, item):
+        """
+        Differentiate board meetings from public hearings.
+        """
+        meeting_description = item[1].lower()
+        if ('bod' or 'board') in meeting_description:
+            return BOARD
+        if 'membership' in meeting_description:
+            return COMMITTEE
+        return NOT_CLASSIFIED
---
+++ tests/test_pitt_allegheny_league.py
@@ -0,0 +1,83 @@
+import pytest
+import datetime
+
+from tests.utils import file_response
+from city_scrapers.spiders.pitt_allegheny_league import PittAlleghenyLeagueSpider
+
+spider = PittAlleghenyLeagueSpider()
+test_response = file_response('files/pitt_allegheny_league.html')
+parsed_items = ([item for item in spider.parse(test_response)
+                if isinstance(item, dict)])
+import pdb;pdb.set_trace()
+
+
+def test_name():
+    assert parsed_items[0]['name'] == 'ACBA General Membership Meeting'
+
+
+def test_description():
+    assert parsed_items[0]['event_description'] == 'ACBA General Membership Meeting'
+
+
+def test_start():
+    assert (parsed_items[0]['start'] ==
+            {'date': datetime.date(2018, 12, 6),
+             'time': datetime.time(18, 0),
+             'note': ''})
+
+
+def test_end():
+    assert (parsed_items[0]['end'] ==
+            {'date': datetime.date(2018, 12, 6),
+             'time': datetime.time(21, 0),
+             'note': ''})
+
+
+def test_id():
+    assert (parsed_items[0]['id'] ==
+            'pitt_allegheny_league/201812061800/'
+            'x/acba_general_membership_meeting' )
+
+
+def test_status():
+    assert parsed_items[0]['status'] == 'tentative'
+
+
+def test_location():
+    assert parsed_items[0]['location'] == {
+        'address': ('Edgewood Country Club, '
+                    '100 Churchill Rd, '
+                    'Pittsburgh, PA 15235, USA'),
+        'name': '',
+        'neighborhood': ''
+    }
+
+
+def test_sources():
+    assert parsed_items[0]['sources'] == [{
+        'url': ('https://www.google.com/calendar/'
+                'event?eid=NGpqNjBuNzFhdHF2ZGNoNTdmbTAyd'
+                'WhkcmcgYWxvbWNhbGVuZGFyQG0&ctz=America/New_York'),
+                'note': ''}]
+
+
+def test_documents():
+    assert parsed_items[0]['sources'] == [{
+        'url': ('https://www.google.com/calendar/'
+                'event?eid=NGpqNjBuNzFhdHF2ZGNoNTdmbTAyd'
+                'WhkcmcgYWxvbWNhbGVuZGFyQG0&ctz=America/New_York'),
+                'note': ''}]
+
+
+@pytest.mark.parametrize('item', parsed_items)
+def test_all_day(item):
+    assert item['all_day'] is False
+
+
+def test_classification():
+    assert parsed_items[0]['classification'] == 'Committee'
+
+
+@pytest.mark.parametrize('item', parsed_items)
+def test__type(item):
+    assert parsed_items[0]['_type'] == 'event'
