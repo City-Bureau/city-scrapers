8690abd816b5b11a24011369687d6b415b044e06
Chan <cglee.toqo@gmail.com>
2018-12-08 13:12:10 -0600

    initial implementation of chi_ssa_21 spider

--- city_scrapers/spiders/chi_ssa_21.py
+++ city_scrapers/spiders/chi_ssa_21.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from city_scrapers.spider import Spider
-
+from city_scrapers.constants import COMMISSION
+from datetime import time, date
+import dateutil.parser
 
 class ChiSsa21Spider(Spider):
     name = 'chi_ssa_21'
@@ -18,7 +20,7 @@ class ChiSsa21Spider(Spider):
         Change the `_parse_id`, `_parse_name`, etc methods to fit your scraping
         needs.
         """
-        for item in response.css('.eventspage'):
+        for item in response.css('.cms-design-panel').css('p'):
 
             data = {
                 '_type': 'event',
@@ -54,31 +56,77 @@ class ChiSsa21Spider(Spider):
         """
         Parse or generate event name.
         """
-        return ''
+        return 'Commission Meetings'
 
     def _parse_description(self, item):
         """
         Parse or generate event description.
         """
-        return ''
+        description = 'N/A'
+
+        # The itinerary of the meeting is always stored in the <ul>
+        # element immediately following
+        detailElement = item.xpath('following-sibling::*[1]')
+        name = detailElement.xpath('name()').extract_first()
+
+        if (name == 'ul'):
+            topics = list(map(
+                lambda topic: ':'.join(filter(
+                    # Remove any strings that are empty
+                    None,
+                    [
+                        # Title of topic
+                        ''.join(topic.xpath('strong/text()').extract()).strip(),
+                        # Detail of topic
+                        ''.join(topic.xpath('text()').extract()).strip()
+                    ]
+                )),
+                detailElement.xpath('li')
+            ))
+
+            description = '-'.join(topics)
+
+        return description
 
     def _parse_classification(self, item):
         """
         Parse or generate classification (e.g. public health, education, etc).
         """
-        return ''
+        return COMMISSION
 
     def _parse_start(self, item):
         """
         Parse start date and time.
         """
-        return ''
+        startTime = self._parse_date(item)
+        startTime = startTime.replace(hour=9,minute=0)
+
+        ret = {
+            'date': startTime.date(),
+            'time': startTime.time(),
+            'note': None
+        }
+
+        return ret
 
     def _parse_end(self, item):
         """
         Parse end date and time.
         """
-        return ''
+        endTime = self._parse_date(item)
+        endTime = endTime.replace(hour=11,minute=0)
+
+        ret = {
+            'date': endTime.date(),
+            'time': endTime.time(),
+            'note': 'estimated 2 hours after start time'
+        }
+
+        return ret
+
+    def _parse_date(self, item):
+        rawDate = item.xpath('strong/text()').extract_first()
+        return dateutil.parser.parse(rawDate)
 
     def _parse_all_day(self, item):
         """
@@ -91,8 +139,19 @@ class ChiSsa21Spider(Spider):
         Parse or generate location. Latitude and longitude can be
         left blank and will be geocoded later.
         """
+
+        defaultLocation = 'Bistro Campagne, 4518 N. Lincoln Avenue'
+
+        # If location has changed, this is where it is noted
+        location = ''.join(
+            item.xpath('em//text()').extract()
+        ).strip()
+
+        if not location:
+            location = defaultLocation
+
         return {
-            'address': '',
+            'address': location,
             'name': '',
             'neighborhood': '',
         }
@@ -101,7 +160,11 @@ class ChiSsa21Spider(Spider):
         """
         Parse or generate documents.
         """
-        return [{'url': '', 'note': ''}]
+        url = item.xpath('a/@href').extract_first()
+
+        if url:
+            return [{'url': url, 'note': 'Meeting notes'}]
+        return []
 
     def _parse_sources(self, item):
         """
