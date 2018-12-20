397c09efa98ad7441307ab0c45a40fed84ae0867
zarifmahmud <35171619+zarifmahmud@users.noreply.github.com>
2018-12-20 10:42:19 -0500

    chi_board_elections spider (#608)

---
+++ city_scrapers/spiders/chi_board_elections.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+import re
+from datetime import datetime
+
+from city_scrapers.constants import COMMISSION
+from city_scrapers.spider import Spider
+
+
+class ChiBoardElectionsSpider(Spider):
+    name = 'chi_board_elections'
+    agency_name = 'Chicago Board of Elections'
+    timezone = 'America/Chicago'
+    allowed_domains = ['chicagoelections.com']
+    start_urls = [
+        'https://app.chicagoelections.com/pages/en/board-meetings.aspx',
+        'https://app.chicagoelections.com/pages/en/meeting-minutes-and-videos.aspx'
+    ]
+
+    def parse(self, response):
+        """
+        `parse` should always `yield` a dict that follows a modified
+        OCD event schema (docs/_docs/05-development.md#event-schema)
+
+        Change the `_parse_id`, `_parse_name`, etc methods to fit your scraping
+        needs.
+        """
+        if "minutes" in response.url:  # Current meetings and past meetings on differerent pages
+            yield from self._prev_meetings(response)
+        else:
+            yield from self._next_meeting(response)
+
+    def _next_meeting(self, response):
+        text = response.xpath('//text()').extract()
+        # Will return full dates, like "9:30 a.m. on Dec. 11, 2018"
+        dates = [
+            re.search(r'\d{1,2}:.*20\d{2}', x).group(0)
+            for x in text
+            if re.search(r'\d{1,2}:.*20\d{2}', x)
+        ]
+        # Has meeting location
+        next_meeting = response.xpath('//div[@class="copy"]/text()').extract()
+        for date in dates:
+            date.replace('\xa0', ' ')
+            data = {
+                '_type': 'event',
+                'name': "Electoral Board",
+                'event_description': "",
+                'classification': COMMISSION,
+                'start': self._parse_start(date, ""),
+                'all_day': False,
+                'documents': self._parse_documents(response, None),
+                'sources': [{
+                    'url': response.url,
+                    'note': ''
+                }],
+            }
+            if any("69" in x for x in next_meeting):
+                data['location'] = self._parse_location(response)
+            else:
+                raise ValueError("The address has changed.")
+
+            data['end'] = {
+                'date': data['start']['date'],
+                'time': None,
+                'note': '',
+            }
+            data['status'] = self._generate_status(data)
+            data['id'] = self._generate_id(data)
+
+            yield data
+
+    def _prev_meetings(self, response):
+        """
+        Meetingdate regex first searches for the 3 types of hyphens that chi_board_elections
+        uses (they like switching it up), and then finds a year number,
+        and returns everything in between.
+        """
+
+        meetings = response.xpath("//a|//span/text()").extract()
+        prevtime = None
+        for meeting in meetings:
+            meeting.replace('\xa0', ' ')  # Gets rid of non-breaking space character
+            meeting.replace(' ', ' ')
+            try:
+                meetingdate = re.search(r'(–|- |-)(.+[0-9]{4})', meeting).group(2)
+                while len(meetingdate) > 30:
+                    meetingdate = re.search(r'(–|- |-)(.+[0-9]{4})', meetingdate).group(2)
+                meetingdate.lstrip()
+                starttime = self._parse_start(meetingdate, meeting)
+                if prevtime != starttime:  # To acount for duplicates
+                    data = {
+                        '_type': 'event',
+                        'name': "Electoral Board",
+                        'event_description': "",
+                        'classification': COMMISSION,
+                        'start': starttime,
+                        'all_day': False,
+                        'location': self._parse_location(response),
+                        'documents': [],
+                        'sources': [{
+                            'url': response.url,
+                            'note': ''
+                        }],
+                    }
+                    data['end'] = {
+                        'date': data['start']['date'],
+                        'time': None,
+                        'note': '',
+                    }
+                    # Checks for link, will catch minutes or video links for one date
+                    if "href" in meeting:
+                        data["documents"].append(self._parse_documents(response, meeting))
+                    nextindex = meetings.index(meeting) + 1
+                    # In case there's both minutes and video for one date
+                    if nextindex < len(meetings):
+                        nextmeeting = meetings[nextindex].replace('\xa0', ' ')
+                        nextdate = re.search(r'(–|- |-)(.+[0-9]{4})', nextmeeting).group(2)
+                        while len(nextdate) > 30:
+                            nextdate = re.search(r'(–|- |-)(.+[0-9]{4})', nextdate).group(2)
+                        nextdate.lstrip()
+                        new_start = self._parse_start(nextdate, nextmeeting)
+                        if new_start == data['start'] and "href" in nextmeeting:
+                            data["documents"].append(self._parse_documents(response, nextmeeting))
+                    data['status'] = self._generate_status(data)
+                    data['id'] = self._generate_id(data)
+                    yield data
+                prevtime = starttime
+            except AttributeError:  # Sometimes meetings will return None
+                continue
+
+    def _parse_start(self, meetingdate, meeting):
+        """
+        Parse start date and time.
+        """
+        date = meetingdate
+        meetingtime = "9:30 AM on "
+        if "7 " in meeting:
+            time = re.search(r'7.+\S[m,.]', meeting).group(0)
+            str(time)
+            time = time.replace("7 ", "7:00 ")
+            meetingtime = "{} on ".format(time)
+            date = meetingtime + meetingdate
+        if ":" not in meetingdate or meeting:
+            date = meetingtime + meetingdate
+        formatitem = date.replace("a.m.", "AM")
+        formatitem = formatitem.replace("am", "AM")
+        formatitem = formatitem.replace("p.m.", "PM")
+        formatitem = formatitem.replace("Sept", "Sep")
+        try:
+            datetime_item = datetime.strptime(formatitem, '%I:%M %p on %b. %d, %Y')
+        except ValueError:  # Some months are abbreviated, some are not
+            datetime_item = datetime.strptime(formatitem, '%I:%M %p on %B %d, %Y')
+        dicti = {'date': datetime_item.date(), 'time': datetime_item.time(), 'note': ''}
+        return dicti
+
+    def _parse_location(self, item):
+        """
+        Parse or generate location. Latitude and longitude can be
+        left blank and will be geocoded later.
+        """
+
+        return {
+            'address': '8th Floor Office, 69 W. Washington St.',
+            'name': 'Cook County Administration Building',
+            'neighborhood': '',
+        }
+
+    def _parse_documents(self, response, meeting):
+        """
+        Parse or generate documents.
+        """
+        if "minutes" in response.url:
+            if "Minutes" in meeting:
+                minuteslink = re.search(r'"\/.+"', meeting).group(0).strip('"')
+                return {
+                    'url': "https://app.chicagoelections.com{}".format(minuteslink),
+                    'note': 'Regular Board Meeting Agenda'
+                }
+            elif "youtu" in meeting:
+                videolink = re.search(r'"h.+"', meeting).group(0).strip('"')
+                return {'url': videolink, 'note': "Regular Board Meeting Video"}
+        else:
+            link = response.xpath("//a/@href").extract()[2]
+            return [{
+                'url': "https://app.chicagoelections.com{}".format(link),
+                'note': 'Regular Board Meeting Agenda'
+            }]
---
+++ tests/test_chi_board_elections.py
@@ -0,0 +1,154 @@
+from datetime import date, time
+
+import pytest
+from freezegun import freeze_time
+from tests.utils import file_response
+
+from city_scrapers.constants import COMMISSION
+from city_scrapers.spiders.chi_board_elections import ChiBoardElectionsSpider
+
+freezer = freeze_time('2018-11-30 12:00:01')
+freezer.start()
+test_response = file_response(
+    'files/chi_board_elections.html',
+    url='https://app.chicagoelections.com/pages/en/board-meetings.aspx'
+)
+spider = ChiBoardElectionsSpider()
+parsed_items = [item for item in spider._next_meeting(test_response) if isinstance(item, dict)]
+
+
+def test_name():
+    assert parsed_items[0]['name'] == 'Electoral Board'
+
+
+def test_description():
+    assert parsed_items[0]['event_description'] == ''
+
+
+def test_start():
+    assert parsed_items[0]['start'] == {'date': date(2018, 11, 27), 'time': time(9, 30), 'note': ''}
+
+
+def test_end():
+    assert parsed_items[0]['end'] == {'date': date(2018, 11, 27), 'time': None, 'note': ''}
+
+
+def test_id():
+    assert parsed_items[0]['id'] == \
+           'chi_board_elections/201811270930/x/electoral_board'
+
+
+def test_status():
+    assert parsed_items[0]['status'] == 'passed'
+
+
+def test_location():
+    assert parsed_items[0]['location'] == {
+        'address': '8th Floor Office, 69 W. Washington St.',
+        'name': 'Cook County Administration Building',
+        'neighborhood': '',
+    }
+
+
+def test_sources():
+    assert parsed_items[0]['sources'] == [{
+        'url': 'https://app.chicagoelections.com/pages/en/board-meetings.aspx',
+        'note': ''
+    }]
+
+
+def test_documents():
+    assert parsed_items[0]['documents'] == [{
+        'note': 'Regular Board Meeting Agenda',
+        'url':
+            'https://app.chicagoelections.com/documents/'
+            'general/Standard-Board-Meeting-Agenda.pdf'
+    }]
+
+
+@pytest.mark.parametrize('item', parsed_items)
+def test_all_day(item):
+    assert item['all_day'] is False
+
+
+@pytest.mark.parametrize('item', parsed_items)
+def test_classification(item):
+    assert item['classification'] is COMMISSION
+
+
+@pytest.mark.parametrize('item', parsed_items)
+def test__type(item):
+    assert parsed_items[0]['_type'] == 'event'
+
+
+# Previous meetings on different page
+prev_url = "https://app.chicagoelections.com/pages/en/meeting-minutes-and-videos.aspx"
+test_response_prev = file_response('files/chi_board_elections_prev.html', url=prev_url)
+parsed_items_prev = \
+    [item for item in spider._prev_meetings(test_response_prev) if isinstance(item, dict)]
+freezer.stop()
+
+
+def test_name_prev():
+    assert parsed_items_prev[0]['name'] == 'Electoral Board'
+
+
+def test_description_prev():
+    assert parsed_items_prev[0]['event_description'] == ''
+
+
+def test_start_prev():
+    assert parsed_items_prev[0]['start'] == \
+           {'date': date(2018, 11, 27), 'time': time(9, 30), 'note': ''}
+
+
+def test_end_prev():
+    assert parsed_items_prev[0]['end'] == {'date': date(2018, 11, 27), 'time': None, 'note': ''}
+
+
+def test_id_prev():
+    assert parsed_items_prev[0]['id'] == \
+           'chi_board_elections/201811270930/x/electoral_board'
+
+
+def test_status_prev():
+    assert parsed_items_prev[0]['status'] == 'passed'
+
+
+def test_location_prev():
+    assert parsed_items_prev[0]['location'] == {
+        'address': '8th Floor Office, 69 W. Washington St.',
+        'name': 'Cook County Administration Building',
+        'neighborhood': '',
+    }
+
+
+def test_sources_prev():
+    assert parsed_items_prev[0]['sources'] == [{
+        'url': 'https://app.chicagoelections.com/pages/en/meeting-minutes-and-videos.aspx',
+        'note': ''
+    }]
+
+
+def test_documents_prev():
+    assert parsed_items_prev[4]['documents'] == \
+           [{'url':
+            'https://app.chicagoelections.com/documents/general/BoardMeetingMinutes-2018-10-30.pdf',
+             'note': 'Regular Board Meeting Agenda'},
+            {'note': 'Regular Board Meeting Video', 'url': 'https://youtu.be/AKFNigWEkc0'}
+            ]
+
+
+@pytest.mark.parametrize('item', parsed_items_prev)
+def test_all_day_prev(item):
+    assert item['all_day'] is False
+
+
+@pytest.mark.parametrize('item', parsed_items_prev)
+def test_classification_prev(item):
+    assert item['classification'] is COMMISSION
+
+
+@pytest.mark.parametrize('item', parsed_items_prev)
+def test__type_prev(item):
+    assert parsed_items_prev[0]['_type'] == 'event'
