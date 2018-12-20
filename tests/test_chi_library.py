729bb2a551a046758c821bd64528aecabae26ff0
Patrick Sier <pjsier@users.noreply.github.com>
2018-12-20 08:56:53 -0600

    Fix chi_library (#638)

--- tests/test_chi_library.py
+++ tests/test_chi_library.py
@@ -2,15 +2,17 @@ from datetime import date, time
 from unittest.mock import MagicMock
 
 import pytest
+from freezegun import freeze_time
 from tests.utils import file_response
 
-from city_scrapers.constants import BOARD, PASSED
+from city_scrapers.constants import BOARD, CONFIRMED
 from city_scrapers.spiders.chi_library import ChiLibrarySpider
 
+freezer = freeze_time('2018-12-20')
+freezer.start()
 session = MagicMock()
 res_mock = MagicMock()
 res_mock.status_code = 200
-res_mock.text = file_response('files/chi_library.json').text
 session.get.return_value = res_mock
 test_response = file_response(
     'files/chi_library.html',
@@ -19,6 +21,7 @@ test_response = file_response(
 )
 spider = ChiLibrarySpider(session=session)
 parsed_items = [item for item in spider.parse(test_response) if isinstance(item, dict)]
+freezer.stop()
 
 
 def test_name():
@@ -26,23 +29,20 @@ def test_name():
 
 
 def test_description():
-    assert parsed_items[0]['description'] == (
-        'There are no meetings in February, July and August. '
-        'Entry into these meetings is permitted at 8:45 a.m.'
-    )
+    assert parsed_items[0]['description'] == ''
 
 
 def test_start():
-    assert parsed_items[0]['start']['date'] == date(2017, 1, 17)
+    assert parsed_items[0]['start']['date'] == date(2019, 1, 15)
     assert parsed_items[0]['start']['time'] == time(9, 0)
 
 
 def test_id():
-    assert parsed_items[0]['id'] == ('chi_library/201701170900/x/board_of_directors')
+    assert parsed_items[0]['id'] == 'chi_library/201901150900/x/board_of_directors'
 
 
 def test_status():
-    assert parsed_items[0]['status'] == PASSED
+    assert parsed_items[0]['status'] == CONFIRMED
 
 
 def test_all_day():
@@ -51,14 +51,12 @@ def test_all_day():
 
 def test_location():
     assert parsed_items[0]['location'] == {
-        'address': '400 S. State Street, CHICAGO IL 60605',
+        'address': '400 S. State Street Chicago, IL',
         'coordinates': {
             'latitude': None,
             'longitude': None
         },
-        'name': ('Harold Washington Library Center, '
-                 'Multi-Purpose Room, Lower Level'),
-        'url': None
+        'name': 'Harold Washington Library Center',
     }
 
 
@@ -67,12 +65,12 @@ def test_documents():
         {
             'note': 'Agenda',
             'url':
-                'https://www.chipublib.org/news/board-of-directors-meeting-agenda-january-17-2017/'
+                'https://www.chipublib.org/news/board-of-directors-meeting-agenda-january-15-2019/'
         },
         {
             'note': 'Minutes',
             'url':
-                'https://www.chipublib.org/news/board-of-directors-meeting-minutes-january-17-2017/'
+                'https://www.chipublib.org/news/board-of-directors-meeting-minutes-january-15-2019/'
         }
     ]
 
