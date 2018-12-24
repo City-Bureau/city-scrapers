8546af3b197b31496476384d981f09bf1c88d76d
Zarif Mahmud <zarif.mahmud@outlook.com>
2018-12-23 20:51:36 -0500

    Started writing tests, and more working on first meeting

--- tests/test_chi_ssa_38.py
+++ tests/test_chi_ssa_38.py
@@ -1,4 +1,6 @@
 import pytest
+from datetime import date, time
+
 from freezegun import freeze_time
 from tests.utils import file_response
 
@@ -15,11 +17,6 @@ parsed_items = [item for item in spider.parse(test_response) if isinstance(item,
 freezer.stop()
 
 
-def test_tests():
-    print('Please write some tests for this spider or at least disable this one.')
-    assert False
-
-
 """
 Uncomment below
 """
@@ -28,16 +25,25 @@ Uncomment below
     # assert parsed_items[0]['name'] == 'EXPECTED NAME'
 
 
-# def test_description():
-    # assert parsed_items[0]['event_description'] == 'EXPECTED DESCRIPTION'
+def test_description():
+    assert parsed_items[0]['event_description'] == ''
 
 
-# def test_start():
-    # assert parsed_items[0]['start'] == {'date': None, 'time': None, 'note': 'EXPECTED DATE AND TIME'}
+def test_start():
+    # assert parsed_items[0]['start'] == {'date': date(2018, 9, 13), 'time': '', 'note': ''} # Fix year here
+    assert parsed_items[1]['start'] == {'date': date(2018, 9, 13), 'time': '', 'note': ''}
+
+# Confirmed that we get all the dates back
+# def test_all_start():
+#     listy = []
+#     for x in parsed_items:
+#         listy.append(x['start'])
+#     assert listy == ["Just for test"]
 
 
-# def test_end():
-    # assert parsed_items[0]['end'] == {'date': None, 'time': None, 'note': 'EXPECTED DATE AND TIME'}
+
+def test_end():
+    assert parsed_items[1]['end'] == {'date': date(2018, 9, 13), 'time': None, 'note': ''}
 
 
 # def test_id():
@@ -48,31 +54,30 @@ Uncomment below
     # assert parsed_items[0]['status'] == 'EXPECTED STATUS'
 
 
-# def test_location():
-    # assert parsed_items[0]['location'] == {
-        # 'neighborhood': 'EXPECTED URL',
-        # 'name': 'EXPECTED NAME',
-        # 'address': 'EXPECTED ADDRESS'
-    # }
+def test_location():
+    assert parsed_items[0]['location'] == {
+            'address': '4054 N. Lincoln Avenue',
+            'name': 'Northcenter Chamber of Commerce',
+            'neighborhood': '',
+        }
 
 
-# def test_sources():
-    # assert parsed_items[0]['sources'] == [{
-        # 'url': 'EXPECTED URL',
-        # 'note': 'EXPECTED NOTE'
-    # }]
+def test_sources():
+    assert parsed_items[0]['sources'] == [{
+                    'url': 'http://www.northcenterchamber.com/pages/MeetingsTransparency1',
+                    'note': ''
+                }]
+
+def test_documents():
+    assert parsed_items[1]['documents'] == [
+        {'url': 'https://chambermaster.blob.core.windows.net'
+                '/userfiles/UserFiles/chambers/1775/CMS/SSA38-Minutes-9132018.docx',
+         'note': 'SSA Commission Meeting Minutes'}]
 
 
-# def test_documents():
-    # assert parsed_items[0]['documents'] == [{
-      # 'url': 'EXPECTED URL',
-      # 'note': 'EXPECTED NOTE'
-    # }]
-
-
-# @pytest.mark.parametrize('item', parsed_items)
-# def test_all_day(item):
-    # assert item['all_day'] is False
+@pytest.mark.parametrize('item', parsed_items)
+def test_all_day(item):
+    assert item['all_day'] is False
 
 
 # @pytest.mark.parametrize('item', parsed_items)
