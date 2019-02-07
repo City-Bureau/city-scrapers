bd1d390443601ac60af3042f116f62e5d83a65be
pjsier <pjsier@gmail.com>
2019-02-07 07:09:50 -0600

    Fix cancellations, update Low Income Housing Trust

--- city_scrapers/spiders/chi_low_income_housing_trust_fund.py
+++ city_scrapers/spiders/chi_low_income_housing_trust_fund.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
 import re
 from datetime import datetime
 
 import scrapy
+from city_scrapers_core.constants import BOARD, COMMITTEE, NOT_CLASSIFIED
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
 
-from city_scrapers.constants import BOARD, COMMITTEE, NOT_CLASSIFIED
-from city_scrapers.spider import Spider
 
-
-class ChiLowIncomeHousingTrustFundSpider(Spider):
+class ChiLowIncomeHousingTrustFundSpider(CityScrapersSpider):
     name = 'chi_low_income_housing_trust_fund'
-    agency_name = 'Chicago Low-Income Housing Trust Fund'
+    agency = 'Chicago Low-Income Housing Trust Fund'
     timezone = 'America/Chicago'
     allowed_domains = ['www.chicagotrustfund.org']
     start_urls = ['http://www.chicagotrustfund.org/about-us/upcomingevents/']
@@ -18,16 +17,15 @@ class ChiLowIncomeHousingTrustFundSpider(Spider):
 
     def parse(self, response):
         """
-        `parse` should always `yield` a dict that follows the Event Schema
-        <https://city-bureau.github.io/city-scrapers/06_event_schema.html>.
+        `parse` should always `yield` Meeting items.
 
-        Change the `_parse_id`, `_parse_name`, etc methods to fit your scraping
+        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
         needs.
         """
         items = self._parse_calendar(response)
         for item in items:
             req = scrapy.Request(
-                item['sources'][0]['url'],
+                item['source'],
                 callback=self._parse_detail,
                 dont_filter=True,
             )
@@ -49,82 +47,64 @@ class ChiLowIncomeHousingTrustFundSpider(Spider):
     def _parse_calendar(self, response):
         """Parse items on the main calendar page"""
         items = []
-        for item in response.css('.day-with-date:not(.no-events)'):
-            name = self._parse_name(item)
+        for item in response.css('.day-with-date:not(.no-events), .current-day:not(.no-events)'):
+            title = self._parse_title(item)
             description = self._parse_description(item)
-            sources = self._parse_sources(item, response.url)
-            items.append({
-                '_type': 'event',
-                'name': name,
-                'event_description': description,
-                'classification': self._parse_classification(name),
-                'all_day': False,
-                'documents': [],
-                'sources': sources,
-            })
+            items.append(
+                Meeting(
+                    title=title,
+                    description=description,
+                    classification=self._parse_classification(title),
+                    all_day=False,
+                    links=[],
+                    source=self._parse_source(item, response.url),
+                )
+            )
         return items
 
     def _parse_detail(self, response):
         """Parse detail page for additional information"""
-        data = {
-            **response.meta.get('item', {}),
-            **self._parse_start_end_time(response),
-            'location': self._parse_location(response),
-        }
-        data['status'] = self._generate_status(data)
-        data['id'] = self._generate_id(data)
-        return data
+        meeting = response.meta.get('item', {})
+        meeting.update(self._parse_start_end_time(response))
+        meeting['location'] = self._parse_location(response)
+        meeting['status'] = self._get_status(meeting)
+        meeting['id'] = self._get_id(meeting)
+        return meeting
 
-    def _parse_name(self, item):
-        """
-        Parse or generate event name.
-        """
+    def _parse_title(self, item):
+        """Parse or generate event title"""
         return item.css('.event-title::text').extract_first()
 
     def _parse_description(self, item):
-        """
-        Parse or generate event description.
-        """
+        """Parse or generate event description"""
         return item.xpath('.//span[@class="event-content-break"]/following-sibling::text()'
                           ).extract_first()
 
-    def _parse_classification(self, name):
-        """
-        Parse or generate classification (e.g. board, committee, etc).
-        """
-        if 'board' in name.lower():
+    def _parse_classification(self, title):
+        """Parse or generate classification (e.g. board, committee, etc)"""
+        if 'board' in title.lower():
             return BOARD
-        if 'committee' in name.lower():
+        if 'committee' in title.lower():
             return COMMITTEE
         return NOT_CLASSIFIED
 
     def _parse_start_end_time(self, response):
-        """
-        Parse start date and time.
-        """
+        """Parse start and end datetimes"""
         time_str = response.css('.cc-panel .cc-block > span::text').extract_first()
         time_str = re.sub(r'\s+', ' ', time_str)
         date_str = re.search(r'(?<=day, ).*(?= fro)', time_str).group().strip()
         start_str = re.search(r'(?<=from ).*(?= to)', time_str).group().strip()
         end_str = re.search(r'(?<=to ).*(?= \w{3})', time_str).group().strip()
-        dt = datetime.strptime(date_str, '%B %d, %Y').date()
+        date_obj = datetime.strptime(date_str, '%B %d, %Y').date()
+        start_time = datetime.strptime(start_str, '%I:%M %p').time()
+        end_time = datetime.strptime(end_str, '%I:%M %p').time()
         return {
-            'start': {
-                'date': dt,
-                'time': datetime.strptime(start_str, '%I:%M %p').time(),
-                'note': '',
-            },
-            'end': {
-                'date': dt,
-                'time': datetime.strptime(end_str, '%I:%M %p').time(),
-                'note': '',
-            }
+            'start': datetime.combine(date_obj, start_time),
+            'end': datetime.combine(date_obj, end_time),
         }
 
     def _parse_location(self, response):
-        """
-        Parse or generate location.
-        """
+        """Parse or generate location"""
         addr_sel = response.css('.cc-panel .cc-block:nth-child(2) > span:nth-of-type(2)::text')
         if not addr_sel:
             addr_sel = response.css('#span_event_where_multiline p:first-of-type::text')
@@ -132,14 +112,11 @@ class ChiLowIncomeHousingTrustFundSpider(Spider):
         return {
             'address': ' '.join([re.sub(r'\s+', ' ', line).strip() for line in addr_lines]),
             'name': '',
-            'neighborhood': '',
         }
 
-    def _parse_sources(self, item, response_url):
-        """
-        Parse or generate sources.
-        """
+    def _parse_source(self, item, response_url):
+        """Parse or generate source"""
         item_link = item.css('.calnk > a::attr(href)').extract_first()
         if item_link:
-            return [{'url': item_link, 'note': ''}]
-        return [{'url': response_url, 'note': ''}]
+            return item_link
+        return response_url
