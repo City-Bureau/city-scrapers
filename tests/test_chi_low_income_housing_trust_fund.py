ed91b1594034669e1ce671ca980552bf979b5871
Patrick Sier <pjsier@users.noreply.github.com>
2019-02-07 07:15:33 -0600

    Fix cancellations, update Low Income Housing Trust (#682)

--- tests/test_chi_low_income_housing_trust_fund.py
+++ tests/test_chi_low_income_housing_trust_fund.py
@@ -1,10 +1,11 @@
-from datetime import date, time
+from datetime import datetime
+from os.path import dirname, join
 
 import pytest
+from city_scrapers_core.constants import BOARD, COMMITTEE, PASSED
+from city_scrapers_core.utils import file_response
 from freezegun import freeze_time
-from tests.utils import file_response
 
-from city_scrapers.constants import BOARD, COMMITTEE, PASSED
 from city_scrapers.spiders.chi_low_income_housing_trust_fund import (
     ChiLowIncomeHousingTrustFundSpider
 )
@@ -12,27 +13,29 @@ from city_scrapers.spiders.chi_low_income_housing_trust_fund import (
 freezer = freeze_time('2018-10-31')
 freezer.start()
 spider = ChiLowIncomeHousingTrustFundSpider()
-cal_res = file_response('files/chi_low_income_housing_trust_fund.html')
+cal_res = file_response(join(dirname(__file__), 'files', 'chi_low_income_housing_trust_fund.html'))
 parsed_items = []
 for item in spider._parse_calendar(cal_res):
-    detail_res = file_response('files/chi_low_income_housing_trust_fund_detail.html')
+    detail_res = file_response(
+        join(dirname(__file__), 'files', 'chi_low_income_housing_trust_fund_detail.html')
+    )
     detail_res.meta['item'] = item
     parsed_items.append(spider._parse_detail(detail_res))
 freezer.stop()
 
 
-def test_name():
-    assert parsed_items[0]['name'] == 'Finance Committee'
-    assert parsed_items[1]['name'] == 'Allocations Committee'
-    assert parsed_items[2]['name'] == 'Board Meeting'
+def test_title():
+    assert parsed_items[0]['title'] == 'Finance Committee'
+    assert parsed_items[1]['title'] == 'Allocations Committee'
+    assert parsed_items[2]['title'] == 'Board Meeting'
 
 
 def test_start():
-    assert parsed_items[0]['start'] == {'date': date(2018, 10, 4), 'time': time(10, 0), 'note': ''}
+    assert parsed_items[0]['start'] == datetime(2018, 10, 4, 10, 0)
 
 
 def test_end():
-    assert parsed_items[0]['end'] == {'date': date(2018, 10, 4), 'time': time(11, 0), 'note': ''}
+    assert parsed_items[0]['end'] == datetime(2018, 10, 4, 11, 0)
 
 
 def test_id():
@@ -51,7 +54,7 @@ def test_status():
 
 
 def test_description():
-    assert parsed_items[0]['event_description'] == (
+    assert parsed_items[0]['description'] == (
         'Meeting of the CLIHTF Finance Committee.  To attend, send Name and '
         'Planned Attendance Date to info@chicagotrustfund.org.  Regular '
         'Meeting Location:  Chicago City Hall, Rm. 1006c.'
@@ -62,25 +65,14 @@ def test_location():
     assert parsed_items[0]['location'] == {
         'address': '121 N. La Salle - Room 1006 Chicago, IL 60602',
         'name': '',
-        'neighborhood': '',
     }
 
 
 @pytest.mark.parametrize('item', parsed_items)
-def test_documents(item):
-    assert item['documents'] == []
+def test_links(item):
+    assert item['links'] == []
 
 
 @pytest.mark.parametrize('item', parsed_items)
 def test_all_day(item):
     assert item['all_day'] is False
-
-
-@pytest.mark.parametrize('item', parsed_items)
-def test_sources(item):
-    assert len(item['sources']) == 1
-
-
-@pytest.mark.parametrize('item', parsed_items)
-def test__type(item):
-    assert item['_type'] == 'event'
