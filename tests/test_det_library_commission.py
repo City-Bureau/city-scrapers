8a730ee23757cddfd41b8cafe4461d6b3fbc5483
pjsier <pjsier@gmail.com>
2019-02-24 13:55:19 -0600

    Fix det_library_commission, turn on throttling

--- tests/test_det_library_commission.py
+++ tests/test_det_library_commission.py
@@ -1,90 +1,74 @@
-from datetime import date, time
+from datetime import datetime
 
-import pytest
+import pytest  # noqa
+from city_scrapers_core.constants import COMMISSION, PASSED
 from freezegun import freeze_time
 from tests.utils import file_response
 
 from city_scrapers.spiders.det_library_commission import DetLibraryCommissionSpider
 
 test_response = file_response(
-    'files/det_library_commission.html', 'https://detroitpubliclibrary.org/about/commission'
+    'files/det_library_commission.html', 'https://detroitpubliclibrary.org/meeting/4908'
 )
 
 spider = DetLibraryCommissionSpider()
 
-
-def test_request_count():
-    requests = list(spider.parse(test_response))
-    assert len(requests) == 4
-
-
-test_detail = file_response(
-    'files/det_library_commission_detail.html', 'https://detroitpubliclibrary.org/meeting/1973'
-)
-
-freezer = freeze_time('2018-08-04 12:00:01')
+freezer = freeze_time('2019-02-24')
 freezer.start()
-parsed_items = [item for item in spider._parse_item(test_detail) if isinstance(item, dict)]
-parsed_items = sorted(parsed_items, key=lambda x: x['start']['date'])
-
+item = spider._parse_item(test_response)
 freezer.stop()
 
 
-def test_name():
-    assert parsed_items[0]['name'] == 'Library Commissioners: Regular Commission Meeting'
+def test_title():
+    assert item['title'] == 'Regular Commission Meeting'
 
 
 def test_description():
-    assert parsed_items[0]['event_description'] == ''
+    assert item['description'] == ''
 
 
 def test_start():
-    assert parsed_items[0]['start'] == {'date': date(2018, 9, 18), 'time': time(13, 30), 'note': ''}
+    assert item['start'] == datetime(2019, 1, 15, 13, 30)
 
 
 def test_end():
-    assert parsed_items[0]['end'] == {'date': date(2018, 9, 18), 'time': time(15, 30), 'note': ''}
+    assert item['end'] == datetime(2019, 1, 15, 14, 30)
+
+
+def test_time_notes():
+    assert item['time_notes'] == ''
 
 
 def test_id():
-    assert parsed_items[0][
-        'id'
-    ] == 'det_library_commission/201809181330/x/library_commissioners_regular_commission_meeting'
+    assert item['id'] == 'det_library_commission/201901151330/x/regular_commission_meeting'
 
 
 def test_status():
-    assert parsed_items[0]['status'] == 'tentative'
+    assert item['status'] == PASSED
 
 
 def test_location():
-    assert parsed_items[0]['location'] == {
-        'neighborhood': '',
-        'name': 'Explorers Room, Main Branch, Detroit Public Library',
-        'address': '5201 Woodward Ave. Detroit, Michigan 48202'
+    assert item['location'] == {
+        'name': 'Main',
+        'address': '5201 Woodward Ave. Detroit, Michigan 48202 U.S.'
     }
 
 
-def test_sources():
-    assert parsed_items[0]['sources'] == [{
-        'url': 'https://detroitpubliclibrary.org/meeting/1973',
-        'note': ''
+def test_source():
+    assert item['source'] == 'https://detroitpubliclibrary.org/meeting/4908'
+
+
+def test_links():
+    assert item['links'] == [{
+        'href':
+            'https://d2qp1eesgvzzix.cloudfront.net/uploads/files/commission/1-15-19-DETROIT-LIBRARY-COMMISSION-PROCEEDINGS.pdf?mtime=20190219160850',  # noqa
+        'title': 'DLC Minutes, January 15, 2019'
     }]
 
 
-def test_documents():
-    assert parsed_items[0]['documents'] == []
-
-
-@pytest.mark.parametrize('item', parsed_items)
-def test_all_day(item):
+def test_all_day():
     assert item['all_day'] is False
 
 
-@pytest.mark.parametrize('item', parsed_items)
-def test_classification(item):
-    assert item['classification'] == 'Commission'
-
-
-@pytest.mark.parametrize('item', parsed_items)
-def test__type(item):
-    assert parsed_items[0]['_type'] == 'event'
+def test_classification():
+    assert item['classification'] == COMMISSION
