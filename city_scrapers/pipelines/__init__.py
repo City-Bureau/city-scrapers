174bfa9b79ba6a8925398af451b474362d0ea2ea
Patrick Sier <pjsier@users.noreply.github.com>
2019-03-08 16:20:06 -0600

    Update city-scrapers-core to 0.2.3 (#701)

--- city_scrapers/pipelines/__init__.py
+++ city_scrapers/pipelines/__init__.py
@@ -1,8 +1,8 @@
-from .logging import CityScrapersLoggingPipeline
-from .travis import TravisValidationPipeline
 from .csv import CsvPipeline
 from .item import CityScrapersItemPipeline
+from .logging import CityScrapersLoggingPipeline
 from .migration import MigrationPipeline
+from .travis import TravisValidationPipeline
 
 __all__ = (
     'CityScrapersLoggingPipeline',
