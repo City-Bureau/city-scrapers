a22fbcf5bf5c9e1250a59131e18056a289e6ec23
Anphisa <kelimaneza@gmail.com>
2019-03-17 22:36:18 +0100

    Coordinates removed from location test.

--- tests/test_cook_medical_examiner.py
+++ tests/test_cook_medical_examiner.py
@@ -63,11 +63,7 @@ def test_status():
 def test_location():
     assert parsed_items[0]["location"] == {
         "name": "Cook County Medical Examiner’s Office",
-        "address": "2121 W Harrison; Chicago, IL 60612",
-        'coordinates': {
-            'latitude': None,
-            'longitude': None
-        }
+        "address": "2121 W Harrison; Chicago, IL 60612"
     }
 
 
