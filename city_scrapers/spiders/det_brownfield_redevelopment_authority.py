3e37ef8f509a60b22ac40669c66a6f21953cb156
pjsier <pjsier@gmail.com>
2019-03-22 14:07:36 -0500

    Fix det_brownfield

--- city_scrapers/spiders/det_brownfield_redevelopment_authority.py
+++ city_scrapers/spiders/det_brownfield_redevelopment_authority.py
@@ -1,153 +1,131 @@
-# -*- coding: utf-8 -*-
 import re
 from collections import defaultdict
 
+from city_scrapers_core.constants import ADVISORY_COMMITTEE, BOARD, COMMITTEE, FORUM, NOT_CLASSIFIED
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
 from dateutil.parser import parse
 
-from city_scrapers.constants import ADVISORY_COMMITTEE, BOARD, NOT_CLASSIFIED
-from city_scrapers.spider import Spider
 
-
-class DetBrownfieldRedevelopmentAuthoritySpider(Spider):
+class DetBrownfieldRedevelopmentAuthoritySpider(CityScrapersSpider):
     name = 'det_brownfield_redevelopment_authority'
-    agency_name = 'Detroit Brownfield Redevelopment Authority'
+    agency = 'Detroit Brownfield Redevelopment Authority'
     timezone = 'America/Detroit'
     allowed_domains = ['www.degc.org']
     start_urls = ['http://www.degc.org/public-authorities/dbra/']
+    location = {
+        'name': 'DEGC, Guardian Building',
+        'address': '500 Griswold St, Suite 2200, Detroit, MI 48226',
+    }
 
     def parse(self, response):
         """
-        `parse` should always `yield` a dict that follows the Event Schema
-        <https://city-bureau.github.io/city-scrapers/06_event_schema.html>.
+        `parse` should always `yield` Meeting items.
 
-        Change the `_parse_id`, `_parse_name`, etc methods to fit your scraping
+        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
         needs.
         """
-        yield from self._prev_meetings(response)
-        yield from self._next_meeting(response)
+        meeting_link_xpath = '//a[contains(., "Minutes")]'
+        self._validate_location(response)
+        meeting_links = response.xpath(meeting_link_xpath)
+        for a in meeting_links:
+            yield response.follow(a, callback=self._parse_meetings)
 
-    def _next_meeting(self, response):
-        # the two meetings seem to always be held back-to-back,
-        # so generate two meetings for each entry
-        next_meeting_xpath = (
-            '//p[contains(., "The next regularly scheduled DBRA meeting ")]//text()'
-        )
-        next_meeting_text = ' '.join(response.xpath(next_meeting_xpath).extract())
-        for i, meeting in enumerate(('Board of Directors', 'Community Advisory Committee')):
-            data = self._set_meeting_defaults(response, meeting)
-            data['start'] = self._parse_start(next_meeting_text, i)
-            if data['start'] is None:
-                continue
-            data['documents'] = []
-            data['status'] = self._generate_status(data)
-            data['id'] = self._generate_id(data)
-            yield data
+    def _parse_meetings(self, response):
+        meeting_link_map = self._parse_meeting_links(response)
+        for title, date_text in meeting_link_map.keys():
+            classification = self._parse_classification(title)
+            meeting = Meeting(
+                title=title,
+                description='',
+                classification=classification,
+                start=self._parse_start(date_text, classification),
+                end=None,
+                time_notes=self._parse_time_notes(classification),
+                all_day=False,
+                location=self.location,
+                links=meeting_link_map[(title, date_text)],
+                source=response.url,
+            )
+            # Check if there's a cancellation notice to help determine status
+            meeting['status'] = self._get_status(
+                meeting, text=' '.join([l['title'] for l in meeting_link_map[(title, date_text)]])
+            )
+            meeting['id'] = self._get_id(meeting)
+            yield meeting
 
-    def _prev_meetings(self, response):
-        prev_meetings_xpath = '//a[contains(., "Agendas, and Minutes")]'
-        prev_meetings = response.xpath(prev_meetings_xpath)
-        for a in prev_meetings:
-            yield response.follow(a, callback=self._parse_prev_meetings)
+    def _parse_start(self, dt, classification):
+        if classification == BOARD:
+            dt = dt.replace(hour=16)
+        elif classification == ADVISORY_COMMITTEE:
+            dt = dt.replace(hour=17)
+        return dt
 
-    def _parse_start(self, date_time_text, meeting_number):
-        time_match = self._parse_time(date_time_text)
-        date_match = self._parse_date(date_time_text)
-        if date_match is None:
-            return None
-        try:
-            dt = parse(date_match.group(1) + ' ' + time_match[meeting_number][0], fuzzy=True)
-            return {'date': dt.date(), 'time': dt.time(), 'note': ''}
-        except ValueError:
-            return None
-
-    def _parse_prev_meetings(self, response):
-        # there are only documents for prev meetings,
-        # so use these to create prev meetings
-        prev_meeting_docs = self._parse_prev_docs(response)
-        for meeting_name, meeting_date in prev_meeting_docs:
-            data = self._set_meeting_defaults(response, meeting_name)
-            data['start'] = {'date': meeting_date.date(), 'time': None, 'note': ''}
-            data['documents'] = prev_meeting_docs[(meeting_name, meeting_date)]
-            data['status'] = self._generate_status(data)
-            data['id'] = self._generate_id(data)
-            yield data
-
-    def _parse_prev_docs(self, response):
+    def _parse_meeting_links(self, response):
         docs = defaultdict(list)
-        links = response.css('li.linksGroup-item a').xpath('self::*[contains(span, "DBRA")]')
+        links = response.css('li.linksGroup-item a')
         for link in links:
             link_text = link.xpath("span/text()").extract_first('')
+            if not link_text:
+                continue
             is_date = self._parse_date(link_text)
             if is_date:
-                name = self._get_meeting_name(link_text, is_date.group(1))
-                if name is None:
+                title = self._parse_title(link_text)
+                if title is None:
                     continue
                 dt = parse(is_date.group(1), fuzzy=True)
                 document = self._create_document(link)
-                docs[(name, dt)].append(document)
+                docs[(title, dt)].append(document)
         return docs
 
     def _create_document(self, link):
         link_text = link.xpath('span/text()').extract_first('')
-        date = self._parse_date(link_text).group(1)
-        desc = link_text.split(date)[-1]
+        date_str = self._parse_date(link_text).group(1)
+        link_title = ' '.join(link_text.split(date_str))
         url = link.xpath("@href").extract_first('')
-        if 'AGENDA' in desc.upper():
-            return {'url': url, 'note': 'agenda'}
-        if 'MINUTES' in desc.upper():
-            return {'url': url, 'note': 'minutes'}
-        return {'url': url, 'note': desc.lower().strip()}
+        if 'AGENDA' in link_title.upper():
+            return {'url': url, 'title': 'Agenda'}
+        if 'MINUTES' in link_title.upper():
+            return {'url': url, 'title': 'Minutes'}
+        return {'href': url, 'title': re.sub(r'\s+', ' ', link_title).strip()}
 
     @staticmethod
     def _parse_date(text):
         date_regex = re.compile(r'([A-z]+ [0-3]?[0-9], \d{4})')
         return date_regex.search(text)
 
-    def _set_meeting_defaults(self, response, name):
-        classification = self._set_classification(name)
-        data = {
-            '_type': 'event',
-            'name': name,
-            'event_description': '',
-            'classification': classification,
-            'end': {
-                'date': None,
-                'time': None,
-                'note': ''
-            },
-            'all_day': False,
-            'location': {
-                'neighborhood': '',
-                'name': 'DEGC, Guardian Building',
-                'address': '500 Griswold, Suite 2200, Detroit'
-            },
-            'documents': [],
-            'sources': [{
-                'url': response.url,
-                'note': ''
-            }]
-        }
-        return data
+    @staticmethod
+    def _parse_time_notes(classification):
+        if classification not in [BOARD, ADVISORY_COMMITTEE]:
+            return 'See source for meeting time'
+        return ''
 
     @staticmethod
-    def _parse_time(date_time_text):
-        time_regex = re.compile(r'((1[012]|[1-9]):([0-5][0-9])\s?[ap].?m\.?)', flags=re.IGNORECASE)
-        time_text = time_regex.findall(date_time_text)
-        return time_text
-
-    @staticmethod
-    def _get_meeting_name(link_text, date_text):
-        name = link_text.split(date_text)[0].strip()
-        if name == 'DBRA':
-            return 'Board of Directors'
-        if name == 'DBRA CAC':
+    def _parse_title(link_text):
+        if 'Committee' in link_text:
+            return '{} Committee'.format(link_text.split(' Committee')[0]).replace('DBRA ', '')
+        elif 'Public Hearing' in link_text:
+            return '{} Public Hearing'.format(link_text.split(' Public Hearing')[0])
+        elif re.match(r'DBRA[\- ]CAC', link_text):
             return 'Community Advisory Committee'
+        elif 'DBRA' in link_text:
+            return 'Board of Directors'
         return None
 
     @staticmethod
-    def _set_classification(name):
-        if name.upper() == 'BOARD OF DIRECTORS':
+    def _parse_classification(title):
+        if title.upper() == 'BOARD OF DIRECTORS':
             return BOARD
-        if name.upper() == 'COMMUNITY ADVISORY COMMITTEE':
+        if title.upper() == 'COMMUNITY ADVISORY COMMITTEE':
             return ADVISORY_COMMITTEE
+        if 'COMMITTEE' in title.upper():
+            return COMMITTEE
+        if 'HEARING' in title.upper():
+            return FORUM
         return NOT_CLASSIFIED
+
+    @staticmethod
+    def _validate_location(response):
+        description = ' '.join(response.css('.content-itemContent *::text').extract())
+        if '500 Griswold' not in description:
+            raise ValueError('Meeting location no longer appears in description')
--- tests/test_det_brownfield_redevelopment_authority.py
+++ tests/test_det_brownfield_redevelopment_authority.py
@@ -1,8 +1,8 @@
-from datetime import date, time
+from datetime import datetime
 
 import pytest
-# Shared properties between two different page / meeting types
 import scrapy
+from city_scrapers_core.constants import BOARD, CANCELLED, PASSED
 from freezegun import freeze_time
 from tests.utils import file_response
 
@@ -10,184 +10,92 @@ from city_scrapers.spiders.det_brownfield_redevelopment_authority import (
     DetBrownfieldRedevelopmentAuthoritySpider
 )
 
-LOCATION = {
-    'neighborhood': '',
-    'name': 'DEGC, Guardian Building',
-    'address': '500 Griswold, Suite 2200, Detroit'
-}
-
-DBRA = 'Board of Directors'
-DBRA_CAC = 'Community Advisory Committee'
-
 test_response = file_response(
     'files/det_brownfield_redevelopment_authority.html',
     'http://www.degc.org/public-authorities/dbra/'
 )
-freezer = freeze_time('2018-07-28 12:00:01')
 spider = DetBrownfieldRedevelopmentAuthoritySpider()
-freezer.start()
-parsed_items = [item for item in spider._next_meeting(test_response) if isinstance(item, dict)]
-freezer.stop()
 
 
 def test_initial_request_count():
     items = list(spider.parse(test_response))
-    assert len(items) == 5
+    assert len(items) == 3
     urls = {r.url for r in items if isinstance(r, scrapy.Request)}
     assert urls == {
         'http://www.degc.org/public-authorities/dbra/fy-2018-2019-notices-agendas-and-minutes/',
         'http://www.degc.org/public-authorities/dbra/fy-2017-2018-meetings/',
         'http://www.degc.org/public-authorities/dbra/dbra-fy-2016-2017-meetings/'
     }
-    items = [i for i in items if isinstance(i, dict)]
-    assert len(items) == 2
 
 
-# current meeting http://www.degc.org/public-authorities/dbra/
-def test_name():
-    assert parsed_items[0]['name'] == DBRA
-    assert parsed_items[1]['name'] == DBRA_CAC
+test_meetings = file_response(
+    'files/det_brownfield_redevelopment_authority_meetings.html',
+    'http://www.degc.org/public-authorities/dbra/fy-2017-2018-meetings/'
+)
+freezer = freeze_time('2018-07-28 12:00:01')
+spider = DetBrownfieldRedevelopmentAuthoritySpider()
+freezer.start()
+
+parsed_items = [item for item in spider._parse_meetings(test_meetings)]
+parsed_items = sorted(parsed_items, key=lambda x: x['id'], reverse=True)
+freezer.stop()
+
+
+def test_meeting_count():
+    ids = {item['id'] for item in parsed_items}
+    assert len(parsed_items) == 51
+    assert len(ids) == 51
+
+
+def test_title():
+    assert parsed_items[0]['title'] == 'Board of Directors'
+    assert parsed_items[1]['title'] == 'Harmonie Social Club City Council Public Hearing'
 
 
 def test_description():
-    assert parsed_items[0]['event_description'] == ''
+    assert parsed_items[0]['description'] == ''
 
 
 def test_start():
-    assert parsed_items[0]['start'] == {'date': date(2018, 7, 25), 'time': time(16, 00), 'note': ''}
-    assert parsed_items[1]['start'] == {'date': date(2018, 7, 25), 'time': time(17, 00), 'note': ''}
+    assert parsed_items[0]['start'] == datetime(2019, 12, 19, 16)
+    assert parsed_items[1]['start'] == datetime(2019, 4, 11)
 
 
 def test_end():
-    assert parsed_items[0]['end'] == {'date': None, 'time': None, 'note': ''}
+    assert parsed_items[0]['end'] is None
 
 
 def test_id():
     assert parsed_items[0][
-        'id'] == 'det_brownfield_redevelopment_authority/201807251600/x/board_of_directors'
-    assert parsed_items[
-        1]['id'
-           ] == 'det_brownfield_redevelopment_authority/201807251700/x/community_advisory_committee'
+        'id'] == 'det_brownfield_redevelopment_authority/201912191600/x/board_of_directors'
 
 
 def test_status():
-    assert parsed_items[0]['status'] == 'passed'
+    assert parsed_items[0]['status'] == CANCELLED
+    assert parsed_items[-1]['status'] == PASSED
 
 
 def test_location():
-    assert parsed_items[0]['location'] == LOCATION
+    assert parsed_items[0]['location'] == spider.location
 
 
-def test_sources():
-    assert parsed_items[0]['sources'] == [{
-        'url': 'http://www.degc.org/public-authorities/dbra/',
-        'note': ''
+def test_source():
+    assert parsed_items[0]['source'
+                           ] == 'http://www.degc.org/public-authorities/dbra/fy-2017-2018-meetings/'
+
+
+def test_links():
+    assert parsed_items[0]['links'] == [{
+        'href': 'http://www.degc.org/wp-content/uploads/DBRA-121918-Meeting-Cancellation.pdf',
+        'title': 'DBRA Regular Meeting Cancellation Notice',
     }]
 
 
-def test_documents():
-    assert parsed_items[0]['documents'] == []
-
-
 def test_classification():
-    assert parsed_items[0]['classification'] == 'Board'
-    assert parsed_items[1]['classification'] == 'Advisory Committee'
+    assert parsed_items[0]['classification'] == BOARD
+    assert parsed_items[-1]['classification'] == BOARD
 
 
 @pytest.mark.parametrize('item', parsed_items)
 def test_all_day(item):
     assert item['all_day'] is False
-
-
-@pytest.mark.parametrize('item', parsed_items)
-def test__type(item):
-    assert item['_type'] == 'event'
-
-
-# # previous meetings e.g. http://www.degc.org/public-authorities/dbra/fy-2017-2018-meetings/
-test_prev_response = file_response(
-    'files/det_brownfield_redevelopment_authority_prev.html',
-    'http://www.degc.org/public-authorities/dbra/fy-2017-2018-meetings/'
-)
-parsed_prev_items = [
-    item for item in spider._parse_prev_meetings(test_prev_response) if isinstance(item, dict)
-]
-parsed_prev_items = sorted(parsed_prev_items, key=lambda x: x['id'], reverse=True)
-
-
-def test_request_count():
-    requests = list(spider._prev_meetings(test_response))
-    urls = {r.url for r in requests}
-    assert len(requests) == 3
-    assert urls == {
-        'http://www.degc.org/public-authorities/dbra/fy-2018-2019-notices-agendas-and-minutes/',
-        'http://www.degc.org/public-authorities/dbra/fy-2017-2018-meetings/',
-        'http://www.degc.org/public-authorities/dbra/dbra-fy-2016-2017-meetings/'
-    }
-
-
-def test_prev_meeting_count():
-    # 2017-2018 page (20 meetings)
-    ids = {item['id'] for item in parsed_prev_items}
-    assert len(parsed_prev_items) == 34
-    assert len(ids) == 34
-
-
-def test_prev_name():
-    assert parsed_prev_items[0]['name'] == DBRA_CAC
-
-
-def test_prev_description():
-    assert parsed_prev_items[0]['event_description'] == ''
-
-
-def test_prev_start():
-    assert parsed_prev_items[0]['start'] == {'date': date(2018, 6, 27), 'time': None, 'note': ''}
-
-
-def test_prev_end():
-    assert parsed_prev_items[0]['end'] == {'date': None, 'time': None, 'note': ''}
-
-
-def test_prev_id():
-    assert parsed_prev_items[0][
-        'id'
-    ] == 'det_brownfield_redevelopment_authority/201806270000/x/community_advisory_committee'  # noqa
-
-
-def test_prev_status():
-    assert parsed_prev_items[0]['status'] == 'passed'
-
-
-def test_prev_location():
-    assert parsed_prev_items[0]['location'] == LOCATION
-
-
-def test_prev_sources():
-    assert parsed_prev_items[0]['sources'] == [{
-        'url': 'http://www.degc.org/public-authorities/dbra/fy-2017-2018-meetings/',
-        'note': ''
-    }]
-
-
-def test_prev_documents():
-    assert parsed_prev_items[0]['documents'] == [{
-        'url':
-            'http://www.degc.org/wp-content/uploads/DBRACAC-062718-Regular-Meeting-Cancellation.pdf',  # noqa
-        'note': 'regular meeting cancellation notice',
-    }]
-
-
-def test_prev_classification():
-    assert parsed_prev_items[0]['classification'] == 'Advisory Committee'
-    assert parsed_prev_items[1]['classification'] == 'Board'
-
-
-@pytest.mark.parametrize('item', parsed_prev_items)
-def test_prev_all_day(item):
-    assert item['all_day'] is False
-
-
-@pytest.mark.parametrize('item', parsed_prev_items)
-def test__prev_type(item):
-    assert item['_type'] == 'event'
