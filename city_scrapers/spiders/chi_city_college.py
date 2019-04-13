bf0bd768ee4dd1a82726b688dd1337b2629914b2
Patrick Sier <pjsier@users.noreply.github.com>
2019-04-13 15:20:38 -0500

    Fix chi_city_college location (#714)

--- city_scrapers/spiders/chi_city_college.py
+++ city_scrapers/spiders/chi_city_college.py
@@ -1,25 +1,18 @@
-# -*- coding: utf-8 -*-
-"""
-All spiders should yield data shaped according to the Open Civic Data
-specification (http://docs.opencivicdata.org/en/latest/data/event.html).
-"""
 import re
-from datetime import datetime, timedelta
+from datetime import datetime
 
 import scrapy
+from city_scrapers_core.constants import BOARD, COMMITTEE
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
 
-from city_scrapers.constants import BOARD, COMMITTEE
-from city_scrapers.spider import Spider
 
-
-class ChiCityCollegeSpider(Spider):
+class ChiCityCollegeSpider(CityScrapersSpider):
     name = 'chi_city_college'
     agency = 'City Colleges of Chicago'
     allowed_domains = ['www.ccc.edu']
-
     start_urls = [
-        'http://www.ccc.edu/events/Pages/default.aspx?dept=Office%20of%20'
-        'the%20Board%20of%20Trustees'
+        'http://www.ccc.edu/events/Pages/default.aspx?dept=Office%20of%20the%20Board%20of%20Trustees',  # noqa
     ]
 
     def parse(self, response):
@@ -31,80 +24,49 @@ class ChiCityCollegeSpider(Spider):
         needs.
         """
         for link in response.css('.event-entry .event-title a::attr(href)').extract():
-            next_url = "http://www.ccc.edu" + link
-            yield scrapy.Request(next_url, callback=self.parse_event_page, dont_filter=True)
+            yield scrapy.Request(
+                response.urljoin(link), callback=self.parse_event_page, dont_filter=True
+            )
 
     def parse_event_page(self, response):
         date_str = response.css('#formatDateA::text').extract_first()
-        description = self._parse_description(response)
         for item in response.css('.page-content table tr'):
             start = self._parse_start(item, date_str)
-            name = self._parse_name(item)
-            data = {
-                '_type': 'event',
-                'name': name,
-                'event_description': description,
-                'classification': self._parse_classification(name),
-                'start': {
-                    'date': start.date(),
-                    'time': start.time(),
-                    'note': None,
-                },
-                'end': {
-                    'date': start.date(),
-                    'time': (start + timedelta(hours=2)).time(),
-                    'note': 'Estimated 2 hours after start time',
-                },
-                'all_day': False,
-                'location': self._parse_location(response),
-                'documents': self._parse_documents(response),
-                'sources': self._parse_sources(response)
-            }
-            data['id'] = self._generate_id(data)
-            data['status'] = self._generate_status(data)
-            yield data
+            title = self._parse_title(item)
+            meeting = Meeting(
+                title=self._parse_title(item),
+                description="",
+                classification=self._parse_classification(title),
+                start=start,
+                end=None,
+                time_notes="",
+                all_day=False,
+                location=self._parse_location(response),
+                links=self._parse_links(response),
+                source=response.url,
+            )
+            meeting['id'] = self._get_id(meeting)
+            meeting['status'] = self._get_status(meeting)
+            yield meeting
 
     def _parse_location(self, response):
-        """
-        Parse or generate location. Url, latitutde and longitude are all
-        optional and may be more trouble than they're worth to collect.
-        """
-        # Default to Harold Washington College
-        default_location = {
-            'name': 'Harold Washington College',
-            'address': '30 E. Lake Street Chicago, IL 60601',
-            'neighborhood': None,
+        """Parse or generate location"""
+        loc_parts = [
+            re.sub(r"\s+", " ", part).strip()
+            for part in response.css("#contact-info .right-col-content .content *::text").extract()
+            if part.strip()
+        ]
+        return {
+            "name": loc_parts[3],
+            "address": " ".join(loc_parts[4:]).replace(" ,", ",").strip(),
         }
-        text = response.css(
-            '#ctl00_PlaceHolderMain_FullDescription__'
-            'ControlWrapper_RichHtmlField span::text'
-        ).extract_first()
-        if not text:
-            return default_location
 
-        match = re.search(r'\.m\.,([^,]+),(.+)', text)
-        if match is not None:
-            return {
-                'name': match.group(1).strip(),
-                'address': match.group(2).strip().rstrip('.'),
-                'neighborhood': None,
-            }
-        else:
-            return default_location
-
-    def _parse_name(self, item):
-        """
-        Parse or generate event name.
-        """
-        name = item.css('th:nth-child(2) ::text, td:nth-child(2) ::text').extract_first()
-        if 'regular board meeting' in name.lower():
+    def _parse_title(self, item):
+        """Parse or generate event title."""
+        title = item.css('th:nth-child(2) ::text, td:nth-child(2) ::text').extract_first()
+        if 'regular board meeting' in title.lower():
             return 'Board of Trustees'
-        return name.replace('\u200b', '').strip()
-
-    def _parse_description(self, response):
-        content = response.css('.page-content > div:not([style="display:none"]) *::text')
-        description = (' '.join([el.extract().strip() for el in [content[0], content[-1]]])).strip()
-        return re.sub(r'\s+', ' ', description.replace('\u200b', '')).strip()
+        return title.replace('\u200b', '').strip()
 
     def _parse_start(self, item, date_str):
         """Parse start date and time from item and page date string"""
@@ -114,27 +76,21 @@ class ChiCityCollegeSpider(Spider):
         time_str = time_str.replace('noon', 'pm').replace('\u200b', '').strip()
         return datetime.strptime('{} {}'.format(date_str, time_str), '%m/%d/%Y %I:%M %p')
 
-    def _parse_classification(self, name):
-        if 'committee' in name.lower():
+    def _parse_classification(self, title):
+        if 'committee' in title.lower():
             return COMMITTEE
         return BOARD
 
-    def _parse_documents(self, response):
-        """Returns an array of documents if available"""
-        documents = []
-        document_links = response.xpath(
+    def _parse_links(self, response):
+        """Returns an array of links if available"""
+        links = []
+        link_els = response.xpath(
             "//div[contains(@class, 'right-col-block')]/h2[text() = 'Learn More']"
             "/following-sibling::*//a"
         )
-        for doc_link in document_links:
-            documents.append({
-                'url': response.urljoin(doc_link.attrib['href']),
-                'note': doc_link.xpath('./text()').extract_first(),
+        for link_el in link_els:
+            links.append({
+                'href': response.urljoin(link_el.attrib['href']),
+                'title': link_el.xpath('./text()').extract_first(),
             })
-        return documents
-
-    def _parse_sources(self, response):
-        """
-        Parse sources.
-        """
-        return [{'url': response.url, 'note': ''}]
+        return links
