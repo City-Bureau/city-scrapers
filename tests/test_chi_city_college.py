469396a463699cb76e0aa694d55920c9fcc9962a
pjsier <pjsier@gmail.com>
2019-04-13 15:14:53 -0500

    Fix chi_city_college location

--- tests/test_chi_city_college.py
+++ tests/test_chi_city_college.py
@@ -1,9 +1,9 @@
-from datetime import date, time
+from datetime import datetime
 
+from city_scrapers_core.constants import BOARD, COMMITTEE, TENTATIVE
 from freezegun import freeze_time
 from tests.utils import file_response
 
-from city_scrapers.constants import BOARD, COMMITTEE, TENTATIVE
 from city_scrapers.spiders.chi_city_college import ChiCityCollegeSpider
 
 freezer = freeze_time('2018-01-12')
@@ -13,25 +13,21 @@ test_response = file_response(
     'http://www.ccc.edu/events/Pages/March-2019-Board-and-Committee-Meetings.aspx'
 )
 spider = ChiCityCollegeSpider()
-parsed_items = [item for item in spider.parse_event_page(test_response) if isinstance(item, dict)]
+parsed_items = [item for item in spider.parse_event_page(test_response)]
 freezer.stop()
 
 
-def test_name():
-    assert parsed_items[0]['name'] == 'Committee on Finance and Administrative Services'
-    assert parsed_items[1]['name'] == 'Board of Trustees'
+def test_title():
+    assert parsed_items[0]['title'] == 'Committee on Finance and Administrative Services'
+    assert parsed_items[1]['title'] == 'Board of Trustees'
 
 
-def test_start_time():
-    assert parsed_items[0]['start']['time'] == time(12, 0)
-    assert parsed_items[0]['start']['date'] == date(2019, 2, 7)
-    assert parsed_items[0]['start']['note'] is None
+def test_start():
+    assert parsed_items[0]['start'] == datetime(2019, 2, 7, 12, 0)
 
 
-def test_end_time():
-    assert parsed_items[0]['end']['time'] == time(14, 0)
-    assert parsed_items[0]['end']['date'] == date(2019, 2, 7)
-    assert parsed_items[0]['end']['note'] == 'Estimated 2 hours after start time'
+def test_end():
+    assert parsed_items[0]['end'] is None
 
 
 def test_id():
@@ -55,20 +51,13 @@ def test_status():
 def test_location():
     assert parsed_items[0]['location'] == {
         'name': 'Harold Washington College',
-        'address': '30 E. Lake Street Chicago, IL 60601',
-        'neighborhood': None,
+        'address': '30 East Lake Street 11th Floor Chicago, IL 60601',
     }
 
 
 def test_description():
-    assert parsed_items[0][
-        'event_description'
-    ] == 'The Board of Trustees of Community College District No. 508, County of Cook and State of Illinois will hold the following meetings on Thursday, February 7, 2019 at Harold Washington College, 30 East Lake Street, 11th Floor, Room 1115, Chicago, Illinois 60601. Any individual planning to attend these meetings and who will need an accommodation under the Americans with Disabilities Act should notify the Board Office, at (312) 553-2515 or at requesttospeak@ccc.edu.'  # noqa
+    assert parsed_items[0]['description'] == ''
 
 
-def test_documents():
-    assert parsed_items[0]['documents'] == []
-
-
-def test__type():
-    assert parsed_items[0]['_type'] == 'event'
+def test_links():
+    assert parsed_items[0]['links'] == []
