b9f8bbb91965937adc52b57c77a825f9863b70f6
Patrick Sier <pjsier@users.noreply.github.com>
2019-04-15 16:49:02 -0500

    Add scraper chi_metro_pier_exposition (#720)

---
+++ city_scrapers/spiders/chi_metro_pier_exposition.py
@@ -0,0 +1,82 @@
+from datetime import datetime, time
+
+from city_scrapers_core.constants import BOARD, COMMITTEE
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+
+
+class ChiMetroPierExpositionSpider(CityScrapersSpider):
+    name = "chi_metro_pier_exposition"
+    agency = "Chicago Metropolitan Pier and Exposition Authority"
+    timezone = "America/Chicago"
+    allowed_domains = ["www.mpea.com"]
+    start_urls = ["http://www.mpea.com/mpea-board-members/"]
+    location = {
+        "name": "MPEA Corporate Center",
+        "address": "301 E Cermak Rd, Corporate Boardroom, 5th Floor, Chicago, IL 60616"
+    }
+
+    def parse(self, response):
+        """
+        `parse` should always `yield` Meeting items.
+
+        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
+        needs.
+        """
+        description = " ".join(response.css(".vc_col-sm-6 .wpb_wrapper p *::text").extract())
+        if "301 East Cermak" not in description:
+            raise ValueError("Meeting location has changed")
+        for item in response.css(".supsystic-table tr")[3:]:
+            title = self._parse_title(item)
+            classification = self._parse_classification(title)
+            meeting = Meeting(
+                title=title,
+                description="",
+                classification=classification,
+                start=self._parse_start(item, classification),
+                end=None,
+                all_day=False,
+                time_notes="Refer to notice for start time",
+                location=self.location,
+                links=self._parse_links(item),
+                source=response.url,
+            )
+
+            meeting["status"] = self._get_status(
+                meeting, text=item.css("td:nth-child(2)::text").extract_first() or ""
+            )
+            meeting["id"] = self._get_id(meeting)
+
+            yield meeting
+
+    def _parse_title(self, item):
+        """Parse or generate meeting title."""
+        title_str = item.css("td:nth-child(2)::text").extract_first()
+        if "committee" in title_str.lower():
+            return title_str.replace("Meeting", "").strip()
+        return "Board of Directors"
+
+    def _parse_classification(self, title):
+        """Parse or generate classification from allowed options."""
+        if "committee" in title.lower():
+            return COMMITTEE
+        return BOARD
+
+    def _parse_start(self, item, classification):
+        """Parse start datetime as a naive datetime object."""
+        date_str = item.css("td::text").extract_first().strip()
+        date_obj = datetime.strptime(date_str, "%B %d, %Y").date()
+        time_obj = time(9)
+        if classification == COMMITTEE:
+            time_obj = time(13, 30)
+        return datetime.combine(date_obj, time_obj)
+
+    def _parse_links(self, item):
+        """Parse or generate links."""
+        links = []
+        for link in item.css("a"):
+            links.append({
+                "href": link.attrib["href"],
+                "title": link.xpath("./text()").extract_first(),
+            })
+        return links
---
+++ tests/test_chi_metro_pier_exposition.py
@@ -0,0 +1,103 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest
+from city_scrapers_core.constants import BOARD, CANCELLED, COMMITTEE, PASSED
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.chi_metro_pier_exposition import ChiMetroPierExpositionSpider
+
+test_response = file_response(
+    join(dirname(__file__), "files", "chi_metro_pier_exposition.html"),
+    url="http://www.mpea.com/mpea-board-members/",
+)
+spider = ChiMetroPierExpositionSpider()
+
+freezer = freeze_time("2019-04-15")
+freezer.start()
+
+parsed_items = [item for item in spider.parse(test_response)]
+
+freezer.stop()
+
+
+def test_count():
+    assert len(parsed_items) == 17
+
+
+def test_title():
+    assert parsed_items[0]["title"] == "Board of Directors"
+    assert parsed_items[10]["title"] == "Audit Committee"
+
+
+def test_description():
+    assert parsed_items[0]["description"] == ""
+
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2018, 1, 23, 9)
+
+
+def test_end():
+    assert parsed_items[0]["end"] is None
+
+
+def test_time_notes():
+    assert parsed_items[0]["time_notes"] == "Refer to notice for start time"
+
+
+def test_id():
+    assert parsed_items[0]["id"] == "chi_metro_pier_exposition/201801230900/x/board_of_directors"
+
+
+def test_status():
+    assert parsed_items[0]["status"] == PASSED
+    assert parsed_items[1]["status"] == CANCELLED
+
+
+def test_location():
+    assert parsed_items[0]["location"] == spider.location
+
+
+def test_source():
+    assert parsed_items[0]["source"] == "http://www.mpea.com/mpea-board-members/"
+
+
+def test_links():
+    assert parsed_items[0]["links"] == [
+        {
+            "href": "http://www.mpea.com/wp-content/uploads/2017/12/January_2018_Board_Notice-1.pdf",  # noqa
+            "title": "Notice"
+        },
+        {
+            "href":
+                "http://www.mpea.com/wp-content/uploads/2018/01/MPEA_-Board_Agenda_January_23_2018.pdf",  # noqa
+            "title": "Agenda"
+        },
+        {
+            "href":
+                "http://www.mpea.com/wp-content/uploads/2018/03/MPEA_Board_Minutes_Regular_Session_January_23_2018_Final.pdf",  # noqa
+            "title": "Minutes"
+        },
+        {
+            "href":
+                "http://www.mpea.com/wp-content/uploads/2018/02/January-2018-Financials-Combined-Operating-Results.pdf",  # noqa
+            "title": "Financial Results"
+        },
+        {
+            "href":
+                "http://www.mpea.com/wp-content/uploads/2018/02/January-2018-Tax-Collections.pdf",
+            "title": "Tax Collections"
+        }
+    ]
+
+
+def test_classification():
+    assert parsed_items[0]["classification"] == BOARD
+    assert parsed_items[10]["classification"] == COMMITTEE
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_all_day(item):
+    assert item["all_day"] is False
