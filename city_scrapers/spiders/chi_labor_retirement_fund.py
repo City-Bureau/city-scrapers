a863812f1fa3347e854a694672999229e8dbc05c
sfmajors373 <sfmajors373@gmail.com>
2019-04-15 17:05:36 -0400

    WIP 0667 spider chi labor retirement fund (#686)
    * First attempt at writing the scraper for chi_labor_retirement_fund
    
    * Edited start date/time and classification
    
    * Adding comments where it is broken
    
    * Update chi_labor_retirement_fund

---
+++ city_scrapers/spiders/chi_labor_retirement_fund.py
@@ -0,0 +1,71 @@
+from datetime import datetime
+
+from city_scrapers_core.constants import BOARD
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+
+
+class ChiLaborRetirementFundSpider(CityScrapersSpider):
+    name = "chi_labor_retirement_fund"
+    agency = "Laborers' & Retirement Board Employees' Annuity & Benefit Fund"
+    timezone = "America/Chicago"
+    allowed_domains = ["www.labfchicago.org"]
+    start_urls = ["http://www.labfchicago.org/agendas-minutes"]
+    location = {
+        "address": "321 N Clark St, Chicago, IL",
+        "name": "Fund Office",
+    }
+
+    def parse(self, response):
+        """
+        `parse` should always `yield` Meeting items.
+
+        Change the `_parse_id`, `_parse_name`, etc methods to fit your scraping
+        needs.
+        """
+        description = " ".join(response.css(".mainRail .block p:nth-child(1) *::text").extract())
+        if "321 N" not in description:
+            raise ValueError("Meeting location has changed")
+        for item in response.css(".days"):
+            meeting = Meeting(
+                title=self._parse_title(item),
+                description="",
+                classification=BOARD,
+                start=self._parse_start(item),
+                end=None,
+                all_day=False,
+                time_notes="",
+                location=self.location,
+                links=self._parse_links(item, response),
+                source=response.url,
+            )
+
+            meeting["status"] = self._get_status(
+                meeting, text=item.css('li:nth-child(3) td::text').extract_first() or ""
+            )
+            meeting["id"] = self._get_id(meeting)
+
+            yield meeting
+
+    def _parse_title(self, item):
+        """Parse or generate meeting title."""
+        title = item.css('li:nth-child(3) td::text').extract_first()
+        if "special" in title.lower():
+            return "Special Meeting"
+        return "Retirement Board"
+
+    def _parse_start(self, item):
+        """Parse start datetime as a naive datetime object."""
+        cal_date = item.css('.calendar-day::text').extract_first()
+        start_time = item.css('li:nth-child(2)::text').extract_first()
+        return datetime.strptime("{} {}".format(cal_date, start_time.upper()), "%m/%d/%Y %I:%M %p")
+
+    def _parse_links(self, item, response):
+        """Parse or generate links."""
+        links = []
+        for link in item.css("li:last-child a"):
+            links.append({
+                "href": response.urljoin(link.attrib["href"]),
+                "title": link.xpath("./text()").extract_first(),
+            })
+        return links
---
+++ tests/test_chi_labor_retirement_fund.py
@@ -0,0 +1,82 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest
+from city_scrapers_core.constants import BOARD, CANCELLED, TENTATIVE
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.chi_labor_retirement_fund import ChiLaborRetirementFundSpider
+
+test_response = file_response(
+    join(dirname(__file__), "files", "chi_labor_retirement_fund.html"),
+    url="http://www.labfchicago.org/agendas-minutes",
+)
+spider = ChiLaborRetirementFundSpider()
+
+freezer = freeze_time("2019-02-10")
+freezer.start()
+
+parsed_items = [item for item in spider.parse(test_response)]
+
+freezer.stop()
+
+
+def test_count():
+    assert len(parsed_items) == 25
+
+
+def test_title():
+    assert parsed_items[0]["title"] == "Retirement Board"
+    assert parsed_items[4]["title"] == "Special Meeting"
+
+
+def test_description():
+    assert parsed_items[0]["description"] == ""
+
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2019, 12, 17, 9)
+
+
+def test_end():
+    assert parsed_items[0]["end"] is None
+
+
+def test_time_notes():
+    assert parsed_items[0]["time_notes"] == ""
+
+
+def test_id():
+    assert parsed_items[0]["id"] == "chi_labor_retirement_fund/201912170900/x/retirement_board"
+
+
+def test_status():
+    assert parsed_items[16]["status"] == CANCELLED
+    assert parsed_items[0]["status"] == TENTATIVE
+
+
+def test_location():
+    assert parsed_items[0]["location"] == spider.location
+
+
+def test_source():
+    assert parsed_items[0]["source"] == "http://www.labfchicago.org/agendas-minutes"
+
+
+def test_links():
+    assert parsed_items[0]["links"] == []
+    assert parsed_items[13]["links"] == [{
+        "href":
+            "http://www.labfchicago.org/assets/1/33/Special_Meeting_Agenda_2019.02.06_to_post.pdf",
+        "title": "Agenda"
+    }]
+
+
+def test_classification():
+    assert parsed_items[0]["classification"] == BOARD
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_all_day(item):
+    assert item["all_day"] is False
