1942cbf2a471653485e815281e9c003093ca0101
Chantel Miller <cmiller@civisanalytics.com>
2019-04-16 17:40:23 -0500

    INIT: barebones spider

---
+++ city_scrapers/spiders/il_opioid_response.py
@@ -0,0 +1,105 @@
+from datetime import datetime, time
+
+from city_scrapers_core.constants import ADVISORY_COMMITTEE
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+
+
+class IlOpioidResponseSpider(CityScrapersSpider):
+    name = "il_opioid_response"
+    agency = "Illinois Opioid Crisis Response Advisory Council"
+    timezone = "America/Chicago"
+    allowed_domains = ["www.dhs.state.il.us"]
+    start_urls = ["https://www.dhs.state.il.us/page.aspx?item=104682"]
+
+    def parse(self, response):
+        """
+        `parse` should always `yield` Meeting items.
+
+        Change the `_parse_id`, `_parse_name`, etc methods to fit your scraping
+        needs.
+        """
+
+        for item in response.xpath('//p//a'):
+            meeting = Meeting(
+                title=self._parse_title(item),
+                description=self._parse_description(item),
+                classification=self._parse_classification(item),
+                start=self._parse_start(item),
+                end=self._parse_end(item),
+                all_day=self._parse_all_day(item),
+                time_notes=self._parse_time_notes(item),
+                location=self._parse_location(item),
+                links=self._parse_links(item),
+                source=self._parse_source(response),
+            )
+
+            # meeting["status"] = self._get_status(meeting)
+            # meeting["id"] = self._get_id(meeting)
+
+            yield meeting
+
+    def _parse_title(self, item):
+        """Parse or generate meeting title."""
+        return ""
+
+    def _parse_description(self, item):
+        """Parse or generate meeting description."""
+        return ""
+
+    def _parse_classification(self, item):
+        """Parse or generate classification from allowed options."""
+        return ADVISORY_COMMITTEE
+
+    def _parse_start(self, item):
+        """Parse start datetime as a naive datetime object."""
+        text_array = item.xpath('text()').get().split()
+        try: # MISSING A SPACE ON ONE OF ENTRIES
+            start_date = datetime.strptime(text_array[1], '%m.%d.%y')
+            return datetime.combine(start_date, time(13)) # static start time
+        except ValueError:
+            return None
+
+    def _parse_end(self, item):
+        """Parse end datetime as a naive datetime object. Added by pipeline if None"""
+        text_array = item.xpath('text()').get().split()
+        try:
+            end_date = datetime.strptime(text_array[1], '%m.%d.%y')
+            return datetime.combine(end_date, time(15)) # static end time
+        except ValueError:
+            return None
+
+    def _parse_time_notes(self, item):
+        """Parse any additional notes on the timing of the meeting"""
+        return ""
+
+    def _parse_all_day(self, item):
+        """Parse or generate all-day status. Defaults to False."""
+        return False
+
+    def _parse_location(self, item): # list of dicts instead of dict? 
+        """Parse or generate location."""
+        return [
+            {
+                "address": "401 S. Clinton Street, 7th Floor Executive Conference Room, Chicago, IL 60607",
+                "name": "Illinois Department of Human Services Clinton Building",
+            },
+            {
+                "address": "100 S. Grand Avenue East, 3rd Floor Executive Video Conference Room, Springfield, IL 62762",
+                "name": "Illinois Department of Human Services Harris Building",
+            },
+        ]
+
+    def _parse_links(self, item):
+        """Parse or generate links."""
+        # differentiate meeting agenda vs minutes
+        return [
+            {
+                "href": item.xpath('@href').get(),
+                "title": item.xpath('text()').get()
+            }
+        ]
+
+    def _parse_source(self, response):
+        """Parse or generate source."""
+        return response.url
