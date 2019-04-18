8f7b8235bf463c011d98e36c6d5e97c9ffbf4933
pjsier <pjsier@gmail.com>
2019-04-17 21:45:22 -0500

    Add cook_pension

---
+++ city_scrapers/spiders/cook_pension.py
@@ -0,0 +1,84 @@
+import re
+from datetime import datetime, time
+
+from city_scrapers_core.constants import BOARD, COMMITTEE
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+
+
+class CookPensionSpider(CityScrapersSpider):
+    name = "cook_pension"
+    agency = "Cook County Pension Fund"
+    timezone = "America/Chicago"
+    allowed_domains = ["www.cookcountypension.com"]
+    start_urls = ["https://www.cookcountypension.com/agendaminutes/"]
+    location = {
+        "name": "Cook County Pension Fund Office",
+        "address": "70 W Madison St, Suite 1925, Chicago, IL 60602",
+    }
+
+    def parse(self, response):
+        """
+        `parse` should always `yield` Meeting items.
+
+        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
+        needs.
+        """
+        # Create a mapping of IDs to meetings to only return unique meetings and group links
+        meeting_id_map = {}
+        for item in response.css(".datatbl tr")[1:]:
+            title = self._parse_title(item)
+            meeting = Meeting(
+                title=title,
+                description="",
+                classification=self._parse_classification(title),
+                start=self._parse_start(item, title),
+                end=None,
+                all_day=False,
+                time_notes="See agenda to confirm times",
+                location=self.location,
+                links=self._parse_links(item, response),
+                source=response.url,
+            )
+
+            meeting["status"] = self._get_status(meeting)
+            meeting["id"] = self._get_id(meeting)
+            # Add newly-found links to meeting if exists, otherwise add to dictionary
+            if meeting["id"] in meeting_id_map:
+                meeting_id_map[meeting["id"]]["links"].extend(meeting["links"])
+            else:
+                meeting_id_map[meeting["id"]] = meeting
+
+        # Yield each unique meeting
+        for meeting in meeting_id_map.values():
+            yield meeting
+
+    def _parse_title(self, item):
+        """Parse or generate meeting title."""
+        title_str = " ".join(item.css("td:nth-child(3)::text").extract()).strip()
+        return re.sub(r"[\d\.]", "", title_str).strip()
+
+    def _parse_classification(self, title):
+        """Parse or generate classification from allowed options."""
+        if "board" in title.lower():
+            return BOARD
+        return COMMITTEE
+
+    def _parse_start(self, item, title):
+        """Parse start datetime as a naive datetime object."""
+        time_obj = time(9, 30)
+        if "legislative" in title.lower():
+            time_obj = time(8, 30)
+        date_str = " ".join(item.css("td:nth-child(1) *::text").extract()).strip()
+        date_obj = datetime.strptime(date_str, "%m/%d/%Y").date()
+        return datetime.combine(date_obj, time_obj)
+
+    def _parse_links(self, item, response):
+        """Parse or generate links."""
+        links = []
+        for link in item.css("a"):
+            links.append({
+                "href": response.urljoin(link.attrib["href"]),
+                "title": " ".join(link.css("*::text").extract()).strip(),
+            })
+        return links
---
+++ tests/test_cook_pension.py
@@ -0,0 +1,83 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest
+from city_scrapers_core.constants import BOARD, COMMITTEE, PASSED
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.cook_pension import CookPensionSpider
+
+test_response = file_response(
+    join(dirname(__file__), "files", "cook_pension.html"),
+    url="https://www.cookcountypension.com/agendaminutes/",
+)
+spider = CookPensionSpider()
+
+freezer = freeze_time("2019-04-17")
+freezer.start()
+
+parsed_items = [item for item in spider.parse(test_response)]
+
+freezer.stop()
+
+
+def test_count():
+    assert len(parsed_items) == 8
+
+
+def test_title():
+    assert parsed_items[0]["title"] == "Retirement Board"
+    assert parsed_items[1]["title"] == "Health Benefits Committee"
+
+
+def test_description():
+    assert parsed_items[0]["description"] == ""
+
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2019, 4, 4, 9, 30)
+
+
+def test_end():
+    assert parsed_items[0]["end"] is None
+
+
+def test_time_notes():
+    assert parsed_items[0]["time_notes"] == "See agenda to confirm times"
+
+
+def test_id():
+    assert parsed_items[0]["id"] == "cook_pension/201904040930/x/retirement_board"
+
+
+def test_status():
+    assert parsed_items[0]["status"] == PASSED
+
+
+def test_location():
+    assert parsed_items[0]["location"] == spider.location
+
+
+def test_source():
+    assert parsed_items[0]["source"] == "https://www.cookcountypension.com/agendaminutes/"
+
+
+def test_links():
+    assert parsed_items[3]["links"] == [{
+        'href': 'https://www.cookcountypension.com/assets/1/6/030719_Board_Agenda.pdf',
+        'title': 'Agenda'
+    }, {
+        'href': 'https://www.cookcountypension.com/assets/1/6/Board_Minutes_030719.pdf',
+        'title': 'Minutes'
+    }]
+
+
+def test_classification():
+    assert parsed_items[0]["classification"] == BOARD
+    assert parsed_items[1]["classification"] == COMMITTEE
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_all_day(item):
+    assert item["all_day"] is False
