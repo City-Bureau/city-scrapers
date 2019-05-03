0730fe7c0b65f4c83078c8063f657c4e24993d46
Patrick Sier <pjsier@users.noreply.github.com>
2019-05-03 17:17:44 -0400

    Fix det mixin, archive legistar sources (#739)

--- city_scrapers/middleware.py
+++ city_scrapers/middleware.py
@@ -6,7 +6,11 @@ class CityScrapersWaybackMiddleware(WaybackMiddleware):
     def get_item_urls(self, item):
         MAX_LINKS = 3
         if isinstance(item, Meeting):
-            return [link.get("href") for link in item.get("links", [])][:MAX_LINKS]
+            links = []
+            if "legistar" in item["source"] and "Calendar.aspx" not in item["source"]:
+                links = [item["source"]]
+            links.extend([link.get("href") for link in item.get("links", [])][:MAX_LINKS])
+            return links
         if isinstance(item, dict):
             return [doc.get("url") for doc in item.get("documents", [])][:MAX_LINKS]
         return []
