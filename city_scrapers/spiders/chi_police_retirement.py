57796e6cb1b44df27bf0c380b2c0326b646b722e
Matt Parrots <pairitma@mail.gvsu.edu>
2019-05-05 19:08:37 -0500

    Updated to Meeting object, adjusted to site layout change, finished link parsing

--- city_scrapers/spiders/chi_police_retirement.py
+++ city_scrapers/spiders/chi_police_retirement.py
@@ -1,19 +1,14 @@
-from city_scrapers_core.constants import NOT_CLASSIFIED
+from city_scrapers_core.constants import BOARD
 from city_scrapers_core.items import Meeting
 from city_scrapers_core.spiders import CityScrapersSpider
 
 import re
+import sys
 from datetime import datetime
 from dateutil.parser import parse
 
-def clean_escape_chars(s, space=False):
-    d_tab = s.replace('\t', '')
-    d_newl = d_tab.replace('\n', '')
-    if not space:
-        clean_s = d_newl.replace('\r', '')
-    else:
-        clean_s = d_newl.replace('\r', ' ')
-    return clean_s
+
+
 
 class ChiPoliceRetirementSpider(CityScrapersSpider):
     name = "chi_police_retirement"
@@ -22,7 +17,8 @@ class ChiPoliceRetirementSpider(CityScrapersSpider):
     allowed_domains = ["www.chipabf.org"]
     start_urls = ["http://www.chipabf.org/ChicagoPolicePension/MonthlyMeetings.html"]
     TAG_RE = re.compile(r'<[^>]+>')
-    NOTES_RE = re.compile(r'^.*\-(.\D*).*$')
+
+    
 
     def parse(self, response):
         """
@@ -32,99 +28,117 @@ class ChiPoliceRetirementSpider(CityScrapersSpider):
         needs.
         """
 
-        data = {
-            '_type':'event',
-            'name':'Board Meetings of The Policemen\'s Annuity & Benefit Fund',
-            'event_description': self._parse_description(response),
-            'classification': 'BOARD',
-            'end': {
-                'date': None,
-                'time': None,
-                'note': ''
-            },
-            'all_day': False,
-            #http://www.chipabf.org/ChicagoPolicePension/aboutus.html
-            'event_location':'221 North LaSalle Street, Suite 1626, Chicago, Illinois 60601-1203',
-            'sources': [{
-                'url': response.url,
-                'note': ''
-            }]
-        }
         year = self._parse_year(response)
-
         date_table = response.xpath('//*[@id="content0"]/div[3]/table')
         date_items = date_table.extract()[0].split('<tr>')
+        
+        skip_first_item = True
         for item in date_items:
-            print(item)
-            start_date, start_time, notes = self._parse_start(item, year)
-            new_item = {
-                'start': {
-                    'date': start_date,
-                    'time': start_time,
-                    'note': notes,
-                },
-                'documents': self._parse_documents(item),
-            }
-            new_item.update(data)
-            new_item['id'] = self._generate_id(new_item)
-            new_item['status'] = self._generate_status(new_item)
-            yield new_item
-        # for item in response.css(".meetings"):
-        #     meeting = Meeting(
-        #         title=self._parse_title(item),
-        #         description=self._parse_description(item),
-        #         classification=self._parse_classification(item),
-        #         start=self._parse_start(item),
-        #         end=self._parse_end(item),
-        #         all_day=self._parse_all_day(item),
-        #         time_notes=self._parse_time_notes(item),
-        #         location=self._parse_location(item),
-        #         links=self._parse_links(item),
-        #         source=self._parse_source(response),
-        #     )
+            if skip_first_item:
+                skip_first_item = False
+                continue
 
-        #     meeting["status"] = self._get_status(meeting)
-        #     meeting["id"] = self._get_id(meeting)
+            meeting = Meeting(
+                title=self._parse_title(response),
+                description=self._parse_description(response),
+                classification=self._parse_classification(response),
+                start=self._parse_start(item, year),
+                time_notes=self._parse_time_notes(),
+                end=self._parse_end(),
+                all_day=False,
+                location=self._parse_location(response),
+                source=self._parse_source(response),
+                links=self._parse_links(item)
+            )
+
+            # replace before this
+            meeting["status"] = self._get_status(meeting)
+            meeting["id"] = self._get_id(meeting)
 
             yield meeting
 
-    def _parse_description(self, item):
+    def _parse_title(self, item):
+        """Parse or generate meeting title."""
+        return "Board Meetings of The Policemen\'s Annuity & Benefit Fund"
+
+    def _parse_description(self, response):
         """Parse or generate meeting description."""
-        raw_description = ''.join(item.xpath('//*[@id="content0"]/p/text()').extract())
-        clean_description = clean_escape_chars(raw_description)
+        raw_description = ''.join(response.xpath('//*[@id="content0"]/p/text()').extract())
+        clean_description = self._clean_escape_chars(raw_description)
         return ' '.join(clean_description.split())
 
+    def _parse_classification(self, item):
+        """Parse or generate classification from allowed options."""
+        return BOARD
+
+    def _parse_start(self, item, year):
+        start = self._get_date_string(item, year)
+        return datetime.strptime(start, '%B %d %Y %I%p')
+        
+    def _parse_time_notes(self):
+        return None
+
+    def _parse_end(self):
+        return None
+
+        # see here for address: http://www.chipabf.org/ChicagoPolicePension/aboutus.html
+    def _parse_location(self, item):
+        """Parse or generate location."""
+        return {
+            "address":
+                "221 North LaSalle Street, Suite 1626,"
+                " Chicago, Illinois 60601-1203",
+            "name": "Policemen's Annuity and Benefit Fund",
+        }
+
+    def _parse_source(self, response):
+        """Parse or generate source."""
+        return response.url
+
+    def _parse_links(self, item):
+        links = []
+        raw_links = item.split('a href')
+        if len(raw_links) > 1:
+            raw_agenda = raw_links[1]
+            file_path = re.search(r'\"(.+?)\"', raw_agenda).group(1)
+            title = re.search(r'\>(.+?)\<', self._clean_escape_chars(raw_agenda)).group(1).strip()
+            agenda = {
+                "href": 'https://' + self.allowed_domains[0] + '/ChicagoPolicePension/' + file_path,
+                "title": title
+            }
+            links.append(agenda)
+
+        if len(raw_links) > 2:
+            raw_minutes = raw_links[2]
+            file_path = re.search(r'\"(.+?)\"', raw_minutes).group(1)
+            title = re.search(r'\>(.+?)\<', raw_minutes).group(1).strip()
+            minutes = {
+                "href": self.allowed_domains[0] + '/ChicagoPolicePension/' + file_path,
+                "title": title
+            }
+            links.append(minutes)
+        return links
+    
     def _parse_year(self, item):
         return item.xpath('//*[@id="content0"]/div[3]/h2[1]/text()').extract()[0][:4]
 
-    def _parse_start(self, item, year):
-        """Parse start datetime as a naive datetime object.
+    def _clean_html_tags(self, item):
+        return self.TAG_RE.sub('', item).strip()
 
-            return start date, start time, and notes
-        """
-
-        # strips table tags and escape chars
-        date_string = clean_escape_chars(self.TAG_RE.sub('', item).strip(), True)
-
-        # dash in the month indicates a note
-        if '-' in date_string:
-            note = self._parse_time_notes(date_string)
+    def _clean_escape_chars(self, s, space=False):
+        d_tab = s.replace('\t', '')
+        d_newl = d_tab.replace('\n', '')
+        if not space:
+            clean_s = d_newl.replace('\r', '')
         else:
-            note = ''
-        print(date_string)
-        dt = parse(date_string)
-        start_time = dt.strftime('%R')
-        start_date = dt.strftime('%B %d')
+            clean_s = d_newl.replace('\r', ' ')
+        return clean_s
 
-        return start_date, start_time, note
-
-    def _parse_time_notes(self, item):
-        """Parse any additional notes on the timing of the meeting"""
-        note = self.NOTES_RE.search(item)
-        return note.group(1)
-
-    def _parse_links(self, item):
-        """Parse or generate links."""
-        return [{"href": "", "title": ""}]
+    def _get_date_string(self, item, year):
+        no_tags = self._clean_html_tags(item)
+        date_pieces = self._clean_escape_chars(no_tags, True).split()
+        date_pieces[3] = ''.join([num for num in date_pieces[3] if num.isdigit()])
+        date = ' '.join(date_pieces[2:4]) + ' ' + year + ' ' + date_pieces[4]
+        return date
 
 
