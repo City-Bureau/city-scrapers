a013ba46e4aeb00129fc494a9980a6d96939143a
Patrick Sier <pjsier@users.noreply.github.com>
2019-05-15 13:06:27 -0500

    Update cook_hospitals, core version (#743)
    * Update cook_hospitals, core version
    
    * Add freezegun

--- tests/test_cook_hospitals.py
+++ tests/test_cook_hospitals.py
@@ -1,21 +1,35 @@
 from datetime import datetime
+from os.path import dirname, join
 
 import pytest
 from city_scrapers_core.constants import BOARD, COMMITTEE, PASSED
-from tests.utils import file_response
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
 
 from city_scrapers.spiders.cook_hospitals import CookHospitalsSpider
 
 test_response = file_response(
-    'files/cook_hospitals.html',
-    url='http://www.cookcountyhhs.org/about-cchhs/governance/board-committee-meetings/'
+    join(dirname(__file__), "files", "cook_hospitals.html"),
+    url=(
+        'https://cookcountyhealth.org/about/board-of-directors/board-committee-meetings-agendas-minutes/'  # noqa
+    )
 )
 spider = CookHospitalsSpider()
+
+freezer = freeze_time("2019-05-15")
+freezer.start()
+
 parsed_items = [item for item in spider.parse(test_response)]
+
+freezer.stop()
+
+
+def test_count():
+    assert len(parsed_items) == 105
 
 
 def test_title():
-    assert parsed_items[0]['title'] == 'Meetings of the Board of Directors'
+    assert parsed_items[0]['title'] == 'Board of Directors'
 
 
 def test_description():
@@ -23,7 +37,7 @@ def test_description():
 
 
 def test_start():
-    assert parsed_items[0]['start'] == datetime(2017, 1, 27, 9)
+    assert parsed_items[0]['start'] == datetime(2019, 1, 25, 9)
 
 
 def test_end():
@@ -31,28 +45,22 @@ def test_end():
 
 
 def test_time_notes():
-    assert parsed_items[0]['time_notes'] == ''
+    assert parsed_items[0]['time_notes'] == 'Confirm time in agenda'
 
 
 def test_links():
-    assert parsed_items[0]['links'] == [
-        {
-            'href':
-                'http://www.cookcountyhhs.org/wp-content/uploads/2016/01/01-27-17-Board-Agenda.pdf',
-            'title': 'Agenda and materials'
-        },
-        {
-            'href':
-                'http://www.cookcountyhhs.org/wp-content/uploads/2017/02/01-27-17-Board-scan-Minutes.pdf',  # noqa
-            'title': 'Minutes'
-        },
-    ]
-    assert parsed_items[-1]['links'] == []
+    assert parsed_items[0]['links'] == []
+    assert parsed_items[3]['links'] == [{
+        'href': 'https://cookcountyhealth.org/wp-content/uploads/03-29-19-Board-Agenda.pdf',
+        'title': 'Agenda'
+    }, {
+        'href': 'https://cookcountyhealth.org/wp-content/uploads/Metrics-combined-03-29-19.pdf',
+        'title': 'Metrics - HR, ACC, MCC, Finance and QPS Committees'
+    }]
 
 
 def test_id():
-    assert parsed_items[0]['id'
-                           ] == 'cook_hospitals/201701270900/x/meetings_of_the_board_of_directors'
+    assert parsed_items[0]['id'] == 'cook_hospitals/201901250900/x/board_of_directors'
 
 
 def test_status():
@@ -60,10 +68,7 @@ def test_status():
 
 
 def test_location():
-    assert parsed_items[0]['location'] == {
-        'name': '',
-        'address': '1900 W. Polk, Second Floor Conference Room, Chicago, Illinois',
-    }
+    assert parsed_items[0]['location'] == spider.location
 
 
 def test_classification():
@@ -78,5 +83,6 @@ def test_all_day(item):
 
 @pytest.mark.parametrize('item', parsed_items)
 def test_source(item):
-    assert item['source'
-                ] == 'http://www.cookcountyhhs.org/about-cchhs/governance/board-committee-meetings/'
+    assert item[
+        'source'
+    ] == 'https://cookcountyhealth.org/about/board-of-directors/board-committee-meetings-agendas-minutes/'  # noqa
