ef6d8a1bd0624decc3402d13572dcfdd6aa670a9
Joshua Groves <grovesjo@oregonstate.edu>
2019-05-28 20:13:02 -0400

    Added chi_ssa_73 spider

---
+++ city_scrapers/spiders/chi_ssa_73.py
@@ -0,0 +1,102 @@
+import re
+from datetime import datetime, time
+
+from city_scrapers_core.constants import BOARD
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+
+
+class ChiSsa73Spider(CityScrapersSpider):
+    name = "chi_ssa_73"
+    agency = "Chicago Special Service Area #73 Chinatown"
+    timezone = "America/Chicago"
+    allowed_domains = ["chinatownssa73.org"]
+    start_urls = ["https://chinatownssa73.org/meeting-schedule/"]
+
+
+    def parse(self, response):
+        """
+        `parse` should always `yield` Meeting items.
+
+        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
+        needs.
+        """
+
+        linksList = self._get_links(response)
+        location = self._parse_location(response)
+        for item in response.css("article p"):
+            start = self._parse_start(item)
+            if not start:
+                continue
+            meeting = Meeting(
+                title='SSA #73 Chinatown Board',
+                description='',
+                classification=BOARD,
+                start=start,
+                end=None,
+                all_day=False,
+                time_notes='',
+                location=location,
+                links=self._parse_links(item, start, linksList),
+                source=response.url,
+            )
+
+            meeting["status"] = self._get_status(meeting)
+            meeting["id"] = self._get_id(meeting)
+
+            yield meeting
+
+    def _parse_start(self, item):
+        """
+        Parse start date and time.
+        """
+        date_str = item.css('*::text').extract_first()
+        date_match = re.search(r'\w{3,9} \d{1,2}, \d{4}', date_str)
+        if date_match:
+            parsed_date = datetime.strptime(date_match.group(), '%B %d, %Y')
+            return datetime.combine(parsed_date.date(), time(18,30))
+
+    def _parse_location(self, response):
+        """
+        Parse or generate location.
+        """
+        if '1700 S. Wentworth' in response.text:
+            return {
+                'address': '1700 S. Wentworth Avenue, Chicago, Illinois',
+                'name': 'Leonard M. Louie Fieldhouse',
+            }
+        else:
+            raise ValueError('Meeting address has changed')
+
+    def _get_links(self, response):
+        linksList = []
+        for item in response.css('a'):
+            newDict = {}
+            addLink = False
+            if 'Agenda' in item.extract():
+                newDict['title'] = "Agenda"
+                addLink = True
+            elif 'Minutes' in item.extract():
+                newDict['title'] = "Minutes"
+                addLink = True
+            if addLink:
+                newDict['href'] = item.attrib['href']
+                rawRef = item.css('*::text').extract_first()
+                newDict['date'] = rawRef.split()[1]
+                linksList.append(newDict)
+        print(linksList)
+        return linksList
+
+
+    def _parse_links(self, item, start, linksList):
+        """Parse or generate links."""
+        resultList = []
+        targetStr1 = start.strftime('%m-%d-%Y').replace(' 0', ' ')
+        targetStr2 = start.strftime('%m-%d-%y').replace(' 0', ' ')
+        for item in linksList:
+            if item['date'] in targetStr1 or item['date'] in targetStr2:
+                newDict = {}
+                newDict["href"] = item["href"]
+                newDict["title"] = item["title"]
+                resultList.append(newDict)
+        return resultList
