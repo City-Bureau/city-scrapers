766f33d153ea24a4db51ea408f3a7eba06259632
pjsier <pjsier@gmail.com>
2019-06-04 09:47:51 -0500

    Add il_gaming_board spider

---
+++ city_scrapers/spiders/il_gaming_board.py
@@ -0,0 +1,83 @@
+import re
+from datetime import datetime, time
+
+from city_scrapers_core.constants import BOARD
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+
+
+class IlGamingBoardSpider(CityScrapersSpider):
+    name = "il_gaming_board"
+    agency = "Illinois Gaming Board"
+    timezone = "America/Chicago"
+    allowed_domains = ["www.igb.illinois.gov"]
+    start_urls = ["http://www.igb.illinois.gov/MeetingsMinutes.aspx"]
+    location = {
+        "name": "5th Floor, Michael A Bilandic Building",
+        "address": "160 N LaSalle St Chicago, IL 60601",
+    }
+
+    def parse(self, response):
+        """
+        `parse` should always `yield` Meeting items.
+
+        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
+        needs.
+        """
+        self._validate_location(response)
+
+        for item in response.css(".space"):
+            item_text = " ".join(item.css(".list *::text").extract()).strip()
+            meeting = Meeting(
+                title=self._parse_title(item_text),
+                description="",
+                classification=BOARD,
+                start=self._parse_start(item_text),
+                end=None,
+                all_day=False,
+                time_notes="See source to confirm meeting time",
+                location=self.location,
+                links=self._parse_links(item, response),
+                source=response.url,
+            )
+
+            meeting["status"] = self._get_status(meeting)
+            meeting["id"] = self._get_id(meeting)
+
+            yield meeting
+
+    def _parse_title(self, text):
+        """Parse or generate meeting title."""
+        if "special" in text.lower():
+            return "Riverboat/Video Gaming Special Meeting"
+        return "Riverboat/Video Gaming"
+
+    def _parse_start(self, text):
+        """Parse start datetime as a naive datetime object."""
+        date_str = re.sub(
+            r"\s+", " ",
+            re.search(r"[a-z]{3,10}\s+\d{1,2},?\s+\d{4}", text, flags=re.I).group()
+        ).replace(",", "")
+        date_obj = datetime.strptime(date_str, "%B %d %Y").date()
+        return datetime.combine(date_obj, time(9))
+
+    def _validate_location(self, response):
+        """Confirm that the location has not changed"""
+        description = " ".join(response.css(".contentBox > .line *::text").extract())
+        if not re.search(r"160\s+N(orth)?\s+LaSalle", description):
+            raise ValueError("Meeting location may have changed")
+
+    def _parse_links(self, item, response):
+        """Parse or generate links."""
+        links = []
+        for link_item in item.css("* + .line > .nestedlist > div:not(.clear):not(.hide)"):
+            item_type = link_item.css(".meetingLabel::text").extract_first().replace(":", "")
+            if "minutes" in item_type.lower():
+                item_type = "Minutes"
+            for link in link_item.css("a"):
+                links.append({
+                    "title": "{}: {}".format(item_type,
+                                             link.css("*::text").extract_first()),
+                    "href": response.urljoin(link.attrib["href"]),
+                })
+        return links
---
+++ tests/test_il_gaming_board.py
@@ -0,0 +1,87 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest
+from city_scrapers_core.constants import BOARD, PASSED
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.il_gaming_board import IlGamingBoardSpider
+
+test_response = file_response(
+    join(dirname(__file__), "files", "il_gaming_board.html"),
+    url="http://www.igb.illinois.gov/MeetingsMinutes.aspx",
+)
+spider = IlGamingBoardSpider()
+
+freezer = freeze_time("2019-06-04")
+freezer.start()
+
+parsed_items = [item for item in spider.parse(test_response)]
+
+freezer.stop()
+
+
+def test_count():
+    assert len(parsed_items) == 10
+
+
+def test_title():
+    assert parsed_items[0]["title"] == "Riverboat/Video Gaming"
+
+
+def test_description():
+    assert parsed_items[0]["description"] == ""
+
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2019, 1, 30, 9)
+
+
+def test_end():
+    assert parsed_items[0]["end"] is None
+
+
+def test_time_notes():
+    assert parsed_items[0]["time_notes"] == "See source to confirm meeting time"
+
+
+def test_id():
+    assert parsed_items[0]["id"] == "il_gaming_board/201901300900/x/riverboat_video_gaming"
+
+
+def test_status():
+    assert parsed_items[0]["status"] == PASSED
+
+
+def test_location():
+    assert parsed_items[0]["location"] == spider.location
+
+
+def test_source():
+    assert parsed_items[0]["source"] == "http://www.igb.illinois.gov/MeetingsMinutes.aspx"
+
+
+def test_links():
+    assert parsed_items[0]["links"] == [{
+        "href": "http://www.igb.illinois.gov/FilesBoardMeeting/20190130RiverboatAgenda.pdf",
+        "title": "Agenda: Riverboat"
+    }, {
+        "href": "http://www.igb.illinois.gov/FilesBoardMeeting/20190130RiverboatMinutes.pdf",
+        "title": "Minutes: Riverboat"
+    }, {
+        "href": "http://www.igb.illinois.gov/FilesBoardMeeting/20190130RiverboatAudio.mp3",
+        "title": "Audio: Riverboat"
+    }, {
+        "href": "http://www.igb.illinois.gov/FilesBoardMeeting/20190130VideoAudio.mp3",
+        "title": "Audio: Video Gaming"
+    }]
+
+
+def test_classification():
+    assert parsed_items[0]["classification"] == BOARD
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_all_day(item):
+    assert item["all_day"] is False
