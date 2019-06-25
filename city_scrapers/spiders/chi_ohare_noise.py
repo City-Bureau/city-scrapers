a5c9e859fe98a1ad0def61831a63ee39017611a0
George Dubuque <dubuqug@wwu.edu>
2019-06-24 19:32:08 -0700

    finished scraping agendas-minutes

--- city_scrapers/spiders/chi_ohare_noise.py
+++ city_scrapers/spiders/chi_ohare_noise.py
@@ -1,7 +1,8 @@
 from city_scrapers_core.constants import NOT_CLASSIFIED
 from city_scrapers_core.items import Meeting
 from city_scrapers_core.spiders import CityScrapersSpider
-
+import datetime
+import time
 
 class ChiOhareNoiseSpider(CityScrapersSpider):
     name = "chi_ohare_noise"
@@ -18,38 +19,30 @@ class ChiOhareNoiseSpider(CityScrapersSpider):
         Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
         needs.
         """
-
-
         if "agendas" in response.url:
 
             table_rows = response.selector.xpath(".//tbody//tr")
             for item in table_rows:
-                table_columns = item.xpath(".//td")
-                title = table_columns[1].xpath("./span/text()").get()
-                date = table_columns[2].xpath("./span/text()").get()
-                #print(title, date)
+
                 meeting = Meeting(
                     # Meeting title will always be second column
-                    # title=self._parse_title(table_columns[1]),
-                    # description=self._parse_description(item),
-                    # classification=self._parse_classification(item),
-                    # start=self._parse_start(item),
-                    # end=self._parse_end(item),
-                    # all_day=self._parse_all_day(item),
-                    # time_notes=self._parse_time_notes(item),
-                    # location=self._parse_location(item),
-                    # links=self._parse_links(item),
-                    # source=self._parse_source(response),
+                    title=self._parse_title(item),
+                    start=self._parse_start(item),
+                    links=self._parse_links(item),
+                    source=self._parse_source(response)
                 )
 
-                # meeting["status"] = self._get_status(meeting)
-                # meeting["id"] = self._get_id(meeting)
+                meeting["status"] = self._get_status(meeting)
+                meeting["id"] = self._get_id(meeting)
 
-                # yield meeting
+                yield meeting
         else:
             event_days = response.selector.xpath(".//td[@class='cal_dayshasevents']")
             for item in event_days:
-                date = item.xpath(".//a[@class='cal_daylink']//span[@class='listview']/text()").get().lstrip()
+                date = item. \
+                        xpath(".//a[@class='cal_daylink']//span[@class='listview']/text()"). \
+                        get(). \
+                        lstrip()
                 events = item.xpath(".//a[@class='cal_titlelink']")
                 for event in events:
 
@@ -59,9 +52,13 @@ class ChiOhareNoiseSpider(CityScrapersSpider):
                     print(name.split()[0].isupper())
                     print(time)
 
+    def convert_date(self, date_str):
+        date_str = datetime.strptime(date_str, "%b %d, %Y")
+        return date_str
+
     def _parse_title(self, item):
         """Parse or generate meeting title."""
-        return ""
+        return item.xpath(".//td[@class='djc_category']/span/text()").get()
 
     def _parse_description(self, item):
         """Parse or generate meeting description."""
@@ -73,7 +70,8 @@ class ChiOhareNoiseSpider(CityScrapersSpider):
 
     def _parse_start(self, item):
         """Parse start datetime as a naive datetime object."""
-        return None
+        date = item.xpath(".//td[@class='djc_producer']/span/text()").get()
+        return datetime.datetime.strptime(date, "%B %d, %Y")
 
     def _parse_end(self, item):
         """Parse end datetime as a naive datetime object. Added by pipeline if None"""
@@ -96,7 +94,18 @@ class ChiOhareNoiseSpider(CityScrapersSpider):
 
     def _parse_links(self, item):
         """Parse or generate links."""
-        return [{"href": "", "title": ""}]
+        agenda_minute = item.xpath(".//li[@class='djc_file']")
+        agenda_href = agenda_minute[0].xpath(".//a/@href").get()
+        minute_href = agenda_minute[1].xpath(".//a/@href").get()
+
+        links = []
+        if agenda_href is not None:
+            links.append({"href": agenda_href, "title": "Agenda"})
+
+        if minute_href is not None:
+            links.append({"href": minute_href, "title": "Minutes"})
+
+        return links
 
     def _parse_source(self, response):
         """Parse or generate source."""
