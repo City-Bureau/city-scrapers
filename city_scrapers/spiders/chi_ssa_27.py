2f60f12a206400377d618b61c769e922f7dc4986
nmschorr <12592910+nmschorr@users.noreply.github.com>
2019-07-10 13:11:07 -0700

    made more formatting changes.

--- city_scrapers/spiders/chi_ssa_27.py
+++ city_scrapers/spiders/chi_ssa_27.py
@@ -13,11 +13,8 @@ class ChiSsa27Spider(CityScrapersSpider):
     start_urls = ["https://www.lakeviewchamber.com/ssa27"]
 
     def parse(self, response):
-        """
-        `parse` should always `yield` Meeting items.
-        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping needs.
-        """
-        meeting, meeting_location = '', ''
+        """   `parse` should always `yield` Meeting items.
+        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping needs.   """
 
         for item in response.css("#content-232764 div.panel-body p"):
 
@@ -27,19 +24,20 @@ class ChiSsa27Spider(CityScrapersSpider):
 
             meeting = Meeting(
                 title=self._parse_title(item),
-                description=self._parse_description(),
+                description="",
                 classification=self._parse_classification(),
                 start=self._parse_start(item),
-                end=self._parse_end(),  # no indication of such
-                all_day=self._parse_all_day(),  # no indication of such
-                time_notes=self._parse_time_notes(),  # haven't seen any
+                end=None,          # no indication of such
+                all_day=False,     # no indication of
+                time_notes="",     # haven't seen any
                 location=meeting_location,
                 links=self._parse_links(item),
-                source=self._parse_source(response),
+                source=response.url,
             )
             meeting["status"] = self._get_status(meeting)
             meeting["id"] = self._get_id(meeting)
-        yield meeting
+            print(str(meeting))
+            yield meeting
 
     def _parse_title(self, item):
         """Parse or generate meeting title."""
@@ -49,16 +47,7 @@ class ChiSsa27Spider(CityScrapersSpider):
         else:
             return COMMISSION
 
-    @staticmethod
-    def _parse_description():
-        """Parse or generate meeting description."""
-        # description is used for any descriptions distinct to a single meeting,
-        # not a description of the agency so this can be left blank
-        return ""
-
-    @staticmethod
-    def _parse_classification():
-        """Parse or generate classification from allowed options."""
+    def _parse_classification(self):
         # This can return COMMISSION generally or COMMITTEE for the committee meetings
         return COMMISSION
 
@@ -68,33 +57,13 @@ class ChiSsa27Spider(CityScrapersSpider):
             item_txt = item.css('p::text').get()
 
         item_txt = item_txt.replace("Annual Meeting", '').replace("Sept", 'Sep')
-
         p_idx = max(item_txt.find('am'), item_txt.find('pm'), 0) + 2  # so we can slice after this
-
         front_str = item_txt[:p_idx]  # remove comments from the rest of the string
         time_str = front_str.replace("am", "AM").replace("pm", "PM").replace('.', '')
-        dt_time_obj = datetime.strptime(time_str, '%b %d, %Y, %H:%M %p')
-        return dt_time_obj
-
-    @staticmethod
-    def _parse_end():
-        """Parse end datetime as a naive datetime object. Added by pipeline if None"""
-        return None
-
-    @staticmethod
-    def _parse_time_notes():
-        """Parse any additional notes on the timing of the meeting"""
-        return ""
-
-    def _parse_all_day(self):
-        """Parse or generate all-day status. Defaults to False."""
-        return False
+        return datetime.strptime(time_str, '%b %d, %Y, %H:%M %p')
 
     def _parse_location(self, item):
-        """Parse or generate location."""
-
         first_note = item.css("p > strong ::text").get()
-
         if item.css("p > strong ::text").get():
             if "Sheil Park" in first_note:
                 return {
@@ -105,13 +74,7 @@ class ChiSsa27Spider(CityScrapersSpider):
                 raise ValueError('Meeting address has changed')
 
     def _parse_links(self, item):
-        """   Parse or generate documents.   """
-        url = item.css('a::attr(href)').get()
-        if url:
-            return [{'href': url, 'title': 'Minutes'}]
-        else:
+        if not item.css('a'):
             return []
-
-    def _parse_source(self, response):
-        """Parse or generate source."""
-        return response.url
+        else:
+            return [{'href': item.css('a::attr(href)').get(), 'title': 'Minutes'}]
--- tests/test_chi_ssa_27.py
+++ tests/test_chi_ssa_27.py
@@ -3,8 +3,6 @@ from os.path import dirname, join
 
 import pytest
 from city_scrapers_core.constants import COMMISSION
-from city_scrapers_core.constants import COMMITTEE
-
 from city_scrapers_core.utils import file_response
 from freezegun import freeze_time
 
@@ -24,36 +22,33 @@ parsed_items = [item for item in spider.parse(test_response)]
 freezer.stop()
 
 
-# def test_tests():
-#     print("Please write some tests for this spider or at least disable this one.")
-#     assert False
-
-
-"""
-Uncomment below
-"""
-
 def test_title():
-    val = parsed_items[0]["title"]
-    assert val == "Annual Meeting" or COMMISSION
+    assert parsed_items[0]["title"] == 'Commission'
+
 
 def test_description():
     assert parsed_items[0]["description"] == ""
 
+
 def test_start():
-    assert parsed_items[0]["start"] == datetime(2019, 12, 12, 8, 30)
+    assert parsed_items[0]["start"] == datetime(2019, 1, 17, 8, 30)
+
 
 def test_end():
     assert parsed_items[0]["end"] is None
 
+
 def test_time_notes():
     assert parsed_items[0]["time_notes"] == ""
 
+
 def test_id():
-    assert parsed_items[0]["id"] == 'chi_ssa_27/201912120830/x/commission'
+    assert parsed_items[0]["id"] == 'chi_ssa_27/201901170830/x/commission'
+
 
 def test_status():
     assert parsed_items[0]["status"] == "tentative"
+
 
 def test_location():
     assert parsed_items[0]["location"] == {
@@ -61,16 +56,26 @@ def test_location():
         "address": "3505 N. Southport Ave., Chicago, IL 60657"
     }
 
+
 def test_source():
     assert parsed_items[0]["source"] == "https://www.lakeviewchamber.com/ssa27"
 
+
 def test_links():
-    url= 'https://chamberma...a.pdf'
-    assert parsed_items[0]["links"] == []
+    url = (
+        'https://chambermaster.blob.core.windows.net/userfiles/UserFiles/'
+        'chambers/1819/CMS/SSA/Agendas/1-17-2019-SSA-Meeting-Agenda.pdf'
+    )
+
+    assert parsed_items[0]["links"] == [{
+        'href': url,
+        'title': 'Minutes',
+    }]
+
 
 def test_classification():
-    val = parsed_items[0]["classification"]
-    assert val ==  COMMISSION or COMMITTEE
+    assert parsed_items[0]["classification"] == COMMISSION
+
 
 @pytest.mark.parametrize("item", parsed_items)
 def test_all_day(item):
