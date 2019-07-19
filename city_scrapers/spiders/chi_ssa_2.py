6ab9b2c0cc7471c2c590623d38b1ca824f260581
Yu Zhou <yuzhou7@gmail.com>
2019-07-19 11:09:53 -0400

    SSA#2 spider

---
+++ city_scrapers/spiders/chi_ssa_2.py
@@ -0,0 +1,65 @@
+from city_scrapers_core.constants import COMMISSION
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+
+import re
+from datetime import datetime, time
+
+class ChiSsa2Spider(CityScrapersSpider):
+    name = "chi_ssa_2"
+    agency = "Chicago Special Service Are #2 Belmont/Central"
+    timezone = "America/Chicago"
+    allowed_domains = ["belmontcentral.org"]
+    start_urls = ["http://belmontcentral.org/about-ssa-2/ssa2-meeting-minutes-audit/"]
+    location = {
+        "address": "5534 W. Belmont Avenue Chicago, IL 60641",
+        "name": "Belmont-Central Chamber of Commerce"
+    }
+
+    def parse(self, response):
+        """
+        `parse` should always `yield` Meeting items.
+
+        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
+        needs.
+        """
+        for item in response.css(".entry-content a"):
+            start = self._parse_start(item)
+            if not start:
+                continue
+            meeting = Meeting(
+                title="SSA #2 Commissioners’ Meeting",
+                description="",
+                classification=COMMISSION,
+                start=start,
+                end=None,
+                all_day=False,
+                time_notes="",
+                location=self.location,
+                links=self._parse_links(item),
+                source=response.url,
+            )
+
+            meeting["status"] = self._get_status(meeting)
+            meeting["id"] = self._get_id(meeting)
+
+            yield meeting
+
+    def _parse_start(self, item):
+        """
+        Parse start date and time.
+        """
+        text = item.css('*::text').extract_first()
+        date = re.search(r'\d{1,2}-\d{1,2}-\d{4}', text)
+        if date:
+            parsed_date = datetime.strptime(date.group(), '%m-%d-%Y')
+            return datetime.combine(parsed_date.date(), time(14))
+
+    def _parse_links(self, item):
+        """Parse or generate links."""
+        link = item.css('*::attr(href)').extract_first()
+        return [{"href": link, "title": "SSA #2 Meeting Minutes"}]
+
+    def _parse_source(self, response):
+        """Parse or generate source."""
+        return response.url
---
+++ tests/test_chi_ssa_2.py
@@ -0,0 +1,85 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest
+from city_scrapers_core.constants import COMMISSION, PASSED
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.chi_ssa_2 import ChiSsa2Spider
+
+test_response = file_response(
+    join(dirname(__file__), "files", "chi_ssa_2.html"),
+    url="http://belmontcentral.org/about-ssa-2/ssa2-meeting-minutes-audit/",
+)
+spider = ChiSsa2Spider()
+
+freezer = freeze_time("2019-07-18")
+freezer.start()
+
+parsed_items = [item for item in spider.parse(test_response)]
+
+freezer.stop()
+
+"""
+Uncomment below
+"""
+
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2018, 10, 2, 14, 0)
+
+
+def test_id():
+    assert parsed_items[0]["id"] == "chi_ssa_2/201810021400/x/ssa_2_commissioners_meeting"
+
+
+def test_status():
+    assert parsed_items[0]["status"] == PASSED
+
+
+def test_links():
+    assert parsed_items[0]["links"] == [{
+      "href": "http://belmontcentral.org/wp-content/uploads/2018/12/October-2-2018-SSA-Minutes.pdf",
+      "title": "SSA #2 Meeting Minutes"
+    }]
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_all_day(item):
+    assert item["all_day"] is False
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_title(item):
+    assert item["title"] == "SSA #2 Commissioners’ Meeting"
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_description(item):
+    assert item["description"] == ""
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_end(item):
+    assert item["end"] == None
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_time_notes(item):
+    assert item["time_notes"] == ""
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_location(item):
+    assert item["location"] == spider.location
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_source(item):
+    assert item["source"] == "http://belmontcentral.org/about-ssa-2/ssa2-meeting-minutes-audit/"
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_classification(item):
+    assert item["classification"] == COMMISSION
