5fe5842907ef9fbee787d78bd2b23e9b101a84ad
Patrick Sier <pjsier@users.noreply.github.com>
2019-07-21 11:44:42 -0500

    Update cook_south_mosquito with site changes (#796)

--- tests/test_cook_south_mosquito.py
+++ tests/test_cook_south_mosquito.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from operator import itemgetter
 from os.path import dirname, join
 
 import pytest  # noqa
@@ -17,13 +18,13 @@ spider = CookSouthMosquitoSpider()
 freezer = freeze_time("2019-07-10")
 freezer.start()
 
-parsed_items = [item for item in spider.parse(test_response)]
+parsed_items = sorted([item for item in spider.parse(test_response)], key=itemgetter("start"))
 
 freezer.stop()
 
 
 def test_count():
-    assert len(parsed_items) == 12
+    assert len(parsed_items) == 17
 
 
 def test_title():
@@ -35,7 +36,7 @@ def test_description():
 
 
 def test_start():
-    assert parsed_items[0]["start"] == datetime(2019, 7, 8, 16)
+    assert parsed_items[0]["start"] == datetime(2019, 4, 8, 16, 0)
 
 
 def test_end():
@@ -47,7 +48,7 @@ def test_time_notes():
 
 
 def test_id():
-    assert parsed_items[0]["id"] == "cook_south_mosquito/201907081600/x/board_of_trustees"
+    assert parsed_items[0]["id"] == "cook_south_mosquito/201904081600/x/board_of_trustees"
 
 
 def test_status():
@@ -64,10 +65,14 @@ def test_source():
 
 def test_links():
     assert parsed_items[0]["links"] == [{
-        "href": "https://sccmad.org/images/agenda/July-2019-agenda.pdf",
-        "title": "Agenda"
+        "title": "Minutes",
+        "href": "https://sccmad.org/images/minutes/april-8-2019-minutes.pdf"
     }]
-    assert parsed_items[1]["links"] == []
+    assert parsed_items[4]["links"] == [{
+        "title": "Agenda",
+        "href": "https://sccmad.org/images/agenda/July-2019-agenda.pdf",
+    }]
+    assert parsed_items[-1]["links"] == []
 
 
 def test_classification():
