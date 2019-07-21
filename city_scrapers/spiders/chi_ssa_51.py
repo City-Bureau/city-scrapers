d01ec245f72586c18ac59b44b085300a66d7d78b
Yu Zhou <yuzhou7@gmail.com>
2019-07-21 12:39:01 -0400

    validate location and code styles

--- city_scrapers/spiders/chi_ssa_51.py
+++ city_scrapers/spiders/chi_ssa_51.py
@@ -13,7 +13,6 @@ class ChiSsa51Spider(CityScrapersSpider):
     allowed_domains = ["www.cbatechworks.org"]
     start_urls = ["http://www.cbatechworks.org/"]
     location = {"address": "806 East 78th Street, Chicago IL 60619", "name": "QBG Foundation"}
-    last_parsed_date = None
 
     def parse(self, response):
         """
@@ -22,24 +21,26 @@ class ChiSsa51Spider(CityScrapersSpider):
         Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
         needs.
         """
+        self._validate_location(response)
+        last_parsed_date = ""
         for item in response.css("div#element106 font"):
             """
-            The date and times contained within sibling divs that are identicals, so we have to
+            The date and times are contained within sibling divs that are identicals, so we have to
             continue the loop and only create the meeting until both date and times have been
             parsed.
             """
-            if not self.last_parsed_date:
-                self._parse_date(item)
+            if not last_parsed_date:
+                last_parsed_date = self._parse_date(item)
                 continue
             else:
                 start_and_end = self._parse_time(item)
                 if not start_and_end:
                     continue
-                start = self.last_parsed_date + " " + start_and_end[0].strip()
+                start = last_parsed_date + " " + start_and_end[0].strip()
                 start = datetime.strptime(start, "%B %d, %Y %I:%M%p")
-                end = self.last_parsed_date + " " + start_and_end[1].strip()
+                end = last_parsed_date + " " + start_and_end[1].strip()
                 end = datetime.strptime(end, "%B %d, %Y %I:%M%p")
-                self.last_parsed_date = None
+                last_parsed_date = ""
 
             meeting = Meeting(
                 title="Commission",
@@ -62,10 +63,11 @@ class ChiSsa51Spider(CityScrapersSpider):
     def _parse_date(self, item):
         text = item.css('*::text').extract_first()
         if text is None:
-            return
+            return ""
         date = re.search(r'\w{3,9} \d{1,2}, \d{4}', text)
         if date:
-            self.last_parsed_date = date.group()
+            return date.group()
+        return ""
 
     def _parse_time(self, item):
         """Parse start datetime as a naive datetime object."""
@@ -73,3 +75,7 @@ class ChiSsa51Spider(CityScrapersSpider):
         times = re.search(r'\d{1,2}:\d{2}[ap]m - \d{1,2}:\d{2}[ap]m', text)
         if times:
             return times.group().split('-')
+
+    def _validate_location(self, response):
+        if "806 East" not in " ".join(response.css("div#element106 *::text").extract()):
+            raise ValueError("Meeting location has changed")
