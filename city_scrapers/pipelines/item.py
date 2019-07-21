0d8225fd2bbc1c61e3ab7090a2c12f647078164e
Patrick Sier <pjsier@users.noreply.github.com>
2019-07-21 11:46:05 -0500

    Add chi_human_relations, support for PDFs (#795)

--- city_scrapers/pipelines/item.py
+++ city_scrapers/pipelines/item.py
@@ -5,7 +5,6 @@ class CityScrapersItemPipeline(object):
     """
     Pipeline for doing any custom processing on individual items
     """
-
     @report_error
     def process_item(self, item, spider):
         item['agency'] = spider.agency
