1680e9be462418b271f3a4112b7af488056fabe7
bendiller <diller.benjamin@gmail.com>
2019-09-20 08:34:59 -0600

    Populate static fields; "low-hanging fruit" requiring no parsing.

--- city_scrapers/spiders/chi_midway_noise.py
+++ city_scrapers/spiders/chi_midway_noise.py
@@ -1,4 +1,6 @@
-from city_scrapers_core.constants import NOT_CLASSIFIED
+from datetime import datetime
+
+from city_scrapers_core.constants import COMMISSION
 from city_scrapers_core.items import Meeting
 from city_scrapers_core.spiders import CityScrapersSpider
 
@@ -9,6 +11,11 @@ class ChiMidwayNoiseSpider(CityScrapersSpider):
     timezone = "America/Chicago"
     allowed_domains = ["www.flychicago.com"]
     start_urls = ["https://www.flychicago.com"]
+    location = {
+        "name": "The Mayfield",
+        "address": "6072 S. Archer Ave., Chicago, IL 60638",
+    }
+    source = "https://www.flychicago.com/community/MDWnoise/AdditionalResources/pages/default.aspx"
 
     def parse(self, response):
         """
@@ -20,15 +27,15 @@ class ChiMidwayNoiseSpider(CityScrapersSpider):
         for item in response.css(".meetings"):
             meeting = Meeting(
                 title=self._parse_title(item),
-                description=self._parse_description(item),
-                classification=self._parse_classification(item),
+                description="",
+                classification=COMMISSION,
                 start=self._parse_start(item),
-                end=self._parse_end(item),
-                all_day=self._parse_all_day(item),
-                time_notes=self._parse_time_notes(item),
-                location=self._parse_location(item),
+                end=None,
+                all_day=False,
+                time_notes="Start times are not explicitly stated, but all observed past meetings occurred at 6:30PM",
+                location=self.location,
                 links=self._parse_links(item),
-                source=self._parse_source(response),
+                source=self.source,
             )
 
             meeting["status"] = self._get_status(meeting)
@@ -40,41 +47,10 @@ class ChiMidwayNoiseSpider(CityScrapersSpider):
         """Parse or generate meeting title."""
         return ""
 
-    def _parse_description(self, item):
-        """Parse or generate meeting description."""
-        return ""
-
-    def _parse_classification(self, item):
-        """Parse or generate classification from allowed options."""
-        return NOT_CLASSIFIED
-
     def _parse_start(self, item):
         """Parse start datetime as a naive datetime object."""
         return None
 
-    def _parse_end(self, item):
-        """Parse end datetime as a naive datetime object. Added by pipeline if None"""
-        return None
-
-    def _parse_time_notes(self, item):
-        """Parse any additional notes on the timing of the meeting"""
-        return ""
-
-    def _parse_all_day(self, item):
-        """Parse or generate all-day status. Defaults to False."""
-        return False
-
-    def _parse_location(self, item):
-        """Parse or generate location."""
-        return {
-            "address": "",
-            "name": "",
-        }
-
     def _parse_links(self, item):
         """Parse or generate links."""
         return [{"href": "", "title": ""}]
-
-    def _parse_source(self, response):
-        """Parse or generate source."""
-        return response.url
