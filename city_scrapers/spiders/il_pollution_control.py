c5ab9928b59acb23fd88be8b00f3902ec33561ce
bendiller <diller.benjamin@gmail.com>
2019-10-04 14:19:31 -0600

    Parse title, meeting times.

--- city_scrapers/spiders/il_pollution_control.py
+++ city_scrapers/spiders/il_pollution_control.py
@@ -1,3 +1,9 @@
+from collections import defaultdict
+from datetime import datetime
+import json
+
+from scrapy import Request
+
 from city_scrapers_core.constants import NOT_CLASSIFIED
 from city_scrapers_core.items import Meeting
 from city_scrapers_core.spiders import CityScrapersSpider
@@ -8,32 +14,50 @@ class IlPollutionControlSpider(CityScrapersSpider):
     agency = "Illinois Pollution Control Board"
     timezone = "America/Chicago"
     allowed_domains = ["pcb.illinois.gov"]
-    start_urls = ["https://pcb.illinois.gov/ClerksOffice/Calendar"]
+    start_urls = ["https://pcb.illinois.gov/ClerksOffice/MeetingMinutes"]
+
+    def __init__(self, *args, **kwargs):
+        self.link_map = defaultdict(list)  # Populated by self._parse_minutes()
+        super().__init__(*args, **kwargs)
 
     def parse(self, response):
         """
         `parse` should always `yield` Meeting items.
-
-        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
-        needs.
         """
-        for item in response.css(".meetings"):
+        # Gather links to meeting minutes, to be associated with Meeting objects later:
+        self._parse_minutes(response)
+
+        # Parse JSON containing meeting data:
+        yield Request("https://pcb.illinois.gov/ClerksOffice/GetCalendarEvents", callback=self._parse_json)
+
+    def _parse_minutes(self, response):
+        """ Populate self.link_map """
+        return None
+
+    def _parse_json(self, response):
+        """ Parse JSON from https://pcb.illinois.gov/ClerksOffice/GetCalendarEvents -> Meetings """
+        data = json.loads(response.body_as_unicode())
+
+        for item in data:
+            if item["CalendarTypeDesc"] == "Holiday":
+                continue  # Not a meeting - just an indication of a holiday on the calendar.
             meeting = Meeting(
-                title=self._parse_title(item),
+                title=item["CalendarTypeDesc"].strip(),
                 description=self._parse_description(item),
                 classification=self._parse_classification(item),
                 start=self._parse_start(item),
-                end=self._parse_end(item),
-                all_day=self._parse_all_day(item),
+                end=None,
+                all_day=item["IsFullDay"],
                 time_notes=self._parse_time_notes(item),
                 location=self._parse_location(item),
                 links=self._parse_links(item),
                 source=self._parse_source(response),
             )
 
-            meeting["status"] = self._get_status(meeting)
+            # meeting["status"] = self._get_status(meeting)
             meeting["id"] = self._get_id(meeting)
 
+            print(meeting)
             yield meeting
 
     def _parse_title(self, item):
@@ -49,8 +73,7 @@ class IlPollutionControlSpider(CityScrapersSpider):
         return NOT_CLASSIFIED
 
     def _parse_start(self, item):
-        """Parse start datetime as a naive datetime object."""
-        return None
+        return datetime.strptime(item["StartDateTime"], '%m/%d/%Y %H:%M:%S %p')
 
     def _parse_end(self, item):
         """Parse end datetime as a naive datetime object. Added by pipeline if None"""
