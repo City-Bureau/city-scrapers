79e09ae9c758d982255c4740f3cc01b6c5794e0c
Brian-Lee <BrianTrewLee@gmail.com>
2019-10-04 23:55:07 -0700

    formatted accordingto linting rules (passed flake8)

--- city_scrapers/spiders/wayne_local_emergency_planning.py
+++ city_scrapers/spiders/wayne_local_emergency_planning.py
@@ -1,8 +1,6 @@
-from city_scrapers_core.constants import NOT_CLASSIFIED, COMMITTEE
+from city_scrapers_core.constants import COMMITTEE
 from city_scrapers_core.items import Meeting
 from city_scrapers_core.spiders import CityScrapersSpider
-
-from datetime import datetime
 from dateutil import parser
 
 
@@ -12,8 +10,6 @@ class WayneLocalEmergencyPlanningSpider(CityScrapersSpider):
     timezone = "America/Detroit"
     allowed_domains = ["www.waynecounty.com"]
     start_urls = ["https://www.waynecounty.com/departments/hsem/wayne-county-lepc.aspx"]
-	
-	
 
     def parse(self, response):
         """
@@ -23,15 +19,15 @@ class WayneLocalEmergencyPlanningSpider(CityScrapersSpider):
         needs.
         """
 
-        # the meeting dates I've seen all had the substring 'day, ' as in 'Wednesday, March 6, 2019'       
-        meeting_dates = response.xpath('''//p[contains(text(),'day, ')]''') 
+        # the meeting dates I've seen all had the substring 'day, ' as in 'Wednesday, March 6, 2019'
+        meeting_dates = response.xpath('''//p[contains(text(),'day, ')]''')
 
         for item in meeting_dates:
             # clean off the paragraph tags from the parsed meeting date (item)
-            item = str(item.extract()).replace('<p>','').replace('</p>','')
-            #print('------------')
-            #print(item)
-            
+            item = str(item.extract()).replace('<p>',  '').replace('</p>', '')
+            # print('------------')
+            # print(item)
+
             meeting = Meeting(
                 title=self._parse_title(item),
                 description=self._parse_description(item),
@@ -49,33 +45,31 @@ class WayneLocalEmergencyPlanningSpider(CityScrapersSpider):
             meeting["id"] = self._get_id(meeting)
 
             yield meeting
-        #exit()
-		
+
     def _parse_title(self, item):
         """Parse or generate meeting title."""
         prefix = 'Wayne County LEPC Meeting - '
-		# create a unique title for each meeting by combining a meaningful prefix with a specific meeting date
+        # create a unique title for each meeting by combining
+        # a meaningful prefix with a specific meeting date
         ret_val = prefix + item
-        #print(ret_val)
-        #exit()
         return ret_val
 
     def _parse_description(self, item):
         """Parse or generate meeting description."""
-        return "" # no description of this specific meeting is shown, so we return an empty string
+        return ""  # no description of this specific meeting is shown, so we return an empty string
 
     def _parse_classification(self, item):
         """Parse or generate classification from allowed options."""
-        return COMMITTEE #done
-		
+        return COMMITTEE
+
     def _parse_start(self, item):
         """Parse start datetime as a naive datetime object."""
-        dt = parser.parse(item + " 2pm") # all meetings will be at 2pm
+        dt = parser.parse(item + " 2pm")  # all meetings will be at 2pm
         return dt
 
     def _parse_end(self, item):
         """Parse end datetime as a naive datetime object. Added by pipeline if None"""
-        return None # no end time is shown on the webpage
+        return None  # no end time is shown on the webpage
 
     def _parse_time_notes(self, item):
         """Parse any additional notes on the timing of the meeting"""
@@ -83,13 +77,13 @@ class WayneLocalEmergencyPlanningSpider(CityScrapersSpider):
 
     def _parse_all_day(self, item):
         """Parse or generate all-day status. Defaults to False."""
-        return False # Since the meetings start at 2pm, I assume this must be False
+        return False  # Since the meetings start at 2pm, I assume this must be False
 
     def _parse_location(self, item):
         """Parse or generate location."""
         ''' from webpage:
-            Wayne County Community College, 21000 Northline Road, Taylor, 
-            in the MIPSE Building. The MIPSE Building is the fire training 
+            Wayne County Community College, 21000 Northline Road, Taylor,
+            in the MIPSE Building. The MIPSE Building is the fire training
             facility located at the rear of the campus.'''
         return {
             "address": "21000 Northline Road, Taylor, MI",
@@ -98,8 +92,8 @@ class WayneLocalEmergencyPlanningSpider(CityScrapersSpider):
 
     def _parse_links(self, item):
         """Parse or generate links."""
-        return [] # I didn't see any links for minutes or agendas
+        return []  # I didn't see any links for minutes or agendas
 
     def _parse_source(self, response):
         """Parse or generate source."""
-        return response.url # I did't see a more specific detail page
+        return response.url  # I did't see a more specific detail page
--- tests/test_wayne_local_emergency_planning.py
+++ tests/test_wayne_local_emergency_planning.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from os.path import dirname, join
 
 import pytest
-from city_scrapers_core.constants import NOT_CLASSIFIED, COMMITTEE
+from city_scrapers_core.constants import COMMITTEE
 from city_scrapers_core.utils import file_response
 from freezegun import freeze_time
 
@@ -24,13 +24,14 @@ freezer.stop()
 
 def test_tests():
     print("Please write some tests for this spider or at least disable this one.")
-    #assert False
-    assert True # disabled this test
+    # assert False
+    assert True  # disabled this test
 
 
 """
 Uncomment below
 """
+
 
 def test_title():
     assert parsed_items[0]["title"] == "Wayne County LEPC Meeting - Wednesday, March 6, 2019"
@@ -45,16 +46,19 @@ def test_start():
 
 
 def test_end():
-    assert parsed_items[0]["end"] == None
+    assert parsed_items[0]["end"] is None
 #     assert parsed_items[0]["end"] == datetime(2019, 1, 1, 0, 0)
 
 
 def test_time_notes():
-    assert parsed_items[0]["time_notes"] == "The Wayne County LEPC meets quarterly. All meetings will be at 2:00 p.m."
+    EXPECTED_TIME_NOTES = "The Wayne County LEPC meets quarterly. All meetings will be at 2:00 p.m."
+    assert parsed_items[0]["time_notes"] == EXPECTED_TIME_NOTES
 
 
 def test_id():
-    assert parsed_items[0]["id"] == "wayne_local_emergency_planning/201903061400/x/wayne_county_lepc_meeting_wednesday_march_6_2019"
+    EXPECTED_ID = '''wayne_local_emergency_planning/201903061400/'''
+    EXPECTED_ID += '''x/wayne_county_lepc_meeting_wednesday_march_6_2019'''
+    assert parsed_items[0]["id"] == EXPECTED_ID
 
 
 def test_status():
@@ -69,11 +73,12 @@ def test_location():
 
 
 def test_source():
-    assert parsed_items[0]["source"] == "https://www.waynecounty.com/departments/hsem/wayne-county-lepc.aspx"
+    EXPECTED_SOURCE = "https://www.waynecounty.com/departments/hsem/wayne-county-lepc.aspx"
+    assert parsed_items[0]["source"] == EXPECTED_SOURCE
 
 
 def test_links():
-    assert parsed_items[0]["links"] == []   
+    assert parsed_items[0]["links"] == []
 #     assert parsed_items[0]["links"] == [{
 #       "href": "EXPECTED HREF",
 #       "title": "EXPECTED TITLE"
@@ -86,4 +91,4 @@ def test_classification():
 
 @pytest.mark.parametrize("item", parsed_items)
 def test_all_day(item):
-     assert item["all_day"] is False
+    assert item["all_day"] is False
