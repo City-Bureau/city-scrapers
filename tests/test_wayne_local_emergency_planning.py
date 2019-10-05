10477265363381c016c6561e4281e6fae4464741
Brian-Lee <BrianTrewLee@gmail.com>
2019-10-04 22:58:32 -0700

    filled in several tests

--- tests/test_wayne_local_emergency_planning.py
+++ tests/test_wayne_local_emergency_planning.py
@@ -32,46 +32,51 @@ def test_tests():
 Uncomment below
 """
 
-# def test_title():
-#     assert parsed_items[0]["title"] == "EXPECTED TITLE"
+def test_title():
+    assert parsed_items[0]["title"] == "Wayne County LEPC Meeting - Wednesday, March 6, 2019"
 
 
-# def test_description():
-#     assert parsed_items[0]["description"] == "EXPECTED DESCRIPTION"
+def test_description():
+    assert parsed_items[0]["description"] == ""
 
 
-# def test_start():
-#     assert parsed_items[0]["start"] == datetime(2019, 1, 1, 0, 0)
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2019, 3, 6, 14, 0)
 
 
-# def test_end():
+def test_end():
+    assert parsed_items[0]["end"] == None
 #     assert parsed_items[0]["end"] == datetime(2019, 1, 1, 0, 0)
 
 
-# def test_time_notes():
-#     assert parsed_items[0]["time_notes"] == "EXPECTED TIME NOTES"
+def test_time_notes():
+    assert parsed_items[0]["time_notes"] == "The Wayne County LEPC meets quarterly. All meetings will be at 2:00 p.m."
 
 
-# def test_id():
-#     assert parsed_items[0]["id"] == "EXPECTED ID"
+def test_id():
+    #print(parsed_items[0]["id"])
+    #print("Wayne County LEPC - Wednesday, March 6, 2019")
+    #exit()
+    assert parsed_items[0]["id"] == "wayne_local_emergency_planning/201903061400/x/wayne_county_lepc_meeting_wednesday_march_6_2019"
 
 
 # def test_status():
 #     assert parsed_items[0]["status"] == "EXPECTED STATUS"
 
 
-# def test_location():
-#     assert parsed_items[0]["location"] == {
-#         "name": "EXPECTED NAME",
-#         "address": "EXPECTED ADDRESS"
-#     }
+def test_location():
+    assert parsed_items[0]["location"] == {
+        "name": "Wayne County Community College, in the MIPSE Building",
+        "address": "21000 Northline Road, Taylor, MI"
+    }
 
 
-# def test_source():
-#     assert parsed_items[0]["source"] == "EXPECTED URL"
+def test_source():
+    assert parsed_items[0]["source"] == "https://www.waynecounty.com/departments/hsem/wayne-county-lepc.aspx"
 
 
-# def test_links():
+def test_links():
+    assert parsed_items[0]["links"] == []   
 #     assert parsed_items[0]["links"] == [{
 #       "href": "EXPECTED HREF",
 #       "title": "EXPECTED TITLE"
@@ -79,10 +84,9 @@ Uncomment below
 
 
 def test_classification():
-    print("len=" + str(len(parsed_items)))	
     assert parsed_items[0]["classification"] == COMMITTEE
 
 
-# @pytest.mark.parametrize("item", parsed_items)
-# def test_all_day(item):
-#     assert item["all_day"] is False
+@pytest.mark.parametrize("item", parsed_items)
+def test_all_day(item):
+     assert item["all_day"] is False
