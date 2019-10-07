6b5e3b1e5b9e2032a5b5dddaa94d77b769c060fa
Mappy <40239457+AmiMappy@users.noreply.github.com>
2019-10-07 19:13:38 +0530

    Add test files for chi_ssa_18

--- test_chi_ssa_18.py
+++ test_chi_ssa_18.py
@@ -2,11 +2,10 @@ from datetime import datetime
 from os.path import dirname, join
 
 import pytest
+from chi_ssa_18 import ChiSsa18Spider
 from city_scrapers_core.constants import NOT_CLASSIFIED
 from city_scrapers_core.utils import file_response
 from freezegun import freeze_time
-
-from chi_ssa_18 import ChiSsa18Spider
 
 test_response = file_response(
     join(dirname(__file__), "files", "chi_ssa_18.html"),
@@ -31,41 +30,50 @@ def test_tests():
 Uncomment below
 """
 
+
 def test_title():
     assert parsed_items[0]["title"] == "EXPECTED TITLE"
+
+
 def test_description():
     assert parsed_items[0]["description"] == "EXPECTED DESCRIPTION"
+
+
 def test_start():
     assert parsed_items[0]["start"] == datetime(2019, 1, 1, 0, 0)
+
+
 def test_end():
     assert parsed_items[0]["end"] == datetime(2019, 1, 1, 0, 0)
+
 
 def test_time_notes():
     assert parsed_items[0]["time_notes"] == "EXPECTED TIME NOTES"
 
+
 def test_id():
     assert parsed_items[0]["id"] == "EXPECTED ID"
+
 
 def test_status():
     assert parsed_items[0]["status"] == "EXPECTED STATUS"
 
+
 def test_location():
-    assert parsed_items[0]["location"] == {
-        "name": "EXPECTED NAME",
-        "address": "EXPECTED ADDRESS"
-    }
+    assert parsed_items[0]["links"] == [{"href": "EXPECTED HREF", "title": "EXPECTED TITLE"}]
+
 
 def test_source():
     assert parsed_items[0]["source"] == "EXPECTED URL"
 
+
 def test_links():
-    assert parsed_items[0]["links"] == [{
-      "href": "EXPECTED HREF",
-      "title": "EXPECTED TITLE"
-    }]
+    assert parsed_items[0]["links"] == [{"href": "EXPECTED HREF", "title": "EXPECTED TITLE"}]
+
 
 def test_classification():
     assert parsed_items[0]["classification"] == NOT_CLASSIFIED
+
 
 @pytest.mark.parametrize("item", parsed_items)
 def test_all_day(item):
