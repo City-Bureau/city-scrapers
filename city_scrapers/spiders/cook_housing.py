71e29b36a8a835508b7070b65c4f2511fc50c578
Peter Gebhard <peter.gebhard@wwt.com>
2019-10-07 23:52:59 -0500

    Improve cook_housing based on review comments (#771)

--- city_scrapers/spiders/cook_housing.py
+++ city_scrapers/spiders/cook_housing.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 
-from city_scrapers_core.constants import BOARD, FORUM, NOT_CLASSIFIED
+from city_scrapers_core.constants import BOARD, NOT_CLASSIFIED
 from city_scrapers_core.items import Meeting
 from city_scrapers_core.spiders import CityScrapersSpider
+from dateutil.relativedelta import relativedelta
 
 
 class CookHousingSpider(CityScrapersSpider):
@@ -10,43 +11,49 @@ class CookHousingSpider(CityScrapersSpider):
     agency = "Cook County Housing Authority"
     timezone = "America/Chicago"
     allowed_domains = ["thehacc.org"]
-    start_urls = ["http://thehacc.org/events/"]
+
+    @property
+    def start_urls(self):
+        """Pull events from the calendar pages for 2 months past through 2 months ahead"""
+        now = datetime.now()
+        urls = []
+        for months_delta in range(-2, 3):
+            month_str = (now + relativedelta(months=months_delta)).strftime("%Y-%m")
+            urls.append("http://thehacc.org/events/{}".format(month_str))
+        return urls
 
     def parse(self, response):
         """
         `parse` should always `yield` Meeting items.
-
-        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
-        needs.
         """
         for item in response.xpath('//div[@class="item-wrapper"]'):
-            meeting = Meeting(
-                title=self._parse_title(item),
-                description=self._parse_description(item),
-                classification=self._parse_classification(item),
-                start=self._parse_times(item)[0],
-                end=self._parse_times(item)[1],
-                all_day=False,
-                time_notes="",
-                location=self._parse_location(item),
-                links=self._parse_links(item),
-                source=self._parse_source(response),
-            )
+            classification = self._parse_classification(item)
+            if classification == BOARD:
+                meeting = Meeting(
+                    title=self._parse_title(item),
+                    description="",
+                    classification=BOARD,
+                    start=self._parse_times(item)[0],
+                    end=self._parse_times(item)[1],
+                    all_day=False,
+                    time_notes="",
+                    location=self._parse_location(item),
+                    links=[],
+                    source=self._parse_source(item),
+                )
 
-            meeting["status"] = self._get_status(meeting)
-            meeting["id"] = self._get_id(meeting)
+                meeting["status"] = self._get_status(meeting)
+                meeting["id"] = self._get_id(meeting)
 
-            yield meeting
+                yield meeting
 
     @staticmethod
     def _parse_title(item):
         """Parse or generate meeting title."""
-        return item.xpath(".//a/text()[normalize-space(.)]").get().strip()
-
-    @staticmethod
-    def _parse_description(item):
-        """Parse or generate meeting description."""
-        return item.xpath('.//p[@class="subtitle"]/following-sibling::p[2]/text()').get().strip()
+        title = item.xpath(".//a/text()[normalize-space(.)]").get().strip()
+        if title == "Housing Authority of Cook County Board Meeting":
+            return "Board of Commissioners"
+        return title
 
     @staticmethod
     def _parse_classification(item):
@@ -54,8 +61,6 @@ class CookHousingSpider(CityScrapersSpider):
         title = item.xpath(".//a/text()[normalize-space(.)]").get().lower()
         if 'board' in title:
             return BOARD
-        elif 'workshop' in title:
-            return FORUM
         return NOT_CLASSIFIED
 
     @staticmethod
@@ -77,15 +82,9 @@ class CookHousingSpider(CityScrapersSpider):
         times = item.xpath('.//p[@class="subtitle"]/br/following-sibling::text()').get().strip()
         address = item.xpath('.//div[@class="info"]/p/text()').get().replace(times, '').strip()
         name = item.xpath('.//p[@class="subtitle"]/text()').get().strip()
-        return {"address": address, "name": name}
+        return {"address": address + ", Chicago, IL", "name": name}
 
     @staticmethod
-    def _parse_links(item):
-        """Parse or generate links."""
-        link = item.xpath('.//div[@class="info"]//a')
-        return [{"href": link.xpath('@href').get(), "title": link.xpath('text()').get()}]
-
-    @staticmethod
-    def _parse_source(response):
+    def _parse_source(item):
         """Parse or generate source."""
-        return response.url
+        return item.xpath('.//div[@class="info"]//a/@href').get()
--- tests/test_cook_housing.py
+++ tests/test_cook_housing.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from os.path import dirname, join
 
 import pytest
-from city_scrapers_core.constants import BOARD, FORUM, TENTATIVE
+from city_scrapers_core.constants import BOARD, TENTATIVE
 from city_scrapers_core.utils import file_response
 from freezegun import freeze_time
 
@@ -23,30 +23,27 @@ freezer.stop()
 
 
 def test_count():
-    assert len(parsed_items) == 4
+    assert len(parsed_items) == 2
 
 
 def test_title():
-    assert parsed_items[0]["title"] == "Landlord Workshop – Nuts and Bolts of the HCV Program"
-    assert parsed_items[1]["title"] == "Housing Authority of Cook County Board Meeting"
+    assert parsed_items[0]["title"] == "Board of Commissioners"
+    assert parsed_items[1]["title"] == "Board of Commissioners"
 
 
-def test_description():
-    assert parsed_items[0]["description"] == "To Register Click Here"
-    assert parsed_items[1]["description"] == "Regular Board of Commissioners Meetings All " \
-                                             "meetings start at 2:00 p.m., Central Time " \
-                                             "HACC Central Office Board Room 175 West " \
-                                             "Jackson, Suite 350, Chicago, IL"
+@pytest.mark.parametrize("item", parsed_items)
+def test_description(item):
+    assert item["description"] == ""
 
 
 def test_start():
-    assert parsed_items[0]["start"] == datetime(2019, 10, 10, 13, 0)
-    assert parsed_items[1]["start"] == datetime(2019, 10, 17, 14, 0)
+    assert parsed_items[0]["start"] == datetime(2019, 10, 17, 14, 0)
+    assert parsed_items[1]["start"] == datetime(2019, 12, 12, 14, 0)
 
 
 def test_end():
-    assert parsed_items[0]["end"] == datetime(2019, 10, 10, 14, 30)
-    assert parsed_items[1]["end"] == datetime(2019, 10, 17, 16, 0)
+    assert parsed_items[0]["end"] == datetime(2019, 10, 17, 16, 0)
+    assert parsed_items[1]["end"] == datetime(2019, 12, 12, 16, 0)
 
 
 def test_time_notes():
@@ -61,31 +58,29 @@ def test_status():
 
 def test_location():
     assert parsed_items[0]["location"] == {
-        "name": "Turlington West Apts.",
-        "address": "15306 S. Robey Ave."
+        "name": "HACC Downtown",
+        "address": "175 W Jackson Blvd, Chicago, IL"
     }
-    assert parsed_items[1]["location"] == {"name": "HACC Downtown", "address": "175 W Jackson Blvd"}
+    assert parsed_items[1]["location"] == {
+        "name": "HACC Downtown",
+        "address": "175 W Jackson Blvd, Chicago, IL"
+    }
 
 
 def test_source():
-    assert parsed_items[0]["source"] == "http://thehacc.org/events/"
+    assert parsed_items[0]["source"] == \
+        "http://thehacc.org/event/housing-authority-of-cook-county-board-meeting-2-2-2-2-4/"
+    assert parsed_items[1]["source"] == \
+        "http://thehacc.org/event/housing-authority-of-cook-county-board-meeting-2-2-2-2-5/"
 
 
-def test_links():
-    assert parsed_items[0]["links"] == [{
-        "href": "http://thehacc.org/event/landlord-workshop-nuts-and-bolts-of-the-hcv-program-3/",
-        "title": "Landlord Workshop – Nuts and Bolts of the HCV Program"
-    }]
-    assert parsed_items[1]["links"] == [{
-        "href":
-            "http://thehacc.org/event/"
-            "housing-authority-of-cook-county-board-meeting-2-2-2-2-4/",
-        "title": "Housing Authority of Cook County Board Meeting"
-    }]
+@pytest.mark.parametrize("item", parsed_items)
+def test_links(item):
+    assert item["links"] == []
 
 
 def test_classification():
-    assert parsed_items[0]["classification"] == FORUM
+    assert parsed_items[0]["classification"] == BOARD
     assert parsed_items[1]["classification"] == BOARD
 
 
