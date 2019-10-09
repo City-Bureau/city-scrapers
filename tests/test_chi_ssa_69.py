11f42405a7caf9c9bf35e05aede490d8654133db
Brian-Lee <BrianTrewLee@gmail.com>
2019-10-09 14:42:47 -0700

    fixed flake8 errors

--- tests/test_chi_ssa_69.py
+++ tests/test_chi_ssa_69.py
@@ -1,8 +1,8 @@
-from datetime import datetime
+# from datetime import datetime
 from os.path import dirname, join
 
-import pytest
-from city_scrapers_core.constants import NOT_CLASSIFIED
+# import pytest
+# from city_scrapers_core.constants import NOT_CLASSIFIED
 from city_scrapers_core.utils import file_response
 from freezegun import freeze_time
 
