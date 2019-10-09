2e3e596404ef136c7b395c80a3c5ca2149c1c061
Brian-Lee <BrianTrewLee@gmail.com>
2019-10-09 13:28:07 -0700

    deleted test that returns False

--- tests/test_chi_ssa_69.py
+++ tests/test_chi_ssa_69.py
@@ -22,11 +22,6 @@ parsed_items = [item for item in spider.parse(test_response)]
 freezer.stop()
 
 
-def test_tests():
-    print("Please write some tests for this spider or at least disable this one.")
-    assert False
-
-
 """
 Uncomment below
 """
