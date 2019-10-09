1a26f8b22aad7dfbbc541078e6fcc716059047eb
Brian-Lee <BrianTrewLee@gmail.com>
2019-10-09 14:49:37 -0700

    adding file I forgot to add last time

--- tests/test_chi_ssa_69.py
+++ tests/test_chi_ssa_69.py
@@ -20,8 +20,6 @@ freezer.start()
 parsed_items = [item for item in spider.parse(test_response)]
 
 freezer.stop()
-
-
 """
 Uncomment below
 """
@@ -29,30 +27,23 @@ Uncomment below
 # def test_title():
 #     assert parsed_items[0]["title"] == "EXPECTED TITLE"
 
-
 # def test_description():
 #     assert parsed_items[0]["description"] == "EXPECTED DESCRIPTION"
-
 
 # def test_start():
 #     assert parsed_items[0]["start"] == datetime(2019, 1, 1, 0, 0)
 
-
 # def test_end():
 #     assert parsed_items[0]["end"] == datetime(2019, 1, 1, 0, 0)
-
 
 # def test_time_notes():
 #     assert parsed_items[0]["time_notes"] == "EXPECTED TIME NOTES"
 
-
 # def test_id():
 #     assert parsed_items[0]["id"] == "EXPECTED ID"
 
-
 # def test_status():
 #     assert parsed_items[0]["status"] == "EXPECTED STATUS"
-
 
 # def test_location():
 #     assert parsed_items[0]["location"] == {
@@ -60,10 +51,8 @@ Uncomment below
 #         "address": "EXPECTED ADDRESS"
 #     }
 
-
 # def test_source():
 #     assert parsed_items[0]["source"] == "EXPECTED URL"
-
 
 # def test_links():
 #     assert parsed_items[0]["links"] == [{
@@ -71,10 +60,8 @@ Uncomment below
 #       "title": "EXPECTED TITLE"
 #     }]
 
-
 # def test_classification():
 #     assert parsed_items[0]["classification"] == NOT_CLASSIFIED
-
 
 # @pytest.mark.parametrize("item", parsed_items)
 # def test_all_day(item):
