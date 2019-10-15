c5fa252da3d7f4c256d9b3246b9637c9293fb3f7
TJ <48883513+pyence@users.noreply.github.com>
2019-10-15 23:24:35 +0200

    0656-spider-cook_county_board_ethics (#876)

---
+++ city_scrapers/spiders/cook_county_board_ethics.py
@@ -0,0 +1,143 @@
+import re
+from datetime import datetime
+
+import scrapy
+from city_scrapers_core.constants import BOARD
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+
+
+class CookCountyBoardEthicsSpider(CityScrapersSpider):
+    name = 'cook_county_board_ethics'
+    agency = 'Cook County Government Board of Ethics'
+    timezone = 'America/Chicago'
+    allowed_domains = ['www.cookcountyil.gov']
+
+    def start_requests(self):
+        today = datetime.now()
+        for month_delta in range(-2, 3):
+            mo_str = today.replace(month=today.month + month_delta).strftime('%Y-%m')
+            url = 'https://www.cookcountyil.gov/calendar-node-field-date/month/{}'.format(mo_str)
+            yield scrapy.Request(url=url, method='GET', callback=self.parse)
+
+    def parse(self, response):
+        """
+        `parse` should always `yield` Meeting items.
+
+        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
+        needs.
+        """
+        for url in self._get_event_urls(response):
+            yield scrapy.Request(url, callback=self._parse_event, dont_filter=True)
+
+    def _parse_event(self, response):
+        """Parse the event page."""
+        title = self._parse_title(response)
+        meeting = Meeting(
+            title=title,
+            description=self._parse_description(response),
+            classification=BOARD,
+            start=self._parse_start(response),
+            end=self._parse_end(response),
+            time_notes='',
+            all_day=self._parse_all_day(response),
+            location=self._parse_location(response),
+            links=self._parse_links(response),
+            source=response.url,
+        )
+        meeting['id'] = self._get_id(meeting)
+        meeting['status'] = self._get_status(meeting)
+        return meeting
+
+    def _get_event_urls(self, response):
+        """
+        Get urls for all Board of ethics meetings on the page.
+        """
+        return [
+            response.urljoin(href)
+            for href in response.xpath('//a[contains(text(), "Board of Ethics")]'
+                                       ).css('a::attr(href)').extract()
+        ]
+
+    def _parse_location(self, response):
+        """
+        Parse or generate location. Url, latitude and longitude are all
+        optional and may be more trouble than they're worth to collect.
+        """
+        address = response.xpath('//div[@class="field event-location"]/descendant::*/text()'
+                                 ).extract()
+        for word in ['Location:', ', ', ' ']:
+            address.remove(word)
+        address = ' '.join(address)
+        return {
+            'address': address,
+            'name': '',
+        }
+
+    def _parse_all_day(self, response):
+        """
+        Parse or generate all-day status. Defaults to false.
+        """
+        date = response.xpath('//span[@class="date-display-single"]/descendant-or-self::*/text()'
+                              ).extract()
+        date = ''.join(date).upper()
+        return 'ALL DAY' in date
+
+    def _parse_title(self, response):
+        """Parse or generate event title."""
+        title = response.xpath('//h1/text()').extract_first()
+        if re.search(r"Special", title):
+            return "Special Board of Ethics Meeting"
+        elif re.search(r"Board of Ethics", title):
+            return "Board of Ethics"
+        else:
+            return title
+
+    def _parse_description(self, response):
+        """
+        Parse or generate event description.
+        """
+        category_field = response.xpath(
+            "//div[contains(., 'Category:') and contains(@class, 'field-label')]"
+        )
+        field_items = category_field.xpath("./following::div[contains(@class, 'field-items')]")
+        return ' '.join(
+            field_items.xpath('.//p/text()').extract() +
+            field_items.xpath('.//strong/text()').extract()
+        ).strip()
+
+    def _parse_start(self, response):
+        """
+        Parse start date and time.
+        """
+        start = response.xpath('//span[@class="date-display-single"]/descendant-or-self::*/text()'
+                               ).extract()
+        start = ''.join(start).upper()
+        start = start.split(' TO ')[0].strip()
+        start = start.replace('(ALL DAY)', '12:00AM')
+
+        return datetime.strptime(start, '%B %d, %Y %I:%M%p')
+
+    def _parse_end(self, response):
+        """
+        Parse end date and time.
+        """
+        date = response.xpath('//span[@class="date-display-single"]/descendant-or-self::*/text()'
+                              ).extract()
+        date = ''.join(date).upper()
+        date.replace('(ALL DAY)', 'TO 11:59PM')
+        start_end = date.split(' TO ')
+
+        if len(start_end) < 2:
+            return
+
+        end_time = start_end[1]
+        date = start_end[0][:start_end[0].rindex(' ')]
+        return datetime.strptime('{} {}'.format(date, end_time), '%B %d, %Y %I:%M%p')
+
+    def _parse_links(self, response):
+        files = response.css('span.file a')
+        return [{
+            'href': f.xpath('./@href').extract_first(),
+            'title': f.xpath('./text()').extract_first()
+        } for f in files]
---
+++ tests/test_cook_county_board_ethics.py
@@ -0,0 +1,69 @@
+from datetime import datetime
+
+from city_scrapers_core.constants import BOARD, PASSED
+from freezegun import freeze_time
+from tests.utils import file_response
+
+from city_scrapers.spiders.cook_county_board_ethics import CookCountyBoardEthicsSpider
+
+test_response = file_response(
+    'files/cook_county_board_ethics_event.html',
+    url='https://www.cookcountyil.gov/event/cook-county-board-ethics-meeting-3'
+)
+spider = CookCountyBoardEthicsSpider()
+
+freezer = freeze_time("2019-10-9")
+freezer.start()
+item = spider._parse_event(test_response)
+freezer.stop()
+
+
+def test_title():
+    assert item['title'] == 'Board of Ethics'
+
+
+def test_start():
+    assert item['start'] == datetime(2019, 8, 29, 14)
+
+
+def test_end():
+    assert item['end'] == datetime(2019, 8, 29, 16)
+
+
+def test_time_notes():
+    assert item['time_notes'] == ''
+
+
+def test_id():
+    assert item['id'] == 'cook_county_board_ethics/201908291400/x/board_of_ethics'
+
+
+def test_all_day():
+    assert item['all_day'] is False
+
+
+def test_classification():
+    assert item['classification'] == BOARD
+
+
+def test_status():
+    assert item['status'] == PASSED
+
+
+def test_location():
+    assert item['location'] == {
+        'name': '',
+        'address': '69 W. Washington Street, Suite 3040 Chicago IL 60602',
+    }
+
+
+def test_sources():
+    assert item['source'] == 'https://www.cookcountyil.gov/event/cook-county-board-ethics-meeting-3'
+
+
+def test_description():
+    assert item['description'] == ''
+
+
+def test_links():
+    assert item['links'] == []
