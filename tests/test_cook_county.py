f967c9a6c87b3fae6d853302ea8297c4b9d3042a
TJ <pyence@protonmail.com>
2019-10-16 17:20:16 +0200

    implement freezegun boilerplate use `file_response` from `city_scrapers_core.utils` changed test file and assert values

--- tests/test_cook_county.py
+++ tests/test_cook_county.py
@@ -1,28 +1,34 @@
 from datetime import datetime
+from os.path import dirname, join
 
 from city_scrapers_core.constants import ADVISORY_COMMITTEE, BOARD, COMMITTEE, PASSED
-from tests.utils import file_response
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
 
 from city_scrapers.spiders.cook_county import CookCountySpider
 
 test_response = file_response(
-    'files/cook_county_event.html',
-    url='https://www.cookcountyil.gov/event/cook-county-zoning-building-committee-6'
+    join(dirname(__file__), "files", "cook_county_event.html"),
+    url="https://www.cookcountyil.gov/event/public-hearing-zoning-board-appeals-2",
 )
 spider = CookCountySpider()
+
+freezer = freeze_time("2019-10-16")
+freezer.start()
 item = spider._parse_event(test_response)
+freezer.stop()
 
 
 def test_title():
-    assert item['title'] == 'ZBA Public Hearing'
+    assert item['title'] == 'Public Hearing - Zoning Board of Appeals'
 
 
 def test_start():
-    assert item['start'] == datetime(2017, 11, 15, 13)
+    assert item['start'] == datetime(2019, 10, 2, 13)
 
 
 def test_end():
-    assert item['end'] == datetime(2017, 11, 15, 15)
+    assert item['end'] == datetime(2019, 10, 2, 15)
 
 
 def test_time_notes():
@@ -30,7 +36,7 @@ def test_time_notes():
 
 
 def test_id():
-    assert item['id'] == 'cook_county/201711151300/x/zba_public_hearing'
+    assert item['id'] == 'cook_county/201910021300/x/public_hearing_zoning_board_of_appeals'
 
 
 def test_all_day():
@@ -68,20 +74,20 @@ def test_location():
 
 def test_sources():
     assert item['source'
-                ] == 'https://www.cookcountyil.gov/event/cook-county-zoning-building-committee-6'
+                ] == 'https://www.cookcountyil.gov/event/public-hearing-zoning-board-appeals-2'
 
 
 def test_description():
     assert item['description'] == (
-        'Public Hearing '
-        'A public hearing has been scheduled for the Cook County Zoning Board of Appeals on '
-        'Wednesday, November 15, 2017, 1:00PM at '
-        '69 W. Washington, 22nd Floor Conference Room, Chicago, Illinois 60602.'
+        'A Public Hearing has been scheduled for the Cook County Zoning Board of Appeals on '
+        '     Wednesday, October 2, 2019 at 1:00PM at '
+        '69 W. Washington - 22nd Floor Conference Room, Chicago, Illinois 60602 '
+        'Public Hearing'
     )
 
 
 def test_links():
     assert item['links'] == [{
-        'href': 'https://www.cookcountyil.gov/sites/default/files/zba_november_15_agenda.pdf',
-        'title': 'ZBA 11-15-2017 Agenda'
+        'href': 'https://www.cookcountyil.gov/sites/default/files/zba-agenda_10.2.19_-_final.pdf',
+        'title': 'ZBA 10-2-2019 Agenda'
     }]
