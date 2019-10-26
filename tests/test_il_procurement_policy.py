4f26f9a8110a045ccde566a5444990850ca898fd
jingwu21 <jingwu721@gmail.com>
2019-10-26 15:48:41 -0500

    updated test cases

--- tests/test_il_procurement_policy.py
+++ tests/test_il_procurement_policy.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from os.path import dirname, join
 
 import pytest
-from city_scrapers_core.constants import NOT_CLASSIFIED
+from city_scrapers_core.constants import BOARD, PASSED
 from city_scrapers_core.utils import file_response
 from freezegun import freeze_time
 
@@ -47,36 +47,36 @@ def test_time_notes():
     assert parsed_items[0]["time_notes"] == "EXPECTED TIME NOTES"
 
 
-# def test_id():
-#     assert parsed_items[0]["id"] == "EXPECTED ID"
+def test_id():
+    assert parsed_items[0]["id"] == "il_procurement_policy/201910151000/x/procurement_policy_board"
 
 
-# def test_status():
-#     assert parsed_items[0]["status"] == "EXPECTED STATUS"
+def test_status():
+    assert parsed_items[0]["status"] == PASSED
 
 
-# def test_location():
-#     assert parsed_items[0]["location"] == {
-#         "name": "EXPECTED NAME",
-#         "address": "EXPECTED ADDRESS"
-#     }
+def test_location():
+    assert parsed_items[0]["location"] == {
+        "name": "Stratton Office Building",
+        "address": "401 S Spring St, Springfield, IL 62704"
+    }
 
 
-# def test_source():
-#     assert parsed_items[0]["source"] == "EXPECTED URL"
+def test_source():
+    assert parsed_items[0]["source"] == "https://www2.illinois.gov/sites/ppb/Pages/future_board_minutes.aspx"
 
 
-# def test_links():
-#     assert parsed_items[0]["links"] == [{
-#       "href": "EXPECTED HREF",
-#       "title": "EXPECTED TITLE"
-#     }]
+def test_links():
+    assert parsed_items[0]["links"] == [{
+        "href": "https://www2.illinois.gov/sites/ppb/Pages/future_board_minutes.aspx",
+       "title": "October 15, 2019 Agenda"
+    }]
 
 
-# def test_classification():
-#     assert parsed_items[0]["classification"] == NOT_CLASSIFIED
+def test_classification():
+    assert parsed_items[0]["classification"] == BOARD
 
 
-# @pytest.mark.parametrize("item", parsed_items)
-# def test_all_day(item):
-#     assert item["all_day"] is False
+@pytest.mark.parametrize("item", parsed_items)
+def test_all_day(item):
+    assert item["all_day"] is False
