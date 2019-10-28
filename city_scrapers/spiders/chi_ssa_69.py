9ea9bfb36d94f3335ac2a5bc56c192dff6275125
Brian-Lee <BrianTrewLee@gmail.com>
2019-10-27 17:32:35 -0700

    changed chicaco time from America/Chicago to-05:00 as it may work better

--- city_scrapers/spiders/chi_ssa_69.py
+++ city_scrapers/spiders/chi_ssa_69.py
@@ -17,7 +17,7 @@ class ChiSsa69Spider(CityScrapersSpider):
         today = datetime.now()
         last_week = today - timedelta(days=7)
         in_two_months = today + timedelta(days=60)
-        chicago_timezone = "America/Chicago"
+        chicago_time_offset = "-05:00"
 
         return [(
             "https://www.googleapis.com/calendar/v3/calendars/gagdcchicago%40gmail.com/events"
@@ -25,8 +25,8 @@ class ChiSsa69Spider(CityScrapersSpider):
             "sanitizeHtml=true&timeMin={}T00:00:00{}&timeMax={}T00:00:00{}&"
             "key=AIzaSyC-KzxSLmmZitsCVv2DeueeUxoVwP0raVk"
         ).format(
-            chicago_timezone, last_week.strftime("%Y-%m-%d"), chicago_timezone,
-            in_two_months.strftime("%Y-%m-%d"), chicago_timezone
+            chicago_time_offset, last_week.strftime("%Y-%m-%d"), chicago_time_offset,
+            in_two_months.strftime("%Y-%m-%d"), chicago_time_offset
         )]
 
     def parse(self, response):
