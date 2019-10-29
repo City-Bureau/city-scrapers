fc9d02074402502fc8b915e03657214ab947a6ad
Chantel Miller <cmiller@civisanalytics.com>
2019-10-29 15:12:03 -0500

    ENH: get_status method kwargs

--- city_scrapers/spiders/il_arts_council.py
+++ city_scrapers/spiders/il_arts_council.py
@@ -35,7 +35,8 @@ class IlArtsCouncilSpider(CityScrapersSpider):
                     source=self._parse_source(response),
                 )
 
-                meeting["status"] = self._get_status(meeting)
+                meeting["status"] = self._get_status(item,
+                                                     text=item.xpath('td[2]/text()').get())
                 meeting["id"] = self._get_id(meeting)
 
                 yield meeting
