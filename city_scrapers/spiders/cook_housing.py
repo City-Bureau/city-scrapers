75a4d974e4e35fd3cc349c75d1cd68f451b96b9c
pjsier <pjsier@gmail.com>
2019-11-01 12:43:20 +0000

    Update cook_housing for site changes

--- city_scrapers/spiders/cook_housing.py
+++ city_scrapers/spiders/cook_housing.py
@@ -3,7 +3,7 @@ import re
 from collections import defaultdict
 from datetime import datetime
 
-from city_scrapers_core.constants import BOARD, NOT_CLASSIFIED
+from city_scrapers_core.constants import BOARD, COMMITTEE
 from city_scrapers_core.items import Meeting
 from city_scrapers_core.spiders import CityScrapersSpider
 from dateutil.relativedelta import relativedelta
@@ -40,35 +40,41 @@ class CookHousingSpider(CityScrapersSpider):
         now = datetime.now()
         for months_delta in range(-2, 3):
             month_str = (now + relativedelta(months=months_delta)).strftime("%Y-%m")
-            yield response.follow("/events/{}/".format(month_str), callback=self._parse_calendar)
+            yield response.follow("/events/{}/".format(month_str), callback=self._parse_events)
 
-    def _parse_calendar(self, response):
-        """
-        `parse` should always `yield` Meeting items.
-        """
-        year_str = re.search(r"\d{4}", response.url).group()
-        for event in response.css(".type-tribe_events"):
-            item = json.loads(event.attrib["data-tribejson"])
-            classification = self._parse_classification(item["title"])
-            if classification == BOARD:
-                start = self._parse_dt(item["startTime"], year_str)
-                meeting = Meeting(
-                    title=self._parse_title(item["title"]),
-                    description="",
-                    classification=BOARD,
-                    start=start,
-                    end=self._parse_dt(item["endTime"], year_str),
-                    all_day=False,
-                    time_notes="",
-                    location=self._parse_location(item),
-                    links=self.link_date_map[start.date()],
-                    source=item["permalink"],
-                )
+    def _parse_events(self, response):
+        for link in response.css(".item-wrapper .info a"):
+            link_text = " ".join(link.css("*::text").extract()).lower()
+            if "board" in link_text or "committee" in link_text:
+                yield response.follow(link.attrib["href"], callback=self._parse_detail)
 
-                meeting["status"] = self._get_status(meeting, text=item["excerpt"])
-                meeting["id"] = self._get_id(meeting)
+    def _parse_detail(self, response):
+        schema_text = response.css("script[type='application/ld+json']::text").extract()[-1]
+        schema_json = json.loads(schema_text)
+        if isinstance(schema_json, list):
+            item = schema_json[0]
+        else:
+            item = schema_json
 
-                yield meeting
+        start = self._parse_dt(item["startDate"])
+        title = self._parse_title(item["name"])
+        meeting = Meeting(
+            title=title,
+            description="",
+            classification=self._parse_classification(title),
+            start=start,
+            end=self._parse_dt(item["endDate"]),
+            all_day=False,
+            time_notes="",
+            location=self._parse_location(item),
+            links=self.link_date_map[start.date()],
+            source=response.url,
+        )
+
+        meeting["status"] = self._get_status(meeting, text=schema_text)
+        meeting["id"] = self._get_id(meeting)
+
+        yield meeting
 
     @staticmethod
     def _parse_title(title):
@@ -80,30 +86,33 @@ class CookHousingSpider(CityScrapersSpider):
     @staticmethod
     def _parse_classification(title):
         """Parse or generate classification from allowed options."""
-        if "board" in title.lower():
-            return BOARD
-        return NOT_CLASSIFIED
+        if "committee" in title.lower():
+            return COMMITTEE
+        return BOARD
 
     @staticmethod
-    def _parse_dt(date_str, year_str):
+    def _parse_dt(dt_str):
         """Parse start and end datetimes as a naive datetime object."""
-        return datetime.strptime(" ".join([year_str, date_str]), "%Y %B %d @ %I:%M %p")
+        return datetime.strptime(dt_str[:-6], "%Y-%m-%dT%H:%M:%S")
 
     def _parse_location(self, item):
         """Parse or generate location."""
-        desc_str = item["excerpt"]
-        if "175 W" in desc_str:
-            return self.location
-        addr_match = re.search(r"(?<=[\< ])\d+ .*(?=\<)", item["excerpt"])
-        if not addr_match:
+        if not item.get("location"):
             return {
                 "name": "TBD",
                 "address": "",
             }
-        addr_str = addr_match.group()
-        if "Chicago" not in addr_str:
-            addr_str += " Chicago, IL"
-        return {
-            "name": "",
-            "address": addr_str,
+
+        loc_obj = {
+            "name": item["location"]["name"],
+            "address": "",
         }
+        if item["location"].get("address"):
+            loc_obj["address"] = " ".join([
+                item["location"]["address"].get(p)
+                for p in ["streetAddress", "addressLocality", "addressRegion", "postalCode"]
+                if item["location"]["address"].get(p)
+            ])
+        if "175 W" in loc_obj["address"]:
+            return self.location
+        return loc_obj
