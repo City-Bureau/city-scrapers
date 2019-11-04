8cf9d90e04ae58f7a4f00dffa745c6f7c97857d9
Lucas Hiago <lucashiago63@gmail.com>
2019-11-04 08:45:31 -0300

    Refactor spider and test

--- city_scrapers/spiders/chi_ssa_50.py
+++ city_scrapers/spiders/chi_ssa_50.py
@@ -1,26 +1,23 @@
 from datetime import datetime
 
-from city_scrapers_core.constants import NOT_CLASSIFIED
+from city_scrapers_core.constants import COMMISSION
 from city_scrapers_core.items import Meeting
 from city_scrapers_core.spiders import CityScrapersSpider
 
 
 class ChiSsa50Spider(CityScrapersSpider):
     name = "chi_ssa_50"
-    agency = "Chicago Special Service Area"
+    agency = "Chicago Special Service Area #50 Calumet Heights/Avalon"
     timezone = "America/Chicago"
     start_urls = ["http://southeastchgochamber.org/special-service-area-50/"]
     base_xpath = '//div[@class="entry-content"]'
     item_xpath = base_xpath + '//p[@style="text-align: center;"]/text()'
 
     def parse(self, response):
-        title = self._parse_title(response)
-        description = self._parse_description(response)
-
         for item in response.xpath(self.item_xpath).getall():
             meeting = Meeting(
-                title=title,
-                description=description,
+                title=self._parse_title(item),
+                description=self._parse_description(item),
                 classification=self._parse_classification(item),
                 start=self._parse_start(item),
                 end=self._parse_end(item),
@@ -31,32 +28,25 @@ class ChiSsa50Spider(CityScrapersSpider):
                 source=self._parse_source(response),
             )
 
-            meeting["status"] = self._get_status(meeting)
+            meeting["status"] = self._get_status(meeting, text=item)
             meeting["id"] = self._get_id(meeting)
 
             yield meeting
 
-    def _parse_title(self, response):
-        title = response.css("title::text").get()
-        return title
+    def _parse_title(self, item):
+        return COMMISSION
 
-    def _parse_description(self, response):
-        description_xpath = '//div[@class="container-fluid page-full-description-above"]'
-        description_content = '//div[@class="row"]//div["col-xs-12"]//p/text()'
-        description = response.xpath(self.base_xpath + description_xpath +
-                                     description_content).get()
-        return description
+    def _parse_description(self, item):
+        return ""
 
     def _parse_classification(self, item):
-        return NOT_CLASSIFIED
+        return COMMISSION
 
     def _parse_start(self, item):
-        date = datetime.strptime(item, "%B %d, %Y")
-        return date
+        return datetime.strptime(item, "%B %d, %Y")
 
     def _parse_end(self, item):
-        date = datetime.strptime(item, "%B %d, %Y")
-        return date
+        return None
 
     def _parse_time_notes(self):
         return ""
@@ -71,7 +61,7 @@ class ChiSsa50Spider(CityScrapersSpider):
         }
 
     def _parse_links(self):
-        return [{"href": "", "title": ""}]
+        return []
 
     def _parse_source(self, response):
         return response.url
--- tests/test_chi_ssa_50.py
+++ tests/test_chi_ssa_50.py
@@ -1,8 +1,8 @@
-import json
 from datetime import datetime
 from os.path import dirname, join
 
 import pytest
+from city_scrapers_core.constants import COMMISSION
 from city_scrapers_core.utils import file_response
 from freezegun import freeze_time
 
@@ -21,54 +21,68 @@ parsed_items = [item for item in spider.parse(test_response)]
 
 freezer.stop()
 
-with open(join(dirname(__file__), "files", "chi_ssa_50.json")) as expected_file:
-    expected = json.load(expected_file)
+expected = {
+    "title": COMMISSION,
+    "description": "",
+    "classification": COMMISSION,
+    "start": "2019-04-17 00:00:00",
+    "end": None,
+    "all_day": False,
+    "time_notes": "",
+    "location": {
+        "address": "8334 S Stony Island Ave, Chicago, IL 60617",
+        "name": "Southeast Chicago Chamber"
+    },
+    "links": [],
+    "source": "http://southeastchgochamber.org/special-service-area-50/",
+    "status": "passed",
+    "id": "chi_ssa_50/201904170000/x/commission"
+}
 
 
 def test_title():
-    assert parsed_items[0]["title"] == expected[0]["title"]
+    assert parsed_items[0]["title"] == expected["title"]
 
 
 def test_description():
-    assert parsed_items[0]["description"] == expected[0]["description"]
+    assert parsed_items[0]["description"] == expected["description"]
 
 
 def test_start():
-    expected_date = expected[0]["start"].split()[0]
+    expected_date = expected["start"].split()[0]
     assert parsed_items[0]["start"] == datetime.strptime(expected_date, "%Y-%m-%d")
 
 
 def test_end():
-    expected_date = expected[0]["end"].split()[0]
-    assert parsed_items[0]["end"] == datetime.strptime(expected_date, "%Y-%m-%d")
+    assert parsed_items[0]["end"] == expected["end"]
 
 
 def test_time_notes():
-    assert parsed_items[0]["time_notes"] == expected[0]["time_notes"]
+    assert parsed_items[0]["time_notes"] == expected["time_notes"]
 
 
 def test_id():
-    assert parsed_items[0]["id"] == expected[0]["id"]
+    assert parsed_items[0]["id"] == expected["id"]
 
 
 def test_status():
-    assert parsed_items[0]["status"] == expected[0]["status"]
+    assert parsed_items[0]["status"] == expected["status"]
 
 
 def test_location():
-    assert parsed_items[0]["location"] == expected[0]["location"]
+    assert parsed_items[0]["location"] == expected["location"]
 
 
 def test_source():
-    assert parsed_items[0]["source"] == expected[0]["source"]
+    assert parsed_items[0]["source"] == expected["source"]
 
 
 def test_links():
-    assert parsed_items[0]["links"] == expected[0]["links"]
+    assert parsed_items[0]["links"] == expected["links"]
 
 
 def test_classification():
-    assert parsed_items[0]["classification"] == expected[0]["classification"]
+    assert parsed_items[0]["classification"] == expected["classification"]
 
 
 @pytest.mark.parametrize("item", parsed_items)
