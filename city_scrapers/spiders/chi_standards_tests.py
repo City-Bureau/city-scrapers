0fd9d9128ab060e31a98ec70261fa2eaafee7e61
Travis Fu <travis.fu@hotmail.com>
2019-11-04 14:51:08 +0000

    Add 0873 chi_standards_tests

---
+++ city_scrapers/spiders/chi_standards_tests.py
@@ -0,0 +1,76 @@
+import datetime
+
+from city_scrapers_core.constants import COMMITTEE
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+
+class ChiStandardsTestsSpider(CityScrapersSpider):
+    name = "chi_standards_tests"
+    agency = "Chicago Committee on Standards and Tests"
+    timezone = "America/Chicago"
+    start_urls = ["https://www.chicago.gov/city/en/depts/bldgs/supp_info/committee_on_standardsandtests.html"]
+
+    def parse(self, response):
+        """
+        `parse` should always `yield` Meeting items.
+        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
+        needs.
+        """
+        for item in response.xpath("//div[@class='col-xs-12']/table[1]//td/p"):            
+            if self._pass_filter(item) is False:
+                continue
+            meeting = Meeting(
+                title="Committee on Standards and Tests",
+                description="The Committee on Standards and Tests is a panel of distinguished building industry professionals and city staff that assist the Department of Buildings and the Chicago Fire Department with project-specific evaluation of building products, systems, equipment, and design solutions not addressed by the Chicago Building Code.\nThe Committee considers requests to use: (1) materials, products and systems shown to be equal to or better than the requirements of the Chicago Building Code; and (2) comprehensive building design solutions that may not meet a specific requirement of the Chicago Building Code but are shown to meet the intent of the code.",
+                classification=COMMITTEE,
+                start=self._parse_start(item),
+                end=None,
+                all_day=False,
+                time_notes="Meetings are scheduled by appointments and, thus, may not take place. Confirm details with the agency",
+                location={"address": "121 North LaSalle Street, Room 906, Chicago, Illinois 60602", "name": "City Hall"},
+                links=self._parse_links(item),
+                source=response.url,
+            )
+            meeting["status"] = self._get_status(meeting)
+            meeting["id"] = self._get_id(meeting)
+            yield meeting
+
+    def _pass_filter(self, item):
+        # If no meeting
+        if item.xpath('.//em/text()').get() == 'no meeting':
+            return False
+
+        # Check if date string 
+        months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+        date_str = item.xpath('.//text()').get()
+        if any(date_str.lower().startswith(month) for month in months):
+            return True
+        else:
+            return False
+
+    def _parse_start(self, item):
+        """Parse start datetime as a naive datetime object."""
+        year = item.xpath(".//../../../../../p[1]/strong/text()").get()
+        year = year.split()[0]
+        month_day = item.xpath(".//text()").get()
+        if month_day.startswith('Feb'):
+            # February has typo on website
+            month_day = month_day.replace('Feburary', 'February')
+        date = year + ' ' + month_day
+
+        date_obj = datetime.datetime.strptime(date, '%Y %B %d')
+        time_obj = datetime.time(13, 30, 0)
+        return datetime.datetime.combine(date_obj, time_obj)
+
+    def _parse_links(self, item):
+        """Parse or generate links."""
+        links = []
+        a_tag = item.xpath('.//a')
+        if a_tag:
+            href = 'chicago.gov' + a_tag.xpath('.//@href').get()
+            title = a_tag.xpath('.//text()').get()
+            links.append({
+                "href": 'chicago.gov' + a_tag.xpath('.//@href').get(), 
+                "title": a_tag.xpath('.//text()').get(),
+            })
+        return links
---
+++ tests/test_chi_standards_tests.py
@@ -0,0 +1,71 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest
+from city_scrapers_core.constants import COMMITTEE
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.chi_standards_tests import ChiStandardsTestsSpider
+
+test_response = file_response(
+    join(dirname(__file__), "files", "chi_standards_tests.html"),
+    url="https://www.chicago.gov/city/en/depts/bldgs/supp_info/committee_on_standardsandtests.html",
+)
+spider = ChiStandardsTestsSpider()
+
+freezer = freeze_time("2019-10-19")
+freezer.start()
+
+parsed_items = [item for item in spider.parse(test_response)]
+
+freezer.stop()
+
+
+def test_result_count():
+    assert len(parsed_items) == 11
+
+
+def test_title():
+    assert parsed_items[0]["title"] == "Committee on Standards and Tests"
+
+
+def test_description():
+    assert parsed_items[0]["description"] == """The Committee on Standards and Tests is a panel of distinguished building industry professionals and city staff that assist the Department of Buildings and the Chicago Fire Department with project-specific evaluation of building products, systems, equipment, and design solutions not addressed by the Chicago Building Code.\nThe Committee considers requests to use: (1) materials, products and systems shown to be equal to or better than the requirements of the Chicago Building Code; and (2) comprehensive building design solutions that may not meet a specific requirement of the Chicago Building Code but are shown to meet the intent of the code."""
+
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2019, 1, 16, 13, 30)
+    assert parsed_items[3]["start"] == datetime(2019, 10, 16, 13, 30)
+
+
+def test_time_notes():
+    assert parsed_items[0]["time_notes"] == "Meetings are scheduled by appointments and, thus, may not take place. Confirm details with the agency"
+
+
+def test_location():
+    assert parsed_items[0]["location"] == {
+        "name": "City Hall",
+        "address": "121 North LaSalle Street, Room 906, Chicago, Illinois 60602"
+    }
+
+
+def test_source():
+    assert parsed_items[0]["source"] == "https://www.chicago.gov/city/en/depts/bldgs/supp_info/committee_on_standardsandtests.html"
+
+
+def test_links():
+    assert parsed_items[0]["links"] == []
+    assert parsed_items[1]["links"] == [{
+      "href": "chicago.gov/content/dam/city/depts/bldgs/general/Standards_Tests/S&T%20Decisions%20April%202019.pdf",
+      "title": "Decisions"
+    }]
+
+
+def test_classification():
+    assert parsed_items[0]["classification"] == COMMITTEE
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_all_day(item):
+    assert item["all_day"] is False
