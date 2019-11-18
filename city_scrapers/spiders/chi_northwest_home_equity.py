1f05a3f64804a13597afe74df7fb708c99f7a65d
jkreynolds <jkr@udel.edu>
2019-11-18 14:18:25 -0800

    updated import statement to address isort issue

--- city_scrapers/spiders/chi_northwest_home_equity.py
+++ city_scrapers/spiders/chi_northwest_home_equity.py
@@ -1,7 +1,8 @@
+from datetime import datetime
+
 from city_scrapers_core.constants import COMMISSION
 from city_scrapers_core.items import Meeting
 from city_scrapers_core.spiders import CityScrapersSpider
-from datetime import datetime
 
 
 class ChiNorthwestHomeEquitySpider(CityScrapersSpider):
