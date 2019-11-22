64d5e9efb62d503d7cb334bcd1bf48e6d5fa0d83
Dominik Blek <zybex1986@gmail.com>
2019-11-22 19:29:18 +0100

    added the items above meetings and agendas, caught the NoneType meetings

--- city_scrapers/spiders/il_sports_facilities_authority.py
+++ city_scrapers/spiders/il_sports_facilities_authority.py
@@ -24,18 +24,38 @@ class IlSportsFacilitiesAuthoritySpider(CityScrapersSpider):
         needs.
         """
         location = self._parse_location(response)
+        for item in response.css('div.wpb_text_column div.wpb_wrapper div.inner-text h2'):
+            meeting = Meeting(
+                title=self._parse_h2_title(item),
+                description='',
+                classification=BOARD,
+                start=self._parse_h2_start(item),
+                end=None,
+                all_day=False,
+                time_notes='',
+                location=self._parse_location(item),
+                links=self._parse_links(item),
+                source=response.url,
+            )
+            try:
+                meeting['status'] = self._get_status(meeting)
+                meeting['id'] = self._get_id(meeting)
+            except TypeError:
+                continue
+            yield meeting
+
         for item in response.css('div.wpb_text_column div.wpb_wrapper p'):
             meeting = Meeting(
                 title=self._parse_title(item),
                 description='',
-                classification=self._parse_classification(item),
+                classification=BOARD,
                 start=self._parse_start(item),
                 end=None,
                 all_day=False,
                 time_notes='',
                 location=location,
                 links=self._parse_links(item),
-                source=self._parse_source(response),
+                source=response.url,
             )
             try:
                 meeting['status'] = self._get_status(meeting)
@@ -45,22 +65,19 @@ class IlSportsFacilitiesAuthoritySpider(CityScrapersSpider):
             yield meeting
 
     def _parse_title(self, item):
-        """Parse or generate meeting title."""
+        """Parse meeting title."""
         return item.css('::text').re_first(r'.*[a-zA-Z] Meeting')
 
-    def _parse_classification(self, item):
-        """Parse or generate classification from allowed options."""
-        return BOARD
+    def _parse_h2_title(self, item):
+        """Parse meeting title from the h2 tag."""
+        return item.css('::text').re_first(r'.*[a-zA-Z] Meeting')
 
-    def _parse_location(self, response):
+    def _parse_location(self, item):
         """Parse the location of the meeting."""
-        if 'Guaranteed Rate Field, 333 West 35th Street, Chicago, IL' not in ' '.join(
-            response.css('div .wpb_wrapper p strong::text').getall()
-        ):
-            return response.css('div .wpb_wrapper .inner-test h2::text'
-                                ).re(r'\b \d* [a-zA-Z].*, [0-9][a-zA-Z].*')
-        else:
-            return self.location
+        return {
+            'name': '',
+            'address': item.css('::text').re_first(r'\b \d* [a-zA-Z].*, [0-9][a-zA-Z].*').lstrip(' ')
+        }
 
     def _parse_start(self, item):
         """Parse start datetime as a naive datetime object."""
@@ -72,13 +89,17 @@ class IlSportsFacilitiesAuthoritySpider(CityScrapersSpider):
                 val = datetime.strptime(parsed, '%m.%d.%y') + timedelta(hours=10)
             return val
 
+    def _parse_h2_start(self, item):
+        """Parse start datetime from h2 tag."""
+        parsed = item.css('::text').re_first(
+            r'[a-zA-Z]* [0-9]*, [0-9]* at [0-9]*[:][0-9]* [APM.]*'
+        ).replace('.', '')
+        return datetime.strptime(parsed, '%B %d, %Y at %I:%M %p')
+
+
     def _parse_links(self, item):
         """Parse or generate links."""
         return [{
             "href": parsed.xpath('@href').get(),
             "title": parsed.css('::text').get()
         } for parsed in item.css('a')]
-
-    def _parse_source(self, response):
-        """Parse or generate source."""
-        return response.url
