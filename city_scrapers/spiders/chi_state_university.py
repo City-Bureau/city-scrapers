8e76f6ef767fa968517f6fa7832aa0d2ea3384d1
Lucas Hiago <lucashiago63@gmail.com>
2019-12-03 17:47:35 -0300

    Finish spider chi_state_university

--- city_scrapers/spiders/chi_state_university.py
+++ city_scrapers/spiders/chi_state_university.py
@@ -1,4 +1,6 @@
-from city_scrapers_core.constants import NOT_CLASSIFIED
+from datetime import datetime
+
+from city_scrapers_core.constants import BOARD
 from city_scrapers_core.items import Meeting
 from city_scrapers_core.spiders import CityScrapersSpider
 
@@ -10,69 +12,62 @@ class ChiStateUniversitySpider(CityScrapersSpider):
     start_urls = ["https://www.csu.edu/boardoftrustees/dates.htm"]
 
     def parse(self, response):
-        """
-        `parse` should always `yield` Meeting items.
-
-        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
-        needs.
-        """
-        for item in response.css(".meetings"):
+        for item in response.css(".table tr td li"):
+            extracted = item.css("::text").extract()
             meeting = Meeting(
-                title=self._parse_title(item),
-                description=self._parse_description(item),
-                classification=self._parse_classification(item),
-                start=self._parse_start(item),
-                end=self._parse_end(item),
-                all_day=self._parse_all_day(item),
-                time_notes=self._parse_time_notes(item),
-                location=self._parse_location(item),
-                links=self._parse_links(item),
+                title=self._parse_title(response),
+                classification=self._parse_classification(),
+                start=self._parse_start(extracted),
+                end=self._parse_end(extracted),
+                all_day=self._parse_all_day(extracted),
+                time_notes=self._parse_time_notes(extracted),
+                location=self._parse_location(),
                 source=self._parse_source(response),
             )
 
-            meeting["status"] = self._get_status(meeting)
+            if "RESCHEDULED" in extracted:
+                meeting["status"] = self._get_status(meeting, text="Meeting is Rescheduled")
+            else:
+                meeting["status"] = self._get_status(meeting, text="Meeting is Confirmed")
             meeting["id"] = self._get_id(meeting)
 
             yield meeting
 
-    def _parse_title(self, item):
-        """Parse or generate meeting title."""
-        return ""
+    def _parse_title(self, response):
+        title = response.css(".goldenTitle::text").extract_first()
+        return title
 
-    def _parse_description(self, item):
-        """Parse or generate meeting description."""
-        return ""
-
-    def _parse_classification(self, item):
-        """Parse or generate classification from allowed options."""
-        return NOT_CLASSIFIED
+    def _parse_classification(self):
+        return BOARD
 
     def _parse_start(self, item):
-        """Parse start datetime as a naive datetime object."""
-        return None
+        wrong_char = "\xa0"
+        time = item[0].replace(wrong_char, '').strip()
+        date = None
+        try:
+            date = datetime.strptime(time, "%A, %B %d, %Y")
+        except Exception:
+            print("Can't get a date")
+            date = datetime.now()
+        return date
 
     def _parse_end(self, item):
-        """Parse end datetime as a naive datetime object. Added by pipeline if None"""
         return None
 
     def _parse_time_notes(self, item):
-        """Parse any additional notes on the timing of the meeting"""
-        return ""
+        notes = ""
+        if "RESCHEDULED" in item:
+            notes = "Rescheduled to {}".format(item.pop())
+        return notes
 
     def _parse_all_day(self, item):
-        """Parse or generate all-day status. Defaults to False."""
         return False
 
-    def _parse_location(self, item):
-        """Parse or generate location."""
+    def _parse_location(self):
         return {
-            "address": "",
-            "name": "",
+            "address": "Room 415",
+            "name": "Gwendolyn Brooks Library Auditorium",
         }
-
-    def _parse_links(self, item):
-        """Parse or generate links."""
-        return [{"href": "", "title": ""}]
 
     def _parse_source(self, response):
         """Parse or generate source."""
