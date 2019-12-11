0ade91f87221f36fbb86307f87b0f812e57694fa
Burak Can Kahraman <brkcnkhrmn@gmail.com>
2019-12-11 16:23:58 +0300

    Tests are implemented for multiple URLs.

--- city_scrapers/spiders/il_elections.py
+++ city_scrapers/spiders/il_elections.py
@@ -25,8 +25,11 @@ class IlElectionsSpider(CityScrapersSpider):
         Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
         needs.
         """
+        # parse meeting minutes and addresses first to associate them with the agenda
         self._parse_minutes(response)
         self._parse_addresses(response)
+
+        # parse the agenda
         yield scrapy.Request(
             "https://www.elections.il.gov/AboutTheBoard/Agenda.aspx",
             callback=self._parse_agenda,
@@ -41,6 +44,15 @@ class IlElectionsSpider(CityScrapersSpider):
             parsed_date = datetime.strptime(date, "%a, %B %d, %Y")
             link = minute.css("::attr(href)").extract_first()
             self.meeting_minutes[parsed_date] = link
+
+    def _parse_addresses(self, response):
+        addr_divs = response.css(".footer-address > div")
+        for addr_div in addr_divs:
+            addr_name = addr_div.css("p::text").extract_first()
+            address_lines = addr_div.css("div > div::text").extract()
+            address = " ".join([line.strip() for line in address_lines if line.strip() != ""])
+            # remove "Office" from addr_name, e.g. "Springfield Office"
+            self.addresses[addr_name.split()[0]] = address
 
     def _parse_agenda(self, response):
         """Parse board agenda."""
@@ -102,21 +114,12 @@ class IlElectionsSpider(CityScrapersSpider):
     def _parse_location(self, item):
         """Parse or generate location."""
         name = item.css("td")[2].css("::text").extract_first().strip()
-        if name in self.addresses:
-            address = self.addresses[name]
+        address = self.addresses.get(name, "")
 
         return {
             "address": address,
             "name": name,
         }
-
-    def _parse_addresses(self, response):
-        addr_divs = response.css(".footer-address > div")
-        for addr_div in addr_divs:
-            addr_name = addr_div.css("p::text").extract_first()
-            address_lines = addr_div.css("div > div::text").extract()
-            address = " ".join([line.strip() for line in address_lines if line.strip() != ""])
-            self.addresses[addr_name.split()[0]] = address
 
     def _parse_links(self, item, response, start):
         """Parse or generate links."""
@@ -124,8 +127,11 @@ class IlElectionsSpider(CityScrapersSpider):
         href = item.css("td")[0].css("::attr(href)").extract_first()
         if href:
             href = response.urljoin(href)
+            title = "Agenda"
         else:
             href = ""
+            title = ""
+        links.append({"href": href, "title": title})
 
         if start.date() in [i.date() for i in self.meeting_minutes]:
             links.append({
@@ -133,9 +139,8 @@ class IlElectionsSpider(CityScrapersSpider):
                     response.urljoin(
                         self.meeting_minutes[datetime.combine(start.date(), datetime.min.time())]
                     ),
-                "title": ""
+                "title": "Minutes"
             })
-        links.append({"href": href, "title": ""})
         return links
 
     def _parse_source(self, response):
--- tests/test_il_elections.py
+++ tests/test_il_elections.py
@@ -2,37 +2,51 @@ from datetime import datetime
 from os.path import dirname, join
 
 import pytest
-from city_scrapers_core.constants import NOT_CLASSIFIED
+from city_scrapers_core.constants import BOARD, PASSED, TENTATIVE
 from city_scrapers_core.utils import file_response
 from freezegun import freeze_time
+from scrapy.http import HtmlResponse
 
 from city_scrapers.spiders.il_elections import IlElectionsSpider
-
-test_response = file_response(
-    join(dirname(__file__), "files", "il_elections.html"),
-    url="https://www.elections.il.gov/AboutTheBoard/Agenda.aspx",
-)
-spider = IlElectionsSpider()
 
 freezer = freeze_time("2019-12-04")
 freezer.start()
 
-parsed_items = [item for item in spider.parse(test_response)]
+test_response = file_response(
+    join(dirname(__file__), "files", "il_elections_minutes.html"),
+    url="https://www.elections.il.gov/AboutTheBoard/MeetingMinutesAll.aspx",
+)
+
+with open(join(dirname(__file__), "files", "il_elections_agenda.html"), "r") as f:
+    file_content = f.read()
+
+feed_url = "https://www.elections.il.gov/AboutTheBoard/Agenda.aspx"
+test_feed_response = HtmlResponse(url=feed_url, body=str.encode(file_content))
+
+spider = IlElectionsSpider()
+spider._parse_minutes(test_response)
+spider._parse_addresses(test_response)
+parsed_items = [item for item in spider._parse_agenda(test_feed_response)]
 
 freezer.stop()
 
 
 def test_title():
     assert parsed_items[0]["title"] == "Illinois State Board of Elections"
+    assert parsed_items[1]["title"] == "Illinois State Board of Elections"
 
 
 def test_description():
     assert parsed_items[0]["description"
                            ] == "Statutory meeting for election of Chairman and Vice Chairman."
+    assert parsed_items[1]["description"] == ""
+    assert parsed_items[6][
+        "description"] == "Statutory date to certify the ballot for the March Primary Election."
 
 
 def test_start():
     assert parsed_items[0]["start"] == datetime(2019, 7, 1, 10, 30)
+    assert parsed_items[1]["start"] == datetime(2019, 8, 21, 10, 30)
 
 
 def test_end():
@@ -48,11 +62,21 @@ def test_id():
 
 
 def test_status():
-    assert parsed_items[0]["status"] == "passed"
+    assert parsed_items[0]["status"] == PASSED
+    assert parsed_items[5]["status"] == TENTATIVE
 
 
 def test_location():
-    assert parsed_items[0]["location"] == {"name": "Springfield", "address": ""}
+    assert parsed_items[0]["location"] == {
+        "name": "Springfield",
+        "address":
+            "2329 S. MacArthur Blvd. Springfield, IL 62704 Phone: 217-782-4141 Fax: 217-782-5959"
+    }
+    assert parsed_items[5]["location"] == {
+        "address":
+            "100 W. Randolph, Suite 14-100 Chicago, IL 60601 Phone: 312-814-6440 Fax: 312-814-6485",
+        "name": "Chicago"
+    }
 
 
 def test_source():
@@ -62,14 +86,20 @@ def test_source():
 def test_links():
     assert parsed_items[0]["links"] == [{
         "href":
-            "https://www.elections.il.gov/" +
-            "DocDisplay.aspx?Doc=/Downloads/AboutTheBoard/PDF/07_01_19Agenda.pdf",
-        "title": ""
+            "https://www.elections.il.gov/DocDisplay.aspx" +
+            "?Doc=/Downloads/AboutTheBoard/PDF/07_01_19Agenda.pdf",
+        "title": "Agenda"
+    }, {
+        "href":
+            "https://www.elections.il.gov/DocDisplay.aspx" +
+            "?Doc=/Downloads/AboutTheBoard/PDF/July 1-19 regular meeting minutes.pdf",
+        "title": "Minutes"
     }]
+    assert parsed_items[7]["links"] == [{"href": "", "title": ""}]
 
 
 def test_classification():
-    assert parsed_items[0]["classification"] == NOT_CLASSIFIED
+    assert parsed_items[0]["classification"] == BOARD
 
 
 @pytest.mark.parametrize("item", parsed_items)
