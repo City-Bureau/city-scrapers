a560e74f8ebfd79da89a5e0c69884ed8c7b28afd
Burak Can Kahraman <brkcnkhrmn@gmail.com>
2019-12-11 13:06:51 +0300

    Handled multiple urls.

--- city_scrapers/spiders/il_elections.py
+++ city_scrapers/spiders/il_elections.py
@@ -1,7 +1,7 @@
 from datetime import datetime
-from urllib.parse import urljoin
 
-from city_scrapers_core.constants import NOT_CLASSIFIED
+import scrapy
+from city_scrapers_core.constants import BOARD
 from city_scrapers_core.items import Meeting
 from city_scrapers_core.spiders import CityScrapersSpider
 
@@ -10,8 +10,13 @@ class IlElectionsSpider(CityScrapersSpider):
     name = "il_elections"
     agency = "Illinois State Board of Elections"
     timezone = "America/Chicago"
-    start_urls = ["https://www.elections.il.gov/AboutTheBoard/Agenda.aspx"]
+    start_urls = ["https://www.elections.il.gov/AboutTheBoard/MeetingMinutesAll.aspx"]
     custom_settings = {"ROBOTSTXT_OBEY": False}
+
+    def __init__(self):
+        self.meeting_minutes = dict()
+        self.addresses = dict()
+        super().__init__()
 
     def parse(self, response):
         """
@@ -20,19 +25,39 @@ class IlElectionsSpider(CityScrapersSpider):
         Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
         needs.
         """
-        meetings = response.css(".SearchListTable").xpath("./tr")[1:-1]
+        self._parse_minutes(response)
+        self._parse_addresses(response)
+        yield scrapy.Request(
+            "https://www.elections.il.gov/AboutTheBoard/Agenda.aspx",
+            callback=self._parse_agenda,
+            dont_filter=True,
+        )
 
+    def _parse_minutes(self, response):
+        """Parse all past board meeting minutes, store for association to meetings."""
+        minutes = response.css("#ContentPlaceHolder1_gvMeetingMinutes td > a")
+        for minute in minutes:
+            date = minute.css("::text").extract_first().strip()
+            parsed_date = datetime.strptime(date, "%a, %B %d, %Y")
+            link = minute.css("::attr(href)").extract_first()
+            self.meeting_minutes[parsed_date] = link
+
+    def _parse_agenda(self, response):
+        """Parse board agenda."""
+        meetings = response.css("#ContentPlaceHolder1_gvAgenda > tr")[1:-1]
         for item in meetings:
+            start = self._parse_start(item)
+            location = self._parse_location(item)
             meeting = Meeting(
                 title=self._parse_title(item),
                 description=self._parse_description(item),
                 classification=self._parse_classification(item),
-                start=self._parse_start(item),
+                start=start,
                 end=self._parse_end(item),
                 all_day=self._parse_all_day(item),
                 time_notes=self._parse_time_notes(item),
-                location=self._parse_location(item),
-                links=self._parse_links(item, response),
+                location=location,
+                links=self._parse_links(item, response, start),
                 source=self._parse_source(response),
             )
 
@@ -47,17 +72,17 @@ class IlElectionsSpider(CityScrapersSpider):
 
     def _parse_description(self, item):
         """Parse or generate meeting description."""
-        desc = item.xpath("./td[4]/text()").extract_first().strip()
+        desc = item.css("td")[3].css("::text").extract_first().strip()
         return desc
 
     def _parse_classification(self, item):
         """Parse or generate classification from allowed options."""
-        return NOT_CLASSIFIED
+        return BOARD
 
     def _parse_start(self, item):
         """Parse start datetime as a naive datetime object."""
-        date = item.xpath("./td[1]").re(r"[A-Z][a-z]{2}, .*20\d\d")[0]
-        time = item.xpath("./td[2]").re(r"\d{2}:\d{2} [a|p].m")[0].replace(".", "")
+        date = item.css("td")[0].css("::text").extract_first().strip()
+        time = item.css("td")[1].css("::text").extract_first().strip().replace(".", "")
         dt = date + " " + time
         parsed_dt = datetime.strptime(dt, "%a, %B %d, %Y %I:%M %p")
         return parsed_dt
@@ -76,17 +101,42 @@ class IlElectionsSpider(CityScrapersSpider):
 
     def _parse_location(self, item):
         """Parse or generate location."""
-        name = item.xpath("./td[3]/text()").extract_first()
+        name = item.css("td")[2].css("::text").extract_first().strip()
+        if name in self.addresses:
+            address = self.addresses[name]
+
         return {
-            "address": "",
+            "address": address,
             "name": name,
         }
 
-    def _parse_links(self, item, response):
+    def _parse_addresses(self, response):
+        addr_divs = response.css(".footer-address > div")
+        for addr_div in addr_divs:
+            addr_name = addr_div.css("p::text").extract_first()
+            address_lines = addr_div.css("div > div::text").extract()
+            address = " ".join([line.strip() for line in address_lines if line.strip() != ""])
+            self.addresses[addr_name.split()[0]] = address
+
+    def _parse_links(self, item, response, start):
         """Parse or generate links."""
-        href = item.xpath("./td[1]//@href").extract_first()
-        href = urljoin(response.url, href)
-        return [{"href": href, "title": ""}]
+        links = []
+        href = item.css("td")[0].css("::attr(href)").extract_first()
+        if href:
+            href = response.urljoin(href)
+        else:
+            href = ""
+
+        if start.date() in [i.date() for i in self.meeting_minutes]:
+            links.append({
+                "href":
+                    response.urljoin(
+                        self.meeting_minutes[datetime.combine(start.date(), datetime.min.time())]
+                    ),
+                "title": ""
+            })
+        links.append({"href": href, "title": ""})
+        return links
 
     def _parse_source(self, response):
         """Parse or generate source."""
