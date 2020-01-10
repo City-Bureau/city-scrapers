a5d2fba9bfbf8fac62faca6131b094a6e06000cf
Mc33k <mc33k@protonmail.com>
2020-01-10 21:35:06 +0100

    Fixed _parse_links and added changes based on a meetings update for 2020.

--- city_scrapers/spiders/chi_ssa_48.py
+++ city_scrapers/spiders/chi_ssa_48.py
@@ -25,9 +25,7 @@ class ChiSsa48Spider(CityScrapersSpider):
 
         for item_date, item_info in zip(meeting_date, meeting_info):
 
-            start_time, end_time, parsed_meeting_date = self._parse_start_end(
-                item_date, item_info, meeting_year
-            )
+            start_time, end_time = self._parse_start_end(item_date, item_info, meeting_year)
 
             meeting = Meeting(
                 title="Commission",
@@ -38,7 +36,7 @@ class ChiSsa48Spider(CityScrapersSpider):
                 all_day=False,
                 time_notes="",
                 location=self._parse_location(item_info),
-                links=self._parse_links(parsed_meeting_date, meeting_links),
+                links=self._parse_links(start_time, meeting_links),
                 source=self._parse_source(response),
             )
 
@@ -67,41 +65,42 @@ class ChiSsa48Spider(CityScrapersSpider):
             year + " " + meeting_date + " " + parse_time[1], "%Y %A %B %d %I:%M%p"
         )
 
-        parsed_meeting_date = datetime.strptime(year + " " + meeting_date, "%Y %A %B %d")
-
-        return start_time, end_time, parsed_meeting_date
+        return start_time, end_time
 
     def _parse_location(self, info):
         """Parse or generate location."""
         element = info.xpath(".//text()").getall()
-        index = element[1].rfind("/n/t/t")
-        name = element[1][index + 1:].strip()
 
-        index = element[2].rfind("/r/n")
-        address = element[2][index + 1:].strip()
+        name = element[1].replace("\r", "").replace("\t", "").strip()
+        """If the location is not known, the element contains only two strings """
+        if len(element) > 2:
+            address = element[2].replace("\r", "").replace("\n", "").strip()
+        else:
+            address = name
 
         return {
             "address": address,
             "name": name,
         }
 
-    def _parse_links(self, date, meeting_links):
+    def _parse_links(self, start_time, meeting_links):
         """Parse or generate links."""
         links = []
         for href in meeting_links.xpath('.//a'):
             title = href.xpath('text()').get().strip()
             minutes_date = title.split(" ")
-            """Verification, that selected link is link to meeting minutes"""
-            if minutes_date[1] != "Minutes":
+            """Verification, that selected link is a link to meeting minutes"""
+            if len(minutes_date) >= 2 and minutes_date[1] != "Minutes":
                 continue
             """Date format for the last meeting minutes is using 2019 instead of 19 format"""
-            if minutes_date[2][-4:-2] == "20":
+            if minutes_date[2][-3] != "/":
                 meeting_minutes_date = datetime.strptime(minutes_date[2], "%m/%d/%Y")
             else:
                 meeting_minutes_date = datetime.strptime(minutes_date[2], "%m/%d/%y")
 
-            if meeting_minutes_date == date:
+            if meeting_minutes_date.date() == start_time.date():
                 links.append({"title": title, "href": href.xpath('@href').get().strip()})
+
         return links
 
     def _parse_source(self, response):
