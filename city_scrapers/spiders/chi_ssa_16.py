c1660e9e670319f330b970d76643dfe343430f5a
Benjamin Fawcett <34294188+benjaminfawcett@users.noreply.github.com>
2020-02-09 22:48:33 -0500

    0558 spider chi ssa 16 (#947)

---
+++ city_scrapers/spiders/chi_ssa_16.py
@@ -0,0 +1,106 @@
+import datetime
+import re
+
+from city_scrapers_core.constants import COMMISSION
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+
+
+class ChiSsa16Spider(CityScrapersSpider):
+    name = "chi_ssa_16"
+    agency = "Chicago Special Service Area #16"
+    timezone = "America/Chicago"
+    start_urls = ["https://greektownchicago.org/about/ssa-16/"]
+
+    def parse(self, response):
+        # Meeting entries are contained in a list item without any specific class or id designation.
+        # Because of this, I filtered the present list items on the page with the ", 20" string
+        # (every meeting li has this in the present text from the year listed),
+        # which indicates the meeting occurred or is scheduled to occur
+        # and therefore contains relevant information to document.
+        for item in response.xpath('//li[contains(text(), ", 20")]'):
+            meeting = Meeting(
+                title=self._parse_title(item),
+                description=self._parse_description(item),
+                classification=self._parse_classification(item),
+                start=self._parse_start(item),
+                end=self._parse_end(item),
+                all_day=self._parse_all_day(item),
+                time_notes=self._parse_time_notes(item),
+                location=self._parse_location(item),
+                links=self._parse_links(item),
+                source=self._parse_source(response),
+            )
+            meeting["status"] = self._get_status(meeting)
+            meeting["id"] = self._get_id(meeting)
+
+            yield meeting
+
+    def _parse_title(self, item):
+        # There was no variation in types of meetings, so this was applicable for all.
+        return "Tax Commission"
+
+    def _parse_description(self, item):
+        return ""
+
+    def _parse_classification(self, item):
+        return COMMISSION
+
+    def _parse_start(self, item):
+        # Extracted all alphanumeric characters from the text of each meeting,
+        # which contained the dates of the meeting, then split into list containing M, D, Y
+        date = re.sub(r"[^a-zA-Z0-9]+", " ", item.xpath("text()").get()).split()
+
+        # Extracted just alphabetical characters from month, then used built-in strptime function
+        # in datetime to parse numeric month from string
+        month = re.sub(r"[^a-zA-Z]+", " ", date[0])
+        month = datetime.datetime.strptime(month, "%B").month
+
+        # Extracted numbers from day and year entries of date list to remove any errant characters
+        # (i.e. one entry said January 28th where all others read January 28).
+        day = int(re.sub(r"[^0-9]+", " ", date[1]))
+        year = int(re.sub(r"[^0-9]+", " ", date[2]))
+
+        # Extracts accompanying meeting information to extract meeting time
+        time = item.xpath(
+            'ancestor::div[@class="gdc_row"]'
+            '/descendant::p[contains(text(), "60661")]/text()'
+        ).get()
+        time = re.sub(r"[^a-zA-Z0-9:]+", "", time)
+        time = re.findall(r'\d{1,2}:\d{2}(?:AM|PM|am|pm)', time)[0].upper()
+
+        hour = datetime.datetime.strptime(time, '%I:%M%p').hour
+        minute = datetime.datetime.strptime(time, '%I:%M%p').minute
+
+        return datetime.datetime(year, month, day, hour, minute)
+
+    def _parse_end(self, item):
+        # Meeting adjournment information contained in files present in Minutes documents
+        # attached to each meeting that are un-optimized scanned docs.
+        return None
+
+    def _parse_time_notes(self, item):
+        # No other details present
+        return ""
+
+    def _parse_all_day(self, item):
+        return False
+
+    def _parse_location(self, item):
+        # Meetings occurred at same location for all years documented
+        return {
+            "address": "306 S. Halsted St, 2nd Floor, Chicago, IL 60661",
+            "name": "SSA #16 Office",
+        }
+
+    def _parse_links(self, item):
+        # Most entries contained a Minutes document, but some not yet posted
+        # or otherwise have not occurred.
+        # When no anchor tag found, returns empty JSON element.
+        if (item.xpath("a/@href").get() is None):
+            return []
+
+        return [{"href": item.xpath("a/@href").get(), "title": item.xpath("a/text()").get()}]
+
+    def _parse_source(self, response):
+        return response.url
---
+++ tests/test_chi_ssa_16.py
@@ -0,0 +1,97 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest
+from city_scrapers_core.constants import COMMISSION, PASSED, TENTATIVE
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.chi_ssa_16 import ChiSsa16Spider
+
+test_response = file_response(
+    join(dirname(__file__), "files", "chi_ssa_16.html"),
+    url="https://greektownchicago.org/about/ssa-16/",
+)
+
+spider = ChiSsa16Spider()
+
+freezer = freeze_time("2020-02-06")
+freezer.start()
+
+parsed_items = [item for item in spider.parse(test_response)]
+
+freezer.stop()
+
+
+def test_num():
+    assert len(parsed_items) == 51
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_title(item):
+    assert item["title"] == "Tax Commission"
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_description(item):
+    assert item["description"] == ""
+
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2020, 1, 23, 14, 0)
+
+
+def test_end():
+    assert parsed_items[0]["end"] is None
+
+
+def test_time_notes():
+    assert parsed_items[0]["time_notes"] == ""
+
+
+def test_id():
+    str0 = "chi_ssa_16/202001231400/x/tax_commission"
+    assert parsed_items[0]["id"] == str0
+    str1 = "chi_ssa_16/202002271400/x/tax_commission"
+    assert parsed_items[1]["id"] == str1
+
+
+def test_status():
+    assert parsed_items[0]["status"] == PASSED
+    assert parsed_items[1]["status"] == TENTATIVE
+    assert parsed_items[-1]["status"] == PASSED
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_location(item):
+    assert item["location"] == {
+        "name": "SSA #16 Office",
+        "address": "306 S. Halsted St, 2nd Floor, Chicago, IL 60661"
+    }
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_source(item):
+    assert item["source"] == "https://greektownchicago.org/about/ssa-16/"
+
+
+def test_links():
+    assert parsed_items[0]["links"] == []
+
+    href11 = "https://5taz8eljj63owlf43qy49n1e-wpengine.netdna-ssl.com"\
+        "/wp-content/uploads/2019/06/SSA-16-January-24-2019-Meeting-Minutes.pdf"
+    assert parsed_items[11]["links"] == [{"href": href11, "title": "Minutes"}]
+
+    href12 = "https://5taz8eljj63owlf43qy49n1e-wpengine.netdna-ssl.com"\
+        "/wp-content/uploads/2019/06/SSA-16-February-28-2019-Meeting-Minutes.pdf"
+    assert parsed_items[12]["links"] == [{"href": href12, "title": "Minutes"}]
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_classification(item):
+    assert item["classification"] == COMMISSION
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_all_day(item):
+    assert item["all_day"] is False
