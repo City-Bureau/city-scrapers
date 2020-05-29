d8a7f97d9b3df2558c9d4a9759f228633a470cde
DiegoCaraballo <diegocaraballo84@gmail.com>
2020-05-29 18:40:54 -0300

    Add spider chi_ssa_64

---
+++ tests/test_chi_ssa_64.py
@@ -0,0 +1,74 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest
+from city_scrapers_core.constants import COMMISSION
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.chi_ssa_64 import ChiSsa64Spider
+
+test_response = file_response(
+    join(dirname(__file__), "files", "chi_ssa_64.html"),
+    url="https://www.mpbhba.org/business-resources/",
+)
+spider = ChiSsa64Spider()
+
+freezer = freeze_time("2020-05-26")
+freezer.start()
+
+parsed_items = [item for item in spider.parse(test_response)]
+
+freezer.stop()
+
+
+def test_title():
+    assert parsed_items[0]["title"] == "Morgan Park Commission"
+
+
+def test_description():
+    assert parsed_items[0]["description"] == ""
+
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2019, 6, 5, 9, 0)
+
+
+def test_end():
+    assert parsed_items[0]["end"] is None
+
+
+def test_time_notes():
+    assert parsed_items[0]["time_notes"] == ""
+
+
+def test_id():
+    assert parsed_items[0]["id"] == "chi_ssa_64/201906050900/x/morgan_park_commission"
+
+
+def test_status():
+    assert parsed_items[0]["status"] == "passed"
+
+
+def test_location():
+    assert parsed_items[0]["location"] == {
+        "name": "Beverly Bank & Trust",
+        "address": "10258 S. Western Ave."
+    }
+
+
+def test_source():
+    assert parsed_items[0]["source"] == "https://www.mpbhba.org/business-resources/"
+
+
+def test_links():
+    assert parsed_items[0]["links"] == []
+
+
+def test_classification():
+    assert parsed_items[0]["classification"] == COMMISSION
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_all_day(item):
+    assert item["all_day"] is False
