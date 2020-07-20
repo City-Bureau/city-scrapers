b1c9c8a69aea9be3cb05a5a6fd6d5622c1bcba0e
pjsier <pjsier@gmail.com>
2020-07-20 13:44:00 +0000

    Fix cook_board locations

--- city_scrapers/spiders/cook_board.py
+++ city_scrapers/spiders/cook_board.py
@@ -35,7 +35,7 @@ class CookBoardSpider(LegistarSpider):
                 source=self.legistar_source(event),
             )
             meeting["status"] = self._get_status(
-                meeting, text=event["Meeting Location"]
+                meeting, text=str(event["Meeting Location"])
             )
             meeting["id"] = self._get_id(meeting)
             yield meeting
@@ -54,7 +54,9 @@ class CookBoardSpider(LegistarSpider):
         """
         Parse or generate location.
         """
-        address = item.get("Meeting Location", None)
+        address = item.get("Meeting Location")
+        if isinstance(address, dict):
+            address = address.get("label", "")
         if address:
             address = re.sub(
                 r"\s+", " ", re.sub(r"(\n)|(--em--)|(--em)|(em--)", " ", address),
