fcca613158c72247cc2077b54ce95de3858d8785
Donal O'Shea <donaloshea@outlook.com>
2020-07-23 16:14:23 +0100

    Added request code to retrieve subpages. Added code to retrieve start and end times from root page. Need to modify routines to retrieve data from subpages and format as meeting items

--- city_scrapers/spiders/chi_ohare_noise_2.py
+++ city_scrapers/spiders/chi_ohare_noise_2.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+from datetime import timedelta
+
+from scrapy import Request
+
 from city_scrapers_core.constants import NOT_CLASSIFIED
 from city_scrapers_core.items import Meeting
 from city_scrapers_core.spiders import CityScrapersSpider
@@ -7,7 +12,7 @@ class ChiOhareNoise2Spider(CityScrapersSpider):
     name = "chi_ohare_noise_2"
     agency = "Chicago O'Hare Noise Compatibility Commission"
     timezone = "America/Chicago"
-    start_urls = ["https://www.oharenoise.org/about-oncc/oncc-meetings/month.calendar/2019/02/03/-"]
+    start_urls = ["https://www.oharenoise.org/meetings-all/year.listevents/2020/07/22/-"]
 
     def parse(self, response):
         """
@@ -16,28 +21,37 @@ class ChiOhareNoise2Spider(CityScrapersSpider):
         Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
         needs.
         """
-        for item in response.css(".meetings"):
-            meeting = Meeting(
-                title=self._parse_title(item),
-                description=self._parse_description(item),
-                classification=self._parse_classification(item),
-                start=self._parse_start(item),
-                end=self._parse_end(item),
-                all_day=self._parse_all_day(item),
-                time_notes=self._parse_time_notes(item),
-                location=self._parse_location(item),
-                links=self._parse_links(item),
-                source=self._parse_source(response),
-            )
+        for item in response.css(".ev_td_li"):
+            surl = self._parse_url(item)
+            yield Request(url=response.urljoin(surl), callback=self._parse_location)
+#            stime = self._parse_start(item)
+#            print(response.urljoin(surl))
+#            meeting = Meeting(
+#                title=self._parse_title(item),
+#                description=self._parse_description(item),
+#                classification=self._parse_classification(item),
+#                start=stime,
+#                end=stime+timedelta(hours=1),
+#                all_day=self._parse_all_day(item),
+#                time_notes=self._parse_time_notes(item),
+#                location= None,
+#                links=self._parse_links(item),
+#                source=self._parse_source(response),
+#            )
+#
 
-            meeting["status"] = self._get_status(meeting)
-            meeting["id"] = self._get_id(meeting)
+            #meeting["status"] = self._get_status(meeting)
+            #meeting["id"] = self._get_id(meeting)
 
-            yield meeting
+            #yield meeting
 
     def _parse_title(self, item):
         """Parse or generate meeting title."""
-        return ""
+        return [i.strip() for i in item.xpath('p/a/text()').extract()][0]
+
+    def _parse_url(self, item):
+        """Parse or generate meeting title."""
+        return  [i.strip() for i in item.xpath('p/a/@href').extract()][0]
 
     def _parse_description(self, item):
         """Parse or generate meeting description."""
@@ -49,7 +63,7 @@ class ChiOhareNoise2Spider(CityScrapersSpider):
 
     def _parse_start(self, item):
         """Parse start datetime as a naive datetime object."""
-        return None
+        return datetime.strptime(item.xpath('p/text()').extract()[1].strip(), '%A, %B %d, %Y %I:%M%p')
 
     def _parse_end(self, item):
         """Parse end datetime as a naive datetime object. Added by pipeline if None"""
@@ -63,8 +77,10 @@ class ChiOhareNoise2Spider(CityScrapersSpider):
         """Parse or generate all-day status. Defaults to False."""
         return False
 
-    def _parse_location(self, item):
+    def _parse_location(self, response):
         """Parse or generate location."""
+        print('started')
+        print(response.text)
         return {
             "address": "",
             "name": "",
