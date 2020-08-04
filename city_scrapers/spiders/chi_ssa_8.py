9fdd0619e1e35009a3984120a460db1407c901dc
Rathnam <jeyachandran.rathnam@alumni.ou.edu>
2020-08-03 22:01:53 -0500

    0879 Remove test start URL

--- city_scrapers/spiders/chi_ssa_8.py
+++ city_scrapers/spiders/chi_ssa_8.py
@@ -14,7 +14,6 @@ class ChiSsa8Spider(CityScrapersSpider):
     agency = "Chicago Special Service Area #8 Lakeview East"
     timezone = "America/Chicago"
     start_urls = ["https://lakevieweast.com/ssa-8/"]
-    start_urls = ["http://localhost:8080/file.html"]
     monthDateRegex = r"(Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Sep(tember)?|Oct(ober)?|Nov(ember)?|Dec(ember)?)\s+\d{1,2}"
 
     months = {m.lower() for m in month_name[1:]}
