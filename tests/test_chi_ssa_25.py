6fe5d0ccc2f184982caaa0e7ce1e800a121baa14
pjsier <pjsier@gmail.com>
2020-08-07 16:07:21 +0000

    Rewrite chi_ssa_25

--- tests/test_chi_ssa_25.py
+++ tests/test_chi_ssa_25.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 from os.path import dirname, join
 
-import pytest
-from city_scrapers_core.constants import COMMISSION, PASSED, TENTATIVE
+import pytest  # noqa
+from city_scrapers_core.constants import COMMISSION, PASSED
 from city_scrapers_core.utils import file_response
 from freezegun import freeze_time
 
@@ -10,69 +10,57 @@ from city_scrapers.spiders.chi_ssa_25 import ChiSsa25Spider
 
 test_response = file_response(
     join(dirname(__file__), "files", "chi_ssa_25.html"),
-    url="http://littlevillagechamber.org/2019-meetings-minutes/",
+    url="https://littlevillagechamber.org/calendar/november-ssa-meeting-2019/",
 )
 spider = ChiSsa25Spider()
 
-freezer = freeze_time("2019-03-17")
+freezer = freeze_time("2020-08-07")
 freezer.start()
 
-parsed_items = [item for item in spider.parse(test_response)]
+parsed_item = [item for item in spider._parse_detail(test_response)][0]
 
 freezer.stop()
 
 
 def test_title():
-    assert parsed_items[0]["title"] == "Commission: Monthly"
+    assert parsed_item["title"] == "November SSA Meeting 2019"
 
 
 def test_start():
-    assert parsed_items[0]["start"] == datetime(2019, 1, 15, 9)
-    assert parsed_items[-1]["start"] == datetime(2019, 12, 17, 9)
+    assert parsed_item["start"] == datetime(2019, 11, 19, 9)
 
 
 def test_end():
-    assert parsed_items[0]["end"] == datetime(2019, 1, 15, 10)
-    assert parsed_items[-1]["end"] == datetime(2019, 12, 17, 10)
+    assert parsed_item["end"] == datetime(2019, 11, 19, 10)
 
 
 def test_id():
-    assert parsed_items[0]["id"] == "chi_ssa_25/201901150900/x/commission_monthly"
+    assert parsed_item["id"] == "chi_ssa_25/201911190900/x/november_ssa_meeting_2019"
 
 
 def test_status():
-    assert parsed_items[0]["status"] == PASSED
-    assert parsed_items[-1]["status"] == TENTATIVE
+    assert parsed_item["status"] == PASSED
 
 
 def test_location():
-    assert parsed_items[0]["location"] == {
-        "name": "LV Chamber",
-        "address": "3610 W. 26th St. 2nd Floor Chicago, IL",
-    }
-    assert parsed_items[-1]["location"] == {
-        "name": "Nuevo Leo Restaurant",
-        "address": "3657 W 26th St. 2nd Floor Chicago, IL",
+    assert parsed_item["location"] == {
+        "name": "Second Federal A Division of Self-Help FCU",
+        "address": "3960 w 26th st chicago, IL 60623",
     }
 
 
 def test_source():
-    assert (
-        parsed_items[0]["source"]
-        == "http://littlevillagechamber.org/2019-meetings-minutes/"
-    )
+    assert parsed_item["source"] == test_response.url
 
 
 def test_links():
-    assert parsed_items[0]["links"] == [
+    assert parsed_item["links"] == [
         {
-            "href": "http://littlevillagechamber.org/wp-content/uploads/2019/03/SSA-Jan.-15.-2019-Meeting-Minutes.pdf",  # noqa
+            "href": "https://littlevillagechamber.org/wp-content/uploads/Nov-15-2019-SSA-25-Meeting-Minutes.pdf",  # noqa
             "title": "Minutes",
         }
     ]
-    assert parsed_items[-1]["links"] == []
 
 
-@pytest.mark.parametrize("item", parsed_items)
-def test_classification(item):
-    assert item["classification"] == COMMISSION
+def test_classification():
+    assert parsed_item["classification"] == COMMISSION
