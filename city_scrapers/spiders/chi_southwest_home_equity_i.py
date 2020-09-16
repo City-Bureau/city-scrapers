9df55651af39e9f616c5ed14d40294f472470e57
egfrank <elliot.g.frank@gmail.com>
2020-09-16 14:01:37 -0500

    Add southwest home equity scraper

---
+++ city_scrapers/spiders/chi_southwest_home_equity_i.py
@@ -0,0 +1,136 @@
+import re
+from datetime import datetime, timedelta
+from io import BytesIO
+
+import requests
+from city_scrapers_core.constants import COMMISSION
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+from PyPDF2 import PdfFileReader
+from PyPDF2.utils import PdfReadError
+
+MONTH_REGEX = (
+    r"(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)",
+    r"?|May|June?|July?|Aug(?:ust)?|Sep(?:tember)",
+    r"?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)",
+)
+
+
+class ChiSouthwestHomeEquityISpider(CityScrapersSpider):
+    name = "chi_southwest_home_equity_i"
+    agency = "Chicago Southwest Home Equity Commission I"
+    timezone = "America/Chicago"
+    allowed_domains = ["swhomeequity.com"]
+    start_urls = ["https://swhomeequity.com/agenda-%26-minutes"]
+    location = {
+        "name": "Southwest Home Equity Assurance office",
+        "address": "5334 W. 65th Street in Chicago, Illinois",
+    }
+
+    def parse(self, response):
+        table = response.xpath(
+            "body/div/div/div/div[2]/div[3]/div/div/section/div/div[2]"
+        )
+
+        for agenda_node in table.xpath(
+            "div[@data-ux=\"GridCell\"][contains(., 'Agenda')]"
+        ):
+            minutes_node = agenda_node.xpath(
+                "following-sibling::div[@data-ux=\"GridCell\"][contains(., 'Minutes')]"
+            )
+
+            if self._get_link(minutes_node):
+                minutes_contents = self.parse_pdf(self._get_link(minutes_node))
+            else:
+                minutes_contents = None
+
+            meeting = Meeting(
+                title=(
+                    self._parse_title(minutes_contents)
+                    if minutes_contents
+                    else "Board Meeting"
+                ),
+                description="",
+                classification=COMMISSION,
+                start=self._parse_start(agenda_node),
+                end=None,
+                all_day=False,
+                time_notes=(
+                    self._parse_time_notes(minutes_contents)
+                    if minutes_contents
+                    else None
+                ),
+                location=self.location,
+                links=self._parse_links([agenda_node, minutes_node]),
+                source=self._parse_source(response),
+            )
+            meeting["status"] = self._get_status(meeting)
+            meeting["id"] = self._get_id(meeting)
+
+            yield meeting
+
+    def _parse_title(self, text):
+        name = re.search(r"Southwest Home Equity Assurance Program", text, flags=re.I)
+        minutes = re.search(r"MINUTES", text, flags=re.I)
+        if name and minutes:
+            title = text[name.end() : minutes.start()]
+            return title.replace("\n", "").title().strip()
+        else:
+            return "Board Meeting"
+
+    def parse_pdf(self, url):
+        response = requests.get(url)
+        try:
+            pdf_obj = PdfFileReader(BytesIO(response.content))
+            pdf_text = pdf_obj.getPage(0).extractText().strip()
+        except PdfReadError:
+            pdf_text = ""
+        return pdf_text
+
+    def _parse_start(self, node):
+        date_str = self.find_date(node)
+        date_object = datetime.strptime(date_str, "%B %d %Y")
+        return date_object + timedelta(hours=18, minutes=30)
+
+    def _parse_links(self, nodes):
+        links = []
+        for node in nodes:
+            if node:
+                links.append(
+                    {"title": self._get_name(node), "href": self._get_link(node)}
+                )
+        return links
+
+    def _get_name(self, node):
+        name = node.xpath("string(.)").get()
+        return name
+
+    def _get_link(self, node):
+        url = node.xpath("a/@href").get()
+        if url:
+            return "https:" + url
+        else:
+            return None
+
+    def _parse_source(self, response):
+        return response.url
+
+    def find_date(self, node):
+        text = self._get_name(node)
+        text_block = text.replace("\n", "")
+        return self._use_date_regex(text_block)
+
+    def _use_date_regex(self, text):
+        month_ind = re.search(MONTH_REGEX, text, flags=re.I)
+        year_ind = re.search(
+            r"\d{4}", text[month_ind.start() :]
+        )  # We need the year to come after the
+        date_str = text[
+            month_ind.start() : (month_ind.start() + year_ind.end())
+        ].replace(",", "")
+        return date_str
+
+    def _parse_time_notes(self, text):
+        c_t_o = re.search(r"CALL TO ORDER", text)
+        commissioners = re.search(r"COMMISSIONER(S)? IN ATTENDANCE", text)
+        return text[c_t_o.end() : commissioners.start()].replace("\n", "").strip()
---
+++ tests/test_chi_southwest_home_equity_i.py
@@ -0,0 +1,95 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest
+from city_scrapers_core.constants import COMMISSION, PASSED
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.chi_southwest_home_equity_i import (
+    ChiSouthwestHomeEquityISpider,
+)
+
+test_response = file_response(
+    join(dirname(__file__), "files", "chi_southwest_home_equity_i.html"),
+    url="https://swhomeequity.com/agenda-%26-minutes",
+)
+
+spider = ChiSouthwestHomeEquityISpider()
+
+freezer = freeze_time("2020-01-23")
+freezer.start()
+
+parsed_items = [item for item in spider.parse(test_response)]
+
+
+freezer.stop()
+
+
+def test_title():
+    assert parsed_items[0]["title"] == "Board Meeting"
+
+
+def test_description():
+    assert parsed_items[0]["description"] == ""
+
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2019, 4, 8, 6, 30)
+
+
+def test_end():
+    assert parsed_items[0]["end"] is None
+
+
+def test_time_notes():
+    assert (
+        parsed_items[0]["time_notes"]
+        == "Chairman Nice began the April 8th board meeting at 6:37 pm."
+    )
+
+
+def test_id():
+    assert (
+        parsed_items[0]["id"]
+        == "chi_southwest_home_equity_i/201904081830/x/board_meeting"
+    )
+
+
+def test_status():
+    assert parsed_items[0]["status"] == PASSED
+
+
+def test_location():
+    assert parsed_items[0]["location"] == {
+        "name": "Southwest Home Equity Assurance office",
+        "address": "5334 W. 65th Street in Chicago, Illinois",
+    }
+
+
+def test_source():
+    assert parsed_items[0]["source"] == "https://swhomeequity.com/agenda-%26-minutes"
+
+
+def test_links():
+    assert parsed_items[0]["links"] == [
+        {
+            "href": """https://img1.wsimg.com/blobby/go/ddf32f03-d1ca-4da2-ba70-a82cbae10fd5/
+        downloads/Minutes%20from%20April%208%202019.pdf?ver=1581453329487""",
+            "title": "Minutes from April 8 2019",
+        },
+        {
+            "href": """https://img1.wsimg.com/blobby/go/ddf32f03-d1ca-4da2-ba70-a82cbae10fd5/
+        downloads/Agenda-April%208%2C%202019.pdf?ver=1581453329487""",
+            "title": "Agenda for April 8, 2019 (pdf)Download",
+        },
+    ]
+
+
+def test_classification():
+    assert parsed_items[0]["classification"] == COMMISSION
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_all_day(item):
+    assert item["all_day"] is False
