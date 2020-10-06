5383371f49353cf6045d5087cec68268bdf8b83f
Shayan <shayan@Home-iMac.localdomain>
2020-10-06 12:31:47 -0700

    Add 0854-spide-cook-pharma

---
+++ tests/test_cook_pharmaceutical_disposal.py
@@ -0,0 +1,90 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest
+from city_scrapers_core.constants import ADVISORY_COMMITTEE, PASSED
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.cook_pharmaceutical_disposal import (
+    CookPharmaceuticalDisposalSpider,
+)
+
+test_response = file_response(
+    join(dirname(__file__), "files", "cook_pharmaceutical_disposal.html"),
+    url="https://www.cookcountysheriff.org/rx/advisory-committee/",
+)
+spider = CookPharmaceuticalDisposalSpider()
+
+freezer = freeze_time("2020-10-06")
+freezer.start()
+
+parsed_items = [item for item in spider.parse(test_response)]
+
+freezer.stop()
+
+
+def test_title():
+    assert (
+        parsed_items[0]["title"]
+        == "Cook County Safe Disposal of Pharmaceuticals Advisory Committee"
+    )
+
+
+def test_description():
+    assert parsed_items[0]["description"] == ""
+
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2019, 12, 10, 13, 0)
+
+
+def test_end():
+    assert parsed_items[0]["end"] is None
+
+
+def test_time_notes():
+    assert parsed_items[0]["time_notes"] == "See agenda to confirm times"
+
+
+def test_id():
+    assert (
+        parsed_items[0]["id"]
+        == "cook_pharmaceutical_disposal/201912101300/x/cook_county_safe_disposal_of_pharmaceuticals_advisory_committee"
+    )
+
+
+def test_status():
+    assert parsed_items[0]["status"] == PASSED
+
+
+def test_location():
+    assert parsed_items[0]["location"] == {
+        "address": "Conference Room of 704 Daley Center, Chicago, Illinois 60602",
+        "name": "Conference Room of 704 Daley Center",
+    }
+
+
+def test_source():
+    assert (
+        parsed_items[0]["source"]
+        == "https://www.cookcountysheriff.org/rx/advisory-committee/"
+    )
+
+
+def test_links():
+    assert parsed_items[0]["links"] == [
+        {
+            "href": "https://www.cookcountysheriff.org/wp-content/uploads/2019/11/Dec.-10-2019-Advisory-Committee-Meeting-Agenda.pdf",
+            "title": "Dec. 10 2019 Advisory Committee Meeting Agenda",
+        }
+    ]
+
+
+def test_classification():
+    assert parsed_items[0]["classification"] == ADVISORY_COMMITTEE
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_all_day(item):
+    assert item["all_day"] is False
