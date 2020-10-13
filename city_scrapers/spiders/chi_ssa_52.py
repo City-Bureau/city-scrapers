f357590181a387042bb7cf7be87e2f7a83380b6f
sosolidkk <pedrochaveslimas3@gmail.com>
2020-10-12 22:58:31 -0300

    Fix based on @pjsier suggestions and added more tests

--- city_scrapers/spiders/chi_ssa_52.py
+++ city_scrapers/spiders/chi_ssa_52.py
@@ -1,7 +1,8 @@
+import re
 from datetime import datetime
 from difflib import SequenceMatcher
 
-from city_scrapers_core.constants import NOT_CLASSIFIED
+from city_scrapers_core.constants import COMMISSION
 from city_scrapers_core.items import Meeting
 from city_scrapers_core.spiders import CityScrapersSpider
 
@@ -19,85 +20,57 @@ class ChiSsa52Spider(CityScrapersSpider):
         Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
         needs.
         """
+
         items = response.css("div.paragraph")[3:4]
-        titles = items.css("strong::text").getall()
-        meetings = items.css("ul")
-        items = list(zip(titles, meetings))
+        title = items.css("strong::text").get()
+        meeting = items.css("ul")[0]
 
-        for i, item in enumerate(items):
-            for meet in item[1].css("li"):
-                meet = self._clean_meet(meet)
+        item = (title, meeting)
 
-                meeting = Meeting(
-                    title=self._parse_title(item[0]),
-                    description=self._parse_description(item),
-                    classification=self._parse_classification(item),
-                    start=self._parse_start(meet),
-                    end=self._parse_end(item),
-                    all_day=self._parse_all_day(item),
-                    time_notes=self._parse_time_notes(item),
-                    location=self._parse_location(item),
-                    links=self._parse_links(item),
-                    source=self._parse_source(response),
-                )
+        for meet in meeting.css("li"):
+            meet = self._clean_meet(meet)
 
-                meeting["status"] = self._get_status(meeting)
-                meeting["id"] = self._get_id(meeting)
+            meeting = Meeting(
+                title=self._parse_title(title),
+                description=self._parse_description(item),
+                classification=self._parse_classification(item),
+                start=self._parse_start(meet),
+                end=self._parse_end(item),
+                all_day=self._parse_all_day(item),
+                time_notes=self._parse_time_notes(item),
+                location=self._parse_location(item),
+                links=self._parse_links(item),
+                source=self._parse_source(response),
+            )
 
-                yield meeting
+            meeting["status"] = self._get_status(meeting)
+            meeting["id"] = self._get_id(meeting)
 
-    def _replace_meet_chars(self, meet):
-        meet = meet.css("::text").get()
-        meet = meet.replace(".", "")
-        meet = meet.replace("\xa0", "")
-        meet = meet.replace("…", "")
-        meet = meet.replace(",", "")
-        meet = meet.strip()
-
-        return meet
+            yield meeting
 
     def _clean_meet(self, meet):
-        """Clean and parse a possible meet <li> item"""
-        payload = {}
-        meet = self._replace_meet_chars(meet)
-        meet = meet.split()
+        """Clean a meet datetime info and group the info"""
+        meet = meet.css("::text").get()
+        meet = meet.replace("\xa0", "")
 
-        if len(meet) == 4:
-            day_week = meet[0]
-            month = meet[1]
-            month_day = meet[2]
-            year = meet[3][:4]
-            time = meet[3][4:]
-            payload = {
-                "day_week": day_week,
-                "month": month,
-                "month_day": month_day,
-                "year": year,
-                "time": time,
-            }
+        clean_str = re.sub(r"[^\w:]+", " ", meet)
+        meet_info = clean_str.split()
 
-        if len(meet) == 6:
-            day_week = meet[0]
-            month = meet[1]
-            month_day = meet[2]
-            year = meet[3][:4]
-            time = meet[3][4:]
-            info = " ".join(meet[4:])
-            payload = {
-                "day_week": day_week,
-                "month": month,
-                "month_day": month_day,
-                "year": year,
-                "time": time,
-                "info": info,
-            }
+        return meet_info
 
-        return payload
+    def _check_am_pm(self, time):
+        time = time.split(":")
+        hour = time[0]
+        minutes = time[1]
+
+        if int(hour) >= 8 and int(hour) <= 12:
+            return f"{hour}:{minutes} AM"
+        return f"{hour}:{minutes} PM"
 
     def _parse_title(self, item):
         """Parse or generate meeting title."""
-        item = item.replace("\xa0", "").strip()
-        return item
+        # item = item.replace("\xa0", "").strip()
+        return COMMISSION
 
     def _parse_description(self, item):
         """Parse or generate meeting description."""
@@ -105,7 +78,7 @@ class ChiSsa52Spider(CityScrapersSpider):
 
     def _parse_classification(self, item):
         """Parse or generate classification from allowed options."""
-        return NOT_CLASSIFIED
+        return COMMISSION
 
     def _parse_start(self, item):
         """Parse start datetime as a naive datetime object."""
@@ -124,20 +97,22 @@ class ChiSsa52Spider(CityScrapersSpider):
             "DECEMBER",
         ]
 
+        time = item[4]
+        time = self._check_am_pm(time)
+
         try:
             date = datetime.strptime(
-                f"{item['month_day']} {item['month']}, {item['year']}", "%d %B, %Y"
+                f"{item[2]} {item[1]} {item[3]} {time}",
+                "%d %B %Y %I:%M %p",
             )
         except ValueError:
             for month in months:
-                ratio = SequenceMatcher(None, month, item["month"]).ratio()
+                ratio = SequenceMatcher(None, month, item[1]).ratio()
                 if ratio > 0.5:
                     date = datetime.strptime(
-                        f"{item['month_day']} {month}, {item['year']}", "%d %B, %Y"
+                        f"{item[2]} {month} {item[3]} {time}",
+                        "%d %B %Y %I:%M %p",
                     )
-
-        time = item["time"].split(":")  # [0] = hours, [1] = minutes
-        date = date.replace(hour=int(time[0]), minute=int(time[1]))
         return date
 
     def _parse_end(self, item):
@@ -155,13 +130,13 @@ class ChiSsa52Spider(CityScrapersSpider):
     def _parse_location(self, item):
         """Parse or generate location."""
         return {
-            "address": "",
-            "name": "",
+            "address": "220 E 51st St Chicago, IL 60615",
+            "name": "51st Street Business Association",
         }
 
     def _parse_links(self, item):
         """Parse or generate links."""
-        return [{"href": "", "title": ""}]
+        return []
 
     def _parse_source(self, response):
         """Parse or generate source."""
