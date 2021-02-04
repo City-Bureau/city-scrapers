fe84c2f14166d8498781fb553feee543c2c35aa0
pjsier <pjsier@gmail.com>
2021-02-04 16:23:42 +0000

    update black formatting

--- city_scrapers/spiders/cook_forest_preserves.py
+++ city_scrapers/spiders/cook_forest_preserves.py
@@ -54,8 +54,6 @@ class CookForestPreservesSpider(LegistarSpider):
         address = item.get("Meeting Location", None)
         if address:
             address = re.sub(
-                r"\s+",
-                " ",
-                re.sub(r"(\n)|(--em--)|(--em)|(em--)", " ", address),
+                r"\s+", " ", re.sub(r"(\n)|(--em--)|(--em)|(em--)", " ", address),
             ).strip()
         return {"address": address, "name": ""}
