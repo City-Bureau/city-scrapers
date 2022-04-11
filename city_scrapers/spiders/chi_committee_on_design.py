600e07c579e10377b59e97bb54f5aaafb786732f
joshag <joshag@umich.edu>
2022-04-11 17:15:10 -0400

    done scraper and testing, almost ready for pull request

--- city_scrapers/spiders/chi_committee_on_design.py
+++ city_scrapers/spiders/chi_committee_on_design.py
@@ -1,10 +1,5 @@
-from calendar import month
 from pickle import FALSE, TRUE
-from posixpath import split
-from re import T
 from datetime import datetime
-from datetime import date
-from city_scrapers_core.constants import NOT_CLASSIFIED
 from city_scrapers_core.items import Meeting
 from city_scrapers_core.spiders import CityScrapersSpider
 
@@ -13,41 +8,61 @@ class ChiCommitteeOnDesignSpider(CityScrapersSpider):
     name = "chi_committee_on_design"
     agency = "Comittee On Design"
     timezone = "America/Chicago"
-    start_urls = ["https://www.chicago.gov/city/en/depts/dcd/supp_info/committee-on-design.html"]
+    start_urls = [
+        "https://www.chicago.gov/city/en/depts/dcd/supp_info/committee-on-design.html"
+    ]
 
     def parse(self, response):
 
-        #Parse strong text on page to get a list of all eyars, each has a meetings table under it
+        # Parse strong text on page to get a list of all years
+        # each has a table of meetings under it
         years = response.css("strong::text").getall()
         years = years[3:]
         years = [i[0:4] for i in years]
 
- 
         tablecount = 0
-        for table in response.css("table"): #loop through every meeting table
-            total = [] #holds all meetings for a given year (loop)
+        for table in response.css("table"):  # loop through every meeting table
+            # holds all meetings for a given year (loop)
+            total = []
+            # list of raw HTML for a given meeting
+            # to later get meeting agenda if link exists
+            total_clean_dates = []
             for item in table.css("td"):
                 dates = item.css("p::text").getall()
-                for i in range(len(dates)): #loop through text from the table, removing non-date strings
-                    curr = dates[i] 
+                # taking raw HTML and splitting by date
+                # to later pull meeting agenda link
+                clean_dates = item.css("p").get().split("<br>")
+                total_clean_dates += clean_dates
+                # loop through text from the table, removing non-date strings
+                for i in range(len(dates)):
+                    curr = dates[i]
                     d = curr.strip()
                     d = d.rstrip("\xa0")
                     dates[i] = d
                 for d in dates:
                     if d[0] == "•":
                         dates.remove(d)
-                total += dates #add dates to list of meetings
-            curyear = years[tablecount]  #get current year based on which table we are on
-            for meet in total:
+                total += dates  # add dates to list of meetings
+            print("SIZE OF TOTAL " + str(total))
+            print("SIZE OF CLEAN DATES " + str(clean_dates))
+            # get current year based on which table we are on
+            curyear = years[tablecount]
+            for i in range(len(total)):
+                meet = total[i]  # current meeting we are working with
                 canceled = FALSE
                 splitMeet = meet.split()
-                if len(splitMeet) > 2: #meeting string is in format "month day" or "month day canceled"
+                # meeting string is in format "month day" or "month day canceled"
+                if len(splitMeet) > 2:
                     if splitMeet[2].strip() == "Canceled":
                         canceled = TRUE
                         meet = splitMeet[0] + " " + splitMeet[1]
-                    else: #if not "canceled", it is unexpected so we throw an error
+                    else:  # if not "canceled", it is unexpected so we throw an error
                         raise ValueError("Unknown addition to date time")
-
+                clean_date = total_clean_dates[i]
+                clean_date_list = clean_date.split('"')
+                agendaLink = ""
+                if len(clean_date_list) > 1:
+                    agendaLink = clean_date_list[1]
 
                 meeting = Meeting(
                     title=self._parse_title(),
@@ -58,16 +73,19 @@ class ChiCommitteeOnDesignSpider(CityScrapersSpider):
                     all_day=self._parse_all_day(),
                     time_notes=self._parse_time_notes(),
                     location=self._parse_location(),
-                    links=self._parse_links(response),
+                    links=self._parse_links(response, agendaLink),
                     source=self._parse_source(response),
                 )
-
-                if meeting["start"] < datetime.now(): #check that meeting is in the future, otherewise do not yield the meedint
+                # check that meeting is in the future, else do not yield the meeting
+                if meeting["start"] < datetime.now():
                     continue
 
                 meeting["status"] = self._get_status(meeting)
-                if canceled == TRUE: #update status if previouslt determined that meeting was canceled
-                    meeting["status"] = self._get_status(item, text="Meeting is cancelled")
+                # update status if previously determined that meeting was canceled
+                if canceled == TRUE:
+                    meeting["status"] = self._get_status(
+                        item, text="Meeting is cancelled"
+                    )
                 meeting["id"] = self._get_id(meeting)
 
                 yield meeting
@@ -78,7 +96,13 @@ class ChiCommitteeOnDesignSpider(CityScrapersSpider):
         return "Committee on Design Meeting"
 
     def _parse_description(self):
-        return "The Committee on Design is a volunteer group of 24 urban design professionals organized by the Department of Planning and Development (DPD) to advise Commissioner Maurice Cox and DPD staff on design excellence for key development projects and urban design initiatives."
+        return (
+            "The Committee on Design is a volunteer group of 24 urban "
+            "design professionals organized by the Department of Planning and"
+            " Development (DPD) to advise Commissioner Maurice Cox and DPD"
+            " staff on design excellence for key development projects "
+            "and urban design initiatives."
+        )
 
     def _parse_classification(self):
         return "COMMITTEE"
@@ -88,7 +112,6 @@ class ChiCommitteeOnDesignSpider(CityScrapersSpider):
         return datetime.strptime(
             "{} {} 1PM".format(monthDay.strip(), curyear), "%B %d %Y %I%p"
         )
-
 
     def _parse_end(self):
         return None
@@ -102,16 +125,29 @@ class ChiCommitteeOnDesignSpider(CityScrapersSpider):
     def _parse_location(self):
         """Parse or generate location."""
         return {
-            "address": "",
-            "name": "",
+            "address": "Virtual",
+            "name": "Virtual",
         }
 
-    def _parse_links(self, response):
+    def _parse_links(self, response, agendaLink):
         """Parse or generate links."""
         text = response.css("main").css("p").css("a::text").get()
         if text != "live streamed":
             raise ValueError("Live Stream link has changed")
-        return [{"href": response.css("main").css("p").css("a::attr(href)").get(), "title": "Meeting Link"}]
+        if agendaLink != "":
+            return [
+                {
+                    "href": response.css("main").css("p").css("a::attr(href)").get(),
+                    "title": "Meeting Link",
+                },
+                {"href": agendaLink, "title": "Agenda"},
+            ]
+        return [
+            {
+                "href": response.css("main").css("p").css("a::attr(href)").get(),
+                "title": "Meeting Link",
+            }
+        ]
 
     def _parse_source(self, response):
         """Parse or generate source."""
--- tests/test_chi_committee_on_design.py
+++ tests/test_chi_committee_on_design.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from os.path import dirname, join
 
 import pytest
-from city_scrapers_core.constants import NOT_CLASSIFIED
 from city_scrapers_core.utils import file_response
 from freezegun import freeze_time
 
@@ -14,7 +13,7 @@ test_response = file_response(
 )
 spider = ChiCommitteeOnDesignSpider()
 
-freezer = freeze_time("2022-03-30")
+freezer = freeze_time("2022-04-11")
 freezer.start()
 
 parsed_items = [item for item in spider.parse(test_response)]
@@ -22,33 +21,43 @@ parsed_items = [item for item in spider.parse(test_response)]
 freezer.stop()
 
 
-def test_tests():
-    print("Please write some tests for this spider or at least disable this one.")
-    assert False
+# def test_tests():
+#     print("Please write some tests for this spider or at least disable this one.")
+#     assert False
 
 
 """
 Uncomment below
 """
 
-# def test_title():
-#     assert parsed_items[0]["title"] == "EXPECTED TITLE"
+
+def test_title():
+    assert parsed_items[0]["title"] == "Committee on Design Meeting"
 
 
-# def test_description():
-#     assert parsed_items[0]["description"] == "EXPECTED DESCRIPTION"
+def test_description():
+    assert parsed_items[0]["description"] == (
+        "The Committee on Design is a volunteer group of 24 urban "
+        "design professionals organized by the Department of Planning "
+        "and Development (DPD) to advise Commissioner Maurice Cox and "
+        "DPD staff on design excellence for key development projects "
+        "and urban design initiatives."
+    )
 
 
-# def test_start():
-#     assert parsed_items[0]["start"] == datetime(2019, 1, 1, 0, 0)
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2022, 4, 13, 13, 0)
 
 
-# def test_end():
-#     assert parsed_items[0]["end"] == datetime(2019, 1, 1, 0, 0)
+def test_end():
+    assert parsed_items[0]["end"] is None
 
 
-# def test_time_notes():
-#     assert parsed_items[0]["time_notes"] == "EXPECTED TIME NOTES"
+def test_time_notes():
+    assert (
+        parsed_items[0]["time_notes"]
+        == "Historically, meetings have started at 1:00pm or 1:30pm"
+    )
 
 
 # def test_id():
@@ -59,28 +68,27 @@ Uncomment below
 #     assert parsed_items[0]["status"] == "EXPECTED STATUS"
 
 
-# def test_location():
-#     assert parsed_items[0]["location"] == {
-#         "name": "EXPECTED NAME",
-#         "address": "EXPECTED ADDRESS"
-#     }
+def test_location():
+    assert parsed_items[0]["location"] == {"name": "Virtual", "address": "Virtual"}
 
 
 # def test_source():
 #     assert parsed_items[0]["source"] == "EXPECTED URL"
 
 
-# def test_links():
-#     assert parsed_items[0]["links"] == [{
-#       "href": "EXPECTED HREF",
-#       "title": "EXPECTED TITLE"
-#     }]
+def test_links():
+    assert parsed_items[0]["links"][0] == {
+        "href": "https://livestream.com/accounts/28669066/events/9117952",
+        "title": "Meeting Link",
+    }
+    assert len(parsed_items[0]["links"]) == 2
+    assert len(parsed_items[1]["links"]) == 1
 
 
-# def test_classification():
-#     assert parsed_items[0]["classification"] == NOT_CLASSIFIED
+def test_classification():
+    assert parsed_items[0]["classification"] == "COMMITTEE"
 
 
-# @pytest.mark.parametrize("item", parsed_items)
-# def test_all_day(item):
-#     assert item["all_day"] is False
+@pytest.mark.parametrize("item", parsed_items)
+def test_all_day(item):
+    assert item["all_day"] is False
