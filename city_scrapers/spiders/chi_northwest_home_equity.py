771a78c8c8dbea6e733b82630360d4d56bf26aa3
KevivJaknap <vivekpankaj1@gmail.com>
2023-09-06 10:45:22 +0530

    Wrote basic scripts

--- city_scrapers/spiders/chi_northwest_home_equity.py
+++ city_scrapers/spiders/chi_northwest_home_equity.py
@@ -1,43 +1,68 @@
-from city_scrapers_core.constants import NOT_CLASSIFIED
+from city_scrapers_core.constants import COMMISSION
 from city_scrapers_core.items import Meeting
 from city_scrapers_core.spiders import CityScrapersSpider
-
+import scrapy
+import re
+import datetime as dt
 
 class ChiNorthwestHomeEquitySpider(CityScrapersSpider):
     name = "chi_northwest_home_equity"
     agency = "Chicago Northwest Home Equity"
     timezone = "America/Chicago"
     start_urls = ["https://nwheap.com/category/meet-minutes-and-agendas/"]
-
-    def parse(self, response):
+     
+        
+    def parse(self, response, url_to_local=None):
         """
         `parse` should always `yield` Meeting items.
 
         Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
         needs.
         """
-        for item in response.css(".meetings"):
-            meeting = Meeting(
-                title=self._parse_title(item),
-                description=self._parse_description(item),
-                classification=self._parse_classification(item),
-                start=self._parse_start(item),
-                end=self._parse_end(item),
-                all_day=self._parse_all_day(item),
-                time_notes=self._parse_time_notes(item),
-                location=self._parse_location(item),
-                links=self._parse_links(item),
-                source=self._parse_source(response),
-            )
+        for link in response.css('div.em-events-widget a::attr(href)').getall():
+            if link != 'https://nwheap.com/events/':
+                if url_to_local:
+                    link = "file:\\"+url_to_local[link]
+                yield scrapy.Request(link, callback=self.parse_page)
 
-            meeting["status"] = self._get_status(meeting)
-            meeting["id"] = self._get_id(meeting)
+    def parse_page(self, response):
+        meeting = Meeting(
+            title=self._parse_title(response),
+            description=self._parse_description(response),
+            classification=self._parse_classification(response),
+            start=self._parse_start(response),
+            end=self._parse_end(response),
+            all_day=self._parse_all_day(response),
+            time_notes=self._parse_time_notes(response),
+            location=self._parse_location(response),
+            links=self._parse_links(response),
+            source=self._parse_source(response),
+        )
+        meeting["id"] = self._get_id(meeting)
+        meeting["status"] = self._get_status(meeting)
+        yield meeting
+       
+        # for item in response.css(".meetings"):
+        #     meeting = Meeting(
+        #         title=self._parse_title(item),
+        #         description=self._parse_description(item),
+        #         classification=self._parse_classification(item),
+        #         start=self._parse_start(item),
+        #         end=self._parse_end(item),
+        #         all_day=self._parse_all_day(item),
+        #         time_notes=self._parse_time_notes(item),
+        #         location=self._parse_location(item),
+        #         links=self._parse_links(item),
+        #         source=self._parse_source(response),
+        #     )
 
-            yield meeting
+            # meeting["status"] = self._get_status(meeting)
+            # meeting["id"] = self._get_id(meeting)
+
+            # yield meeting
 
     def _parse_title(self, item):
-        """Parse or generate meeting title."""
-        return ""
+        return item.css("h1.entry-title::text").get()
 
     def _parse_description(self, item):
         """Parse or generate meeting description."""
@@ -45,15 +70,15 @@ class ChiNorthwestHomeEquitySpider(CityScrapersSpider):
 
     def _parse_classification(self, item):
         """Parse or generate classification from allowed options."""
-        return NOT_CLASSIFIED
+        return COMMISSION
 
     def _parse_start(self, item):
         """Parse start datetime as a naive datetime object."""
-        return None
+        return self._parse_date(item, start_time=True)
 
     def _parse_end(self, item):
         """Parse end datetime as a naive datetime object. Added by pipeline if None"""
-        return None
+        return self._parse_date(item, start_time=False)
 
     def _parse_time_notes(self, item):
         """Parse any additional notes on the timing of the meeting"""
@@ -65,8 +90,10 @@ class ChiNorthwestHomeEquitySpider(CityScrapersSpider):
 
     def _parse_location(self, item):
         """Parse or generate location."""
+        location_ = item.css('p > strong:contains("Location") ~ a::text').get()
+
         return {
-            "address": "",
+            "address": location_,
             "name": "",
         }
 
@@ -77,3 +104,20 @@ class ChiNorthwestHomeEquitySpider(CityScrapersSpider):
     def _parse_source(self, response):
         """Parse or generate source."""
         return response.url
+
+    def _parse_date(self, item, start_time=True):
+        date = item.css('p:contains("Date/Time")').get()
+        date_match = re.search(r'(\d{1,2}/\d{1,2}/\d{4})', date)
+        date = date_match.group(0)
+
+        time = item.css('p > strong:contains("Date/Time") ~ *::text').get()
+        start, end = "", ""
+        if not time:
+            start = "Not found"
+            end = "Not found"
+        else:
+            start, end = time.split(" - ")
+        if start_time:
+            return dt.datetime.strptime(f"{date} {start}", "%m/%d/%Y %I:%M %p")
+        return dt.datetime.strptime(f"{date} {end}", "%m/%d/%Y %I:%M %p")
+
--- tests/test_chi_northwest_home_equity.py
+++ tests/test_chi_northwest_home_equity.py
@@ -12,39 +12,53 @@ test_response = file_response(
     join(dirname(__file__), "files", "chi_northwest_home_equity.html"),
     url="https://nwheap.com/category/meet-minutes-and-agendas/",
 )
+
+url_to_local = {"https://nwheap.com/category/meet-minutes-and-agendas/": "chi_northwest_home_equity.html",
+                "https://nwheap.com/events/governing-commissioners-public-meeting-18/": "1.html",
+                "https://nwheap.com/events/governing-commissioners-public-meeting-17/": "2.html",
+                "https://nwheap.com/events/governing-commissioners-public-meeting-16/": "3.html",
+                "https://nwheap.com/events/governing-commissioners-public-meeting-15/": "4.html",
+                "https://nwheap.com/events/governing-commissioners-public-meeting-14/": "5.html",
+                "https://nwheap.com/events/governing-commissioners-public-meeting-13/": "6.html",
+                "https://nwheap.com/events/governing-commissioners-public-meeting-12/": "7.html",}
+
+for link, local in url_to_local.items():
+    url_to_local[link] = join(dirname(__file__), "files", local)
+
 spider = ChiNorthwestHomeEquitySpider()
 
 freezer = freeze_time("2023-09-04")
 freezer.start()
 
-parsed_items = [item for item in spider.parse(test_response)]
+
 
 freezer.stop()
 
 
-def test_tests():
-    print("Please write some tests for this spider or at least disable this one.")
-    assert False
+# def test_tests():
+#     print("Please write some tests for this spider or at least disable this one.")
+#     assert False
 
 
 """
 Uncomment below
 """
 
-# def test_title():
-#     assert parsed_items[0]["title"] == "EXPECTED TITLE"
+def test_title():
+    assert parsed_items[0]["title"] == "Governing Commissioners Public Meeting"
 
 
-# def test_description():
-#     assert parsed_items[0]["description"] == "EXPECTED DESCRIPTION"
+def test_description():
+    assert parsed_items[0]["description"] == ""
+
+#{"title": "Governing Commissioners Public Meeting", "description": "", "classification": "Commission", "start": "2022-12-15 18:30:00", "end": "2022-12-15 19:30:00", "all_day": false, "time_notes": "", "location": {"address": "Northwest Home Equity Assurance Program", "name": ""}, "links": [{"href": "", "title": ""}], "source": "https://nwheap.com/events/governing-commissioners-public-meeting-12/", "id": "chi_northwest_home_equity/202212151830/x/governing_commissioners_public_meeting", "status": "passed"},
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2022, 12, 15, 18, 30)
 
 
-# def test_start():
-#     assert parsed_items[0]["start"] == datetime(2019, 1, 1, 0, 0)
-
-
-# def test_end():
-#     assert parsed_items[0]["end"] == datetime(2019, 1, 1, 0, 0)
+def test_end():
+    assert parsed_items[0]["end"] == datetime(2022, 12, 15, 19, 30)
 
 
 # def test_time_notes():
