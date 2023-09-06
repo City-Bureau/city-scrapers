3a6e237e7ce599f7b89019c0d57ad760c6919121
KevivJaknap <vivekpankaj1@gmail.com>
2023-09-06 17:35:35 +0530

    Some new tests added

--- tests/test_chi_northwest_home_equity.py
+++ tests/test_chi_northwest_home_equity.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from os.path import dirname, join
 
 import pytest
-from city_scrapers_core.constants import NOT_CLASSIFIED
+from city_scrapers_core.constants import COMMISSION
 from city_scrapers_core.utils import file_response
 from freezegun import freeze_time
 import json
@@ -36,67 +36,44 @@ def parsed_items(monkeypatch):
     freezer.stop()
     return parsed_items
 
-
-# def test_tests():
-#     print("Please write some tests for this spider or at least disable this one.")
-#     assert False
-
-
-"""
-Uncomment below
-"""
+def test_length(parsed_items):
+    assert len(parsed_items) == 7
 
 def test_title(parsed_items):
     assert parsed_items[0]["title"] == "Governing Commissioners Public Meeting"
 
-
 def test_description(parsed_items):
     assert parsed_items[0]["description"] == ""
-
-#{"title": "Governing Commissioners Public Meeting", "description": "", "classification": "Commission", "start": "2022-12-15 18:30:00", "end": "2022-12-15 19:30:00", "all_day": false, "time_notes": "", "location": {"address": "Northwest Home Equity Assurance Program", "name": ""}, "links": [{"href": "", "title": ""}], "source": "https://nwheap.com/events/governing-commissioners-public-meeting-12/", "id": "chi_northwest_home_equity/202212151830/x/governing_commissioners_public_meeting", "status": "passed"},
 
 def test_start(parsed_items):
     assert parsed_items[0]["start"] == datetime(2023, 10, 19, 18, 30)
 
-
 def test_end(parsed_items):
     assert parsed_items[0]["end"] == datetime(2023, 10, 19, 19, 30)
 
+def test_location(parsed_items):
+    assert parsed_items[0]["location"] == {
+        "name": "",
+        "address": "3234 N. Central Ave."
+    }
 
-# def test_time_notes():
-#     assert parsed_items[0]["time_notes"] == "EXPECTED TIME NOTES"
+def test_source(parsed_items):
+    assert parsed_items[0]["source"] == "https://nwheap.com/events/governing-commissioners-public-meeting-18/"
 
+def test_time_notes(parsed_items):
+    assert parsed_items[0]["time_notes"] == ""
 
-# def test_id():
-#     assert parsed_items[0]["id"] == "EXPECTED ID"
+def test_id(parsed_items):
+    assert parsed_items[0]["id"] == "chi_northwest_home_equity/202310191830/x/governing_commissioners_public_meeting"
 
+def test_status(parsed_items):
+    assert parsed_items[0]["status"] == "tentative"
 
-# def test_status():
-#     assert parsed_items[0]["status"] == "EXPECTED STATUS"
+def test_links(parsed_items):
+    assert parsed_items[0]["links"] == [{
+      "href": "",
+      "title": ""
+    }]
 
-
-# def test_location():
-#     assert parsed_items[0]["location"] == {
-#         "name": "EXPECTED NAME",
-#         "address": "EXPECTED ADDRESS"
-#     }
-
-
-# def test_source():
-#     assert parsed_items[0]["source"] == "EXPECTED URL"
-
-
-# def test_links():
-#     assert parsed_items[0]["links"] == [{
-#       "href": "EXPECTED HREF",
-#       "title": "EXPECTED TITLE"
-#     }]
-
-
-# def test_classification():
-#     assert parsed_items[0]["classification"] == NOT_CLASSIFIED
-
-
-# @pytest.mark.parametrize("item", parsed_items)
-# def test_all_day(item):
-#     assert item["all_day"] is False
+def test_classification(parsed_items):
+    assert parsed_items[0]["classification"] == COMMISSION
