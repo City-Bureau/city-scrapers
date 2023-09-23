25c01d1465ccef4a1bd6233937ded3fb7df019d0
Hails <whatuphails@yahoo.com>
2023-09-23 14:15:11 -0400

    redo pytests

--- tests/test_chi_citycouncil.py
+++ tests/test_chi_citycouncil.py
@@ -10,7 +10,7 @@ from city_scrapers.spiders.chi_citycouncil import ChiCitycouncilSpider
 
 test_response = file_response(
     join(dirname(__file__), "files", "chi_citycouncil.html"),
-    url="https://api.chicityclerkelms.chicago.gov/meeting?filter=body%20eq%20%27City%20Council%27&sort=date%20desc",  # noqa
+    url="https://chicityclerkelms.chicago.gov/Meetings/",
 )
 spider = ChiCitycouncilSpider()
 
@@ -21,19 +21,20 @@ parsed_items = [item for item in spider.parse(test_response)]
 
 freezer.stop()
 
+
 """
 def test_tests():
     print("Please write some tests for this spider or at least disable this one.")
     assert False
-
-
-
 Uncomment below
 """
 
 
 def test_title():
-    assert parsed_items[0]["title"] == "Chicago City Council"
+    assert (
+        parsed_items[0]["title"]
+        == "Committee on Zoning, Landmarks and Building Standards"
+    )
 
 
 def test_description():
@@ -41,20 +42,24 @@ def test_description():
 
 
 def test_start():
-    assert parsed_items[0]["start"] == datetime(2023, 10, 4, 15, 0)
+    assert parsed_items[0]["start"] == datetime(2023, 10, 12, 15, 0)
 
 
 # def test_end():
-#   assert parsed_items[0]["end"] == datetime(2023, 10, 4, 17, 0)
+#     assert parsed_items[0]["end"] == datetime(2019, 1, 1, 0, 0)
 
 
-# def test_time_notes():
-#     assert parsed_items[0]["time_notes"] == "EXPECTED TIME NOTES"
+def test_time_notes():
+    assert (
+        parsed_items[0]["time_notes"]
+        == "Please double check the meeting time on the meeting page."
+    )
 
 
 def test_id():
     assert (
-        parsed_items[0]["id"] == "chi_citycouncil/202310041500/x/chicago_city_council"
+        parsed_items[0]["id"]
+        == "chi_citycouncil/202310121500/x/committee_on_zoning_landmarks_and_building_standards"  # noqa
     )
 
 
@@ -71,18 +76,16 @@ def test_location():
 
 def test_source():
     assert (
-        parsed_items[0]["source"]
-        == "https://api.chicityclerkelms.chicago.gov/meeting?filter=body%20eq%20%27City%20Council%27&sort=date%20desc"  # noqa
+        parsed_items[0]["source"] == "https://api.chicityclerkelms.chicago.gov/meeting"
     )
 
 
 def test_links():
     assert parsed_items[0]["links"] == [
         {
-            "href": "https://occprodstoragev1.blob.core.usgovcloudapi.net/meetingattachmentspublic/05a907c9-676b-4617-9d0b-266830f5ebcf.pdf",  # noqa
-            "title": "Agenda",
-        },
-        {"href": "", "title": "Video Link"},
+            "href": "https://chicityclerkelms.chicago.gov/Meeting/?meetingId=05889499-4C56-EE11-BE6E-001DD8098532",  # noqa
+            "title": "Meeting Page",
+        }
     ]
 
 
