c62f23bb506a107e88a45a13c74a6b78ebfb61e1
Daniel Simmons-Ritchie <37225902+SimmonsRitchie@users.noreply.github.com>
2023-12-17 13:11:33 -0800

    Update with new fixtures and assertions

--- tests/test_il_commerce.py
+++ tests/test_il_commerce.py
@@ -10,29 +10,31 @@ from city_scrapers.spiders.il_commerce import IlCommerceSpider
 
 test_response = file_response(
     join(dirname(__file__), "files", "il_commerce.html"),
-    url="https://www.icc.illinois.gov/meetings/default.aspx?dts=32&et=1&et=5&et=3",
+    url="https://www.icc.illinois.gov/meetings?dts=32&scm=True&sps=True&sh=True&sjc=True&ssh=False&smceb=True",
 )
 test_detail_response = file_response(
     join(dirname(__file__), "files", "il_commerce_detail.html"),
-    url="https://www.icc.illinois.gov/meetings/policy-session/meeting/21353",
+    url="https://www.icc.illinois.gov/meetings/meeting/hearing/138529",
 )
 spider = IlCommerceSpider()
 
-freezer = freeze_time("2020-03-05")
+freezer = freeze_time("2023-12-01")
 freezer.start()
 
 parsed_requests = [item for item in spider.parse(test_response)]
-parsed_item = [item for item in spider._parse_detail(test_detail_response)][0]
+parsed_item = [item for item in spider._parse_event_page(test_detail_response)][0]
 
 freezer.stop()
 
 
 def test_count():
-    assert len(parsed_requests) == 6
+    parsed_requests = [item for item in spider.parse(test_response)]
+    print(parsed_requests)
+    assert len(parsed_requests) == 74
 
 
 def test_title():
-    assert parsed_item["title"] == "Policy Session Summer Preparedness Policy Session"
+    assert parsed_item["title"] == "Regular Open Meeting"
 
 
 def test_description():
@@ -40,7 +42,7 @@ def test_description():
 
 
 def test_start():
-    assert parsed_item["start"] == datetime(2020, 5, 27, 11, 30)
+    assert parsed_item["start"] == datetime(2023, 12, 14, 11, 30)
 
 
 def test_end():
@@ -54,7 +56,7 @@ def test_time_notes():
 def test_id():
     assert (
         parsed_item["id"]
-        == "il_commerce/202005271130/x/policy_session_summer_preparedness_policy_session"  # noqa
+        == "il_commerce/202312141130/x/regular_open_meeting"  # noqa
     )
 
 
@@ -76,45 +78,20 @@ def test_source():
 def test_links():
     assert parsed_item["links"] == [
         {
-            "href": "https://www.icc.illinois.gov/downloads/public/letter/21690.pdf",
+            "href": "https://www.icc.illinois.gov/efiling/agenda/public/coverletter?agid=21912",
             "title": "Public Utility Cover Letter",
         },
         {
-            "href": "https://www.icc.illinois.gov/downloads/public/agenda/21690.pdf",
-            "title": "Policy Agenda",
+            "href": "https://www.icc.illinois.gov/efiling/agenda/public/agendareport?agid=21912",
+            "title": "Regular Open Meeting Agenda",
         },
         {
-            "href": "https://www.icc.illinois.gov/downloads/public/Ameren-Summer2020.pdf",  # noqa
-            "title": "Ameren",
+            "href": "https://www.icc.illinois.gov/efiling/agenda/public/coverletter?agid=21938",
+            "title": "Transportation Cover Letter",
         },
         {
-            "href": "https://www.icc.illinois.gov/downloads/public/ComEd_summer2020.pdf",  # noqa
-            "title": "ComEd",
-        },
-        {
-            "href": "https://www.icc.illinois.gov/downloads/public/CUB_summer2020.pdf",
-            "title": "CUB",
-        },
-        {
-            "href": "https://www.icc.illinois.gov/downloads/public/AG.pdf",
-            "title": "Illinois Attorney General",
-        },
-        {
-            "href": "https://www.icc.illinois.gov/downloads/public/Mid "
-            "American_summer2020.pdf",
-            "title": "Mid-American",
-        },
-        {
-            "href": "https://www.icc.illinois.gov/downloads/public/MISO_summer2020.pdf",
-            "title": "MISO",
-        },
-        {
-            "href": "https://www.icc.illinois.gov/downloads/public/NCLC_summer2020.pdf",
-            "title": "NCLC",
-        },
-        {
-            "href": "https://www.icc.illinois.gov/downloads/public/PJM_summer2020.pdf",
-            "title": "PJM",
+            "href": "https://www.icc.illinois.gov/efiling/agenda/public/agendareport?agid=21938",
+            "title": "Bench Agenda",
         },
     ]
 
