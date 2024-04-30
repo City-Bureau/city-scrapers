f9eac52ef4538df64b61d92aaf7281cb019511b1
Daniel Simmons-Ritchie <37225902+SimmonsRitchie@users.noreply.github.com>
2024-04-30 15:51:08 -0500

    Deprecate spider chi_ssa_24

--- city_scrapers/spiders/chi_ssa_24.py
+++
@@ -1,9 +0,0 @@
-from city_scrapers_core.spiders import CityScrapersSpider
-
-from city_scrapers.mixins import ChiRogersParkSsaMixin
-
-
-class ChiSsa24Spider(ChiRogersParkSsaMixin, CityScrapersSpider):
-    name = "chi_ssa_24"
-    agency = "Chicago Special Service Area #24 Clark/Morse/Glenwood"
-    start_urls = ["https://rpba.org/ssa-24/"]
