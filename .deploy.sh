eab9aa20f07b78b25a768c7a401c550491274c69
Duong Cao <duong.cao@eastagile.com>
2026-03-09 16:13:50 +0700

    Enable deploy logging to debug chi_transit missing meetings
    Remove -s LOG_ENABLED=False from .deploy.sh to surface Scrapy
    logs during cron runs. This will help diagnose why chi_transit
    spider output is not reaching Azure.

--- .deploy.sh
+++ .deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pipenv run scrapy list | xargs -I {} pipenv run scrapy crawl {} -s LOG_ENABLED=False &
+pipenv run scrapy list | xargs -I {} pipenv run scrapy crawl {} &
 
 # Output to the screen every 9 minutes to prevent a timeout
 # https://stackoverflow.com/a/40800348
