4240f48a513eec13a8d024606a9b2a871d930dde
msrezaie <79583632+msrezaie@users.noreply.github.com>
2026-03-17 15:22:58 -0400

    reworked the deduplication logic

--- tests/test_il_regional_transit.py
+++ tests/test_il_regional_transit.py
@@ -2,17 +2,11 @@ from datetime import datetime
 from os.path import dirname, join
 
 import pytest
-from city_scrapers_core.constants import BOARD, PASSED, TENTATIVE
+from city_scrapers_core.constants import BOARD, PASSED
 from city_scrapers_core.utils import file_response
 from freezegun import freeze_time
 
 from city_scrapers.spiders.il_regional_transit import IlRegionalTransitSpider
-
-upcoming_response = file_response(
-    join(dirname(__file__), "files", "il_regional_transit_upcoming.html"),
-    url="https://www.rtachicago.org/about-rta/boards-and-committees/meeting-materials",
-)
-
 
 past_response = file_response(
     join(dirname(__file__), "files", "il_regional_transit_past.html"),
@@ -28,31 +22,23 @@ def spider():
 @pytest.fixture
 def parsed_items(spider):
     with freeze_time("2026-03-13"):
-        upcoming_section = upcoming_response.css(
-            ".bg-rtadarkgray-500.w-full.grid.grid-cols-1.p-8.mb-12.border-t-4.border-rtayellow-500"  # noqa
-        )
-        past_response.meta["upcoming_section"] = upcoming_section
         return [item for item in spider.parse(past_response)]
 
 
 def test_title(parsed_items):
-    assert parsed_items[0]["title"] == "Board Meeting"
-    assert parsed_items[3]["title"] == "Pension Board of Trustees"
+    assert parsed_items[0]["title"] == "Pension Board of Trustees"
 
 
 def test_description(parsed_items):
     assert parsed_items[0]["description"] == ""
-    assert parsed_items[3]["description"] == ""
 
 
 def test_start(parsed_items):
-    assert parsed_items[0]["start"] == datetime(2026, 3, 19, 9)
-    assert parsed_items[3]["start"] == datetime(2026, 2, 26, 0)
+    assert parsed_items[0]["start"] == datetime(2026, 2, 26, 0)
 
 
 def test_end_time(parsed_items):
     assert parsed_items[0]["end"] is None
-    assert parsed_items[3]["end"] is None
 
 
 def test_time_notes(parsed_items):
@@ -60,28 +46,21 @@ def test_time_notes(parsed_items):
         parsed_items[0]["time_notes"]
         == "Check the source page to confirm details on meeting time and location."
     )
-    assert (
-        parsed_items[3]["time_notes"]
-        == "Check the source page to confirm details on meeting time and location."
-    )
 
 
 def test_id(parsed_items):
-    assert parsed_items[0]["id"] == "il_regional_transit/202603190900/x/board_meeting"
     assert (
-        parsed_items[3]["id"]
+        parsed_items[0]["id"]
         == "il_regional_transit/202602260000/x/pension_board_of_trustees"
     )
 
 
 def test_status(parsed_items):
-    assert parsed_items[0]["status"] == TENTATIVE
-    assert parsed_items[3]["status"] == PASSED
+    assert parsed_items[0]["status"] == PASSED
 
 
 def test_links(parsed_items):
-    assert parsed_items[0]["links"] == []
-    assert parsed_items[3]["links"] == [
+    assert parsed_items[0]["links"] == [
         {
             "href": "https://www.rtachicago.org/uploads/files/meeting-materials/RTA-Trustee-Meeting-Agenda-February-26-2026-draft.pdf-final.pdf",  # noqa
             "title": "Agenda",
@@ -91,20 +70,14 @@ def test_links(parsed_items):
 
 def test_classification(parsed_items):
     assert parsed_items[0]["classification"] == BOARD
-    assert parsed_items[3]["classification"] == BOARD
 
 
 def test_all_day(parsed_items):
     assert parsed_items[0]["all_day"] is False
-    assert parsed_items[3]["all_day"] is False
 
 
 def test_location(parsed_items):
     assert parsed_items[0]["location"] == {
-        "name": "Board Room, 16th Floor",
-        "address": "175 W. Jackson Blvd., Chicago, IL 60604",
-    }
-    assert parsed_items[3]["location"] == {
         "name": "RTA Headquarters",
         "address": "175 W. Jackson Blvd., Chicago, IL 60604",
     }
@@ -113,9 +86,5 @@ def test_location(parsed_items):
 def test_sources(parsed_items):
     assert (
         parsed_items[0]["source"]
-        == "https://www.rtachicago.org/about-rta/boards-and-committees/meeting-materials"  # noqa
-    )
-    assert (
-        parsed_items[3]["source"]
         == "https://www.rtachicago.org/about-rta/boards-and-committees/meeting-materials"  # noqa
     )
